61af39bd37f63236560c7957ef4b04374b249450
DaeHyun_Woo <po668312@naver.com>
2023-02-24 00:08:35 +0900

    feat: 모임 목록 페이지 마크업 초안 구현

--- src/app/meeting/page.tsx
+++ src/app/meeting/page.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { Box, Flex, Button, List, ListItem } from '@chakra-ui/react';
+import { Box, Flex, Button, List } from '@chakra-ui/react';
 
 const Meeting = () => {
   const dummyData = [
     {
-      title: '일',
-      content: '일일일일일일일일',
+      title: '제목',
+      content:
+        '용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내내용내용내용내용내용',
       id: 1,
       avatar: '아바타',
       nickName: '닉네임',
@@ -14,8 +15,9 @@ const Meeting = () => {
       bookImage: '대한민국',
     },
     {
-      title: '이',
-      content: '이이이이이이',
+      title: '제목',
+      content:
+        '용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내내용내용내용내용내용',
       id: 2,
       avatar: '아바타',
       nickName: '닉네임',
@@ -24,8 +26,9 @@ const Meeting = () => {
       bookImage: '일본',
     },
     {
-      title: '삼',
-      content: '삼삼삼삼삼삼',
+      title: '제목',
+      content:
+        '용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내내용내용내용내용내용',
       id: 2,
       avatar: '아바타',
       nickName: '닉네임',
@@ -34,8 +37,9 @@ const Meeting = () => {
       bookImage: '미국',
     },
     {
-      title: '사',
-      content: '사사사사사사',
+      title: '제목',
+      content:
+        '용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내내용내용내용내용내용',
       id: 2,
       avatar: '아바타',
       nickName: '닉네임',
@@ -46,44 +50,67 @@ const Meeting = () => {
   ];
 
   return (
-    <Flex direction="column" border={'1px'}>
-      <Flex>
-        <Box>독서 모임</Box>
-        <Button>모임 만들기</Button>
-      </Flex>
-      <Box className="모임리스트">
-        <List>
-          {dummyData.map(meeting => {
-            return (
-              <ListItem key={meeting.id}>
-                <Box className="모임전체정보">
-                  <Box className="제목내용">
-                    <Box className="제목">{meeting.title}</Box>
-                    <Box className="내용">{meeting.content}</Box>
-                  </Box>
-                  <Box className="방장및모임정보">
-                    <Box className="아바타">{meeting.avatar}</Box>
-                    <Box className="모임정보">
-                      <Box className="닉네임">{meeting.nickName}</Box>
-                      <Box className="댓글인원정보">
-                        <Box className="인원정보">
-                          <Box className="사람아이콘">{'아이콘'}</Box>
-                          <Box className="사람수">{meeting.people}</Box>
-                        </Box>
-                        <Box className="댓글정보">
-                          <Box className="댓글아이콘">{'아이콘'}</Box>
-                          <Box className="댓글수">{meeting.comments}</Box>
-                        </Box>
+    <Flex justify="center">
+      <Flex mt="30px" w="90%" direction="column">
+        <Box>
+          <Flex>
+            <Box w={'80%'} fontSize="2rem">
+              독서 모임
+            </Box>
+            <Flex justify="center" align="center" w="20%">
+              <Button fontSize="1.3rem">모임 만들기</Button>
+            </Flex>
+          </Flex>
+        </Box>
+        <Box className="모임리스트">
+          <List>
+            {dummyData.map(meeting => {
+              return (
+                <Flex
+                  key={meeting.id}
+                  m="0.8rem 0"
+                  w="100%"
+                  h="100%"
+                  justify="space-between"
+                  border="1px solid blue"
+                >
+                  <Box className="모임전체정보" w="65%">
+                    <Box className="제목내용" h="70%" p="1.5rem">
+                      <Box className="제목" h="30%" fontSize="2rem">
+                        {meeting.title}
+                      </Box>
+                      <Box className="내용" fontSize="1.3rem">
+                        {meeting.content}
                       </Box>
                     </Box>
+                    <Flex p="1.5rem 1.5rem 1.5rem 1.5rem">
+                      <Box className="아바타" w="20%">
+                        {meeting.avatar}
+                      </Box>
+                      <Flex className="모임정보" direction="column" w="80%">
+                        <Box className="닉네임">{meeting.nickName}</Box>
+                        <Flex className="댓글인원정보">
+                          <Flex className="인원정보">
+                            <Box className="사람아이콘">{'아이콘'}</Box>
+                            <Box className="사람수">{meeting.people}</Box>
+                          </Flex>
+                          <Flex className="댓글정보">
+                            <Box className="댓글아이콘">{'아이콘'}</Box>
+                            <Box className="댓글수">{meeting.comments}</Box>
+                          </Flex>
+                        </Flex>
+                      </Flex>
+                    </Flex>
                   </Box>
-                </Box>
-                <div className="책이미지">{meeting.bookImage}</div>
-              </ListItem>
-            );
-          })}
-        </List>
-      </Box>
+                  <Box className="책이미지" w="35%">
+                    {meeting.bookImage}
+                  </Box>
+                </Flex>
+              );
+            })}
+          </List>
+        </Box>
+      </Flex>
     </Flex>
   );
 };
