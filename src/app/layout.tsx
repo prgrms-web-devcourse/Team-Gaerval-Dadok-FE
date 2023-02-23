3dd0126ee5a43cba47de7fc36025707c11191f86
hanyugeon <hanyugeon@naver.com>
2023-02-23 15:16:23 +0900

    chore: 글로벌 스타일 및 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
-import './globals.css'
+import ChakraThemeProvider from '@/components/ChakraThemeProvider';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="ko-KR">
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraThemeProvider>{children}</ChakraThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/styles/font/index.tsx
@@ -0,0 +1,6 @@
+import localFont from '@next/font/local';
+
+export const LineSeedKrFont = localFont({
+  src: './LINESeedKR.woff2',
+  style: 'normal',
+});
