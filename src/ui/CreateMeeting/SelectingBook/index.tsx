4692a84cf67888b19366d659053f0d5a88a91c9b
DaeHyun_Woo <po668312@naver.com>
2023-02-27 17:54:52 +0900

    feat: 책 선택 컴포넌트 마크업 완료

--- src/ui/CreateMeeting/SelectingBook/index.tsx
+++ src/ui/CreateMeeting/SelectingBook/index.tsx
@@ -1,10 +1,21 @@
-import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 
 const SelectingBook = () => {
   return (
-    <Box>
-      <Box></Box>
-    </Box>
+    <Flex p="1rem 2rem" justify="center">
+      <Flex
+        w="9rem"
+        h="12rem"
+        fontSize="sm"
+        color="black.600"
+        align="center"
+        textAlign="center"
+        bgColor="white.600"
+        borderRadius="0.5rem"
+      >
+        책을 선택해 주세요
+      </Flex>
+    </Flex>
   );
 };
 
--- src/ui/CreateMeeting/index.tsx
+++ src/ui/CreateMeeting/index.tsx
@@ -1,12 +1,14 @@
 'use client';
 import CreateMeetingForm from './CreateMeetingForm';
 import Goback from '../GoBack';
+import SelectingBook from './SelectingBook';
 import { Box } from '@chakra-ui/react';
 
 const CreateMeeting = () => {
   return (
     <Box>
       <Goback />
+      <SelectingBook />
       <CreateMeetingForm />
     </Box>
   );
