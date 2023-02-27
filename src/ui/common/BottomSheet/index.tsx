f11d23f9002ca6b5deb2af4f2d270b1b72a67669
gxxrxn <rbfks1221@naver.com>
2023-02-27 18:22:03 +0900

    fix: DrawerContent maxWidth 설정

--- src/ui/common/BottomSheet/index.tsx
+++ src/ui/common/BottomSheet/index.tsx
@@ -16,13 +16,16 @@ const BottomSheet = ({ isOpen, onClose, children }: Props) => {
   return isOpen ? (
     <Drawer placement="bottom" isOpen={isOpen} onClose={onClose}>
       <DrawerOverlay onClick={onClose} />
-      <DrawerContent bgColor="white" borderTopRadius="1rem">
+      <DrawerContent
+        bgColor="white"
+        borderTopRadius="1rem"
+        w="43rem"
+        margin="0 auto"
+      >
         {children}
       </DrawerContent>
     </Drawer>
-  ) : (
-    <></>
-  );
+  ) : null;
 };
 
 export default BottomSheet;
