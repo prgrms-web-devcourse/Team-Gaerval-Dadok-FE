b1948ccdc705b6e3f602626fdc75ccaab4731b3c
gxxrxn <rbfks1221@naver.com>
2023-03-01 14:04:01 +0900

    refactor: 공통 컴포넌트 사용하도록 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,25 @@
 'use client';
 
 import Image from 'next/image';
-import { Button, Flex, Heading, Highlight, VStack } from '@chakra-ui/react';
+import { Flex, Heading, Highlight, VStack } from '@chakra-ui/react';
+
+import Logo from '@/ui/common/Logo';
+import Button from '@/ui/common/Button';
 
 const LoginPage = () => {
   return (
     <Flex
       height="100vh"
       direction="column"
-      alignItems="center"
-      justifyContent="space-between"
+      align="center"
+      justify="space-between"
       p="22vh 6rem"
       pos="relative"
       zIndex="1"
       bgColor="white.800"
     >
       <VStack w="100%" align="flex-start" spacing="3rem">
-        <Image
-          src="/images/logo.svg"
-          alt="다독다독 로고"
-          width={65}
-          height={65 * (70 / 71)}
-          priority
-        />
+        <Logo width={65} />
         <Heading fontSize="2xl" fontWeight="medium">
           책에 대한 모든 이야기
           <br />
@@ -34,26 +31,14 @@ const LoginPage = () => {
           </Highlight>
         </Heading>
       </VStack>
-      <Button
-        leftIcon={
-          <Image
-            src="/images/kakao.svg"
-            alt="카카오 로고"
-            width={21}
-            height={19}
-            priority
-          />
-        }
-        width="100%"
-        height="4.5rem"
-        p="1.8rem 2.5rem"
-        color="kakao.brown"
-        bgColor="kakao.yellow"
-        fontSize="md"
-        fontWeight="bold"
-        iconSpacing="1rem"
-        borderRadius="1.2rem"
-      >
+      <Button scheme="kakao" fullWidth>
+        <Image
+          src="/images/kakao.svg"
+          alt="카카오 로고"
+          width={21}
+          height={19}
+          priority
+        />
         카카오 로그인
       </Button>
     </Flex>
