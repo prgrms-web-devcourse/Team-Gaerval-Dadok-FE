03a6ad16d648acd527f9f95b88d9457fe4653c4f
hanyugeon <hanyugeon@naver.com>
2023-03-01 22:32:42 +0900

    feat: 다른 사용자 책장 페이지 헤더 작성

---
+++ src/ui/UsersBookShelfPage/UsersBookShelfHeader.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Flex, HStack, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import UsersBookShelfTag from './UsersBookShelfTag';
+
+const DUMMY_USER = {
+  userName: '벌레',
+  tags: ['개발', '프론트엔드'],
+};
+
+const UsersBookShelfHeader = () => {
+  return (
+    <Flex width="100%" justifyContent="flex-start">
+      <Image
+        width={24}
+        height={24}
+        alt="bookshelfIcon"
+        src="/icons/bookshelf.svg"
+      />
+      <Text fontSize="lg" fontWeight="700" marginRight="0.8rem">
+        {DUMMY_USER.userName}님의 책장
+      </Text>
+      <HStack gap="0.2rem">
+        {DUMMY_USER.tags.map((tag, idx) => (
+          <UsersBookShelfTag key={idx} tag={tag} />
+        ))}
+      </HStack>
+    </Flex>
+  );
+};
+
+export default UsersBookShelfHeader;
