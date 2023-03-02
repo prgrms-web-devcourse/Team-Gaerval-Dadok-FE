f5cbc6b67f1e30cf0460c133ff619e2c1c3c320d
minjongbaek <minjongbaek@gmail.com>
2023-03-03 01:34:01 +0900

    refector: userId 에 따라 다른 화면을 보여주도록 분리

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,67 +3,91 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import useUserProfileQuery from '@/queries/user/useUserProfileQuery';
+import useOtherProfileQuery from '@/queries/user/useOtherProfileQuery';
+import useMyProfileQuery from '@/queries/user/useMyProfileQuery';
+import { APIUser } from '@/types/user';
+import { ReactNode } from 'react';
 
 interface ProfilePageProps {
-  params: { id: number | string };
+  params: { id: number | 'me' };
 }
 
 const ProfilePage = ({ params }: ProfilePageProps) => {
   const { id } = params;
+
+  return id === 'me' ? <MyProfile /> : <OtherProfile id={id} />;
+};
+
+const MyProfile = () => {
+  const userProfileQuery = useMyProfileQuery();
   const pathname = usePathname();
 
-  const userProfileQuery = useUserProfileQuery({ id });
-
   if (userProfileQuery.isSuccess) {
-    const { nickname, profileImage, email, job } = userProfileQuery.data;
     return (
-      <Flex direction="column" justify="center" gap="2rem" pt="4rem" px="2rem">
-        <Flex width="100%" gap="1.5rem">
-          <Avatar
-            src={profileImage}
-            name={nickname}
-            // TODO: API 받으면 사용자의 프로필 페이지로 이동하도록 구현
-            w="8rem"
-            h="8rem"
-          />
-          <Flex direction="column" justify="center">
-            <Text fontSize="xl">{nickname}</Text>
-            <Text fontSize="sm">{email}</Text>
-          </Flex>
-        </Flex>
-        {job && (
-          <Box>
-            <Text fontSize="sm">직군 / 직업</Text>
-            <Text fontSize="md">
-              {`${job.jobGroupKoreanName} / ${job.jobName}`}
-            </Text>
-          </Box>
-        )}
-        <Box>
-          <Text fontSize="sm">내 책장</Text>
-          <Text fontSize="md">책장이 비어있습니다.</Text>
+      <Template user={userProfileQuery.data}>
+        <Box
+          as={Link}
+          href={`${pathname}/edit`}
+          px="2rem"
+          py="1rem"
+          color="main"
+          border="1px solid"
+          borderRadius="5rem"
+          textAlign="center"
+          fontSize="md"
+        >
+          프로필 수정
         </Box>
-        {id === 'me' && (
-          <Box
-            as={Link}
-            href={`${pathname}/edit`}
-            px="2rem"
-            py="1rem"
-            color="main"
-            border="1px solid"
-            borderRadius="5rem"
-            textAlign="center"
-            fontSize="md"
-          >
-            프로필 수정
-          </Box>
-        )}
-      </Flex>
+      </Template>
     );
   }
 
   return null;
+};
+
+const OtherProfile = ({ id }: { id: APIUser['userId'] }) => {
+  const userProfileQuery = useOtherProfileQuery({ id });
+
+  if (userProfileQuery.isSuccess) {
+    return <Template user={userProfileQuery.data} />;
+  }
+
+  return null;
+};
+
+const Template = ({
+  user,
+  children,
+}: {
+  user: APIUser;
+  children?: ReactNode;
+}) => {
+  const { nickname, profileImage, email, job } = user;
+  const { jobGroupKoreanName, jobName } = job;
+  return (
+    <Flex direction="column" justify="center" gap="2rem" pt="4rem" px="2rem">
+      <Flex width="100%" gap="1.5rem">
+        <Avatar src={profileImage} name={nickname} w="8rem" h="8rem" />
+        <Flex direction="column" justify="center">
+          <Text fontSize="xl">{nickname}</Text>
+          <Text fontSize="sm">{email}</Text>
+        </Flex>
+      </Flex>
+      <Box>
+        <Text fontSize="sm">직군 / 직업</Text>
+        <Text fontSize="md">
+          {jobGroupKoreanName && jobName
+            ? `${job.jobGroupKoreanName} / ${job.jobName}`
+            : '직업이 등록되지 않았습니다.'}
+        </Text>
+      </Box>
+      <Box>
+        <Text fontSize="sm">내 책장</Text>
+        <Text fontSize="md">책장이 비어있습니다.</Text>
+      </Box>
+      {children}
+    </Flex>
+  );
 };
 
 export default ProfilePage;
