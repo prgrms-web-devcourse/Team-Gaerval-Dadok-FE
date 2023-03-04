f98f81ec6da5ec0f3b6e60351cecf8855c95b091
DaeHyun_Woo <po668312@naver.com>
2023-03-05 05:01:24 +0900

    feat: 모임 리스트 검색 기능 구현

--- src/ui/Meeting/MeetingSearch/index.tsx
+++ src/ui/Meeting/MeetingSearch/index.tsx
@@ -1,14 +1,43 @@
+import { useState } from 'react';
 import { Input, Select, Button, Image, Flex } from '@chakra-ui/react';
 
-const MeetingSearch = () => {
+interface setSearchValueProps {
+  input: string;
+  select: string;
+}
+interface MeetingSearchProps {
+  setSearchValue: (arg0: setSearchValueProps) => void;
+}
+
+const MeetingSearch = ({ setSearchValue }: MeetingSearchProps) => {
+  const [inputValue, setInputValue] = useState('');
+  const [selectValue, setSelectValue] = useState('모임');
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectValue(event.target.value);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLDivElement>) => {
+    event && event.preventDefault();
+    setSearchValue({
+      input: inputValue,
+      select: selectValue,
+    });
+  };
+
   return (
-    <Flex as="form" m="1rem 0" boxShadow="default">
+    <Flex as="form" m="1rem 0" boxShadow="default" onSubmit={handleSubmit}>
       <Select
         w="14rem"
         h="3.5rem"
         borderRightRadius="none"
         borderRight="none"
         variant="filled"
+        onChange={handleSelectChange}
       >
         <option value="모임">모임</option>
         <option value="책 제목">책 제목</option>
@@ -19,6 +48,7 @@ const MeetingSearch = () => {
         background="white"
         border="none"
         placeholder="검색어를 입력해 주세요."
+        onChange={handleInputChange}
       />
       <Button
         type="submit"
