e3da717ca3cfe60e9b2714cf5089a9c15aee3403
minjongbaek <minjongbaek@gmail.com>
2023-03-06 03:25:20 +0900

    fix: APIUser interface 수정

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,7 +3,8 @@ import { APIJob } from './job';
 export interface APIUser {
   userId: number;
   name: string | null;
-  nickname: string;
+  nickname: string | null;
+  oauthNickname: string;
   email: string | null;
   profileImage: string;
   gender: true;
