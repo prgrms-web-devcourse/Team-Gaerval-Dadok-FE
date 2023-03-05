9fe4a36e86dc997c0c80f3599176226a01287bf5
minjongbaek <minjongbaek@gmail.com>
2023-03-06 03:48:53 +0900

    refector: job interface 수정

--- src/types/job.ts
+++ src/types/job.ts
@@ -1,7 +1,25 @@
 export interface APIJob {
-  jobGroupKoreanName: string;
-  jobGroupName: string;
-  jobNameKoreanName: string | null;
-  jobName: string;
-  order: string;
+  jobGroupKoreanName: APIJobGroup['koreanName'];
+  jobGroupName: APIJobGroup['name'];
+  jobNameKoreanName: APIJobName['koreanName'] | null;
+  jobName: APIJobName['name'];
+  order: APIJobName['order'];
+}
+
+export interface APIJobGroup {
+  koreanName: string;
+  name: string;
+}
+
+export interface APIJobName {
+  koreanName: string;
+  name: string;
+  order: number;
+}
+
+export interface APIAllJob {
+  jobs: {
+    jobGroup: APIJobGroup;
+    jobNames: APIJobName[];
+  }[];
 }
