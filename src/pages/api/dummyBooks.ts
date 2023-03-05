3106d374e8e6a79bf6cd1e7b049768ec6102af37
hanyugeon <hanyugeon@naver.com>
2023-03-05 16:17:21 +0900

    chore: 책 더미데이터 수정

--- src/pages/api/dummyBooks.ts
+++ src/pages/api/dummyBooks.ts
@@ -5,6 +5,120 @@ import {
   imgRefactoringBook,
 } from '@public/images';
 
+// 책장 정보 조회
+// /api/bookshelves?userId=1
+export const USERS_BOOKSHELF_INFO = {
+  bookshelfId: 1,
+  bookshelfName: '책장이름',
+  isPublic: true,
+  userId: 1,
+  username: 'username',
+  userNickname: 'userNickname',
+  userProfileImage: 'http://dadok.com/images',
+  job: {
+    jobGroupKoreanName: '개발',
+    jobGroupName: 'DEVELOPMENT',
+    jobNameKoreanName: '백엔드 개발자',
+    jobName: 'BACKEND_DEVELOPER',
+    order: 5,
+  },
+};
+
+// 책장 책 조회
+// /api/bookshelves/3/books?type=READ&pageSize=10&bookCursorId=3&sortDirection=DESC
+export const USERS_BOOKSHELF_BOOKLIST = {
+  count: 7,
+  books: [
+    {
+      bookId: 1,
+      title: '미움받을 용기',
+      author: '기시미 이치로, 고가 후미타케',
+      isbn: '9788996991342',
+      contents:
+        '인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기 위해서는 ‘용기’가 필요하다고 말한 철학자가 있다.',
+      imageUrl:
+        'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F6291759',
+      url: 'https://search.daum.net/search?w=bookpage&bookId=1467038&q=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
+      publisher: '인플루엔셜',
+    },
+    {
+      bookId: 2,
+      title: '미움받을 용기',
+      author: '기시미 이치로, 고가 후미타케',
+      isbn: '9788996991342',
+      contents:
+        '인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기 위해서는 ‘용기’가 필요하다고 말한 철학자가 있다.',
+      imageUrl:
+        'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F6052931%3Ftimestamp%3D20221011185513',
+      url: 'https://search.daum.net/search?w=bookpage&bookId=1467038&q=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
+      publisher: '인플루엔셜',
+    },
+    {
+      bookId: 3,
+      title: '미움받을 용기',
+      author: '기시미 이치로, 고가 후미타케',
+      isbn: '9788996991342',
+      contents:
+        '인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기 위해서는 ‘용기’가 필요하다고 말한 철학자가 있다.',
+      imageUrl:
+        'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F5477653%3Ftimestamp%3D20221107233622',
+      url: 'https://search.daum.net/search?w=bookpage&bookId=1467038&q=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
+      publisher: '인플루엔셜',
+    },
+    {
+      bookId: 3,
+      title: '미움받을 용기',
+      author: '기시미 이치로, 고가 후미타케',
+      isbn: '9788996991342',
+      contents:
+        '인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기 위해서는 ‘용기’가 필요하다고 말한 철학자가 있다.',
+      imageUrl:
+        'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F5326912%3Ftimestamp%3D20221231221935',
+      url: 'https://search.daum.net/search?w=bookpage&bookId=1467038&q=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
+      publisher: '인플루엔셜',
+    },
+    {
+      bookId: 3,
+      title: '미움받을 용기',
+      author: '기시미 이치로, 고가 후미타케',
+      isbn: '9788996991342',
+      contents:
+        '인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기 위해서는 ‘용기’가 필요하다고 말한 철학자가 있다.',
+      imageUrl:
+        'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1624084%3Ftimestamp%3D20230217231000',
+      url: 'https://search.daum.net/search?w=bookpage&bookId=1467038&q=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
+      publisher: '인플루엔셜',
+    },
+    {
+      bookId: 3,
+      title: '미움받을 용기',
+      author: '기시미 이치로, 고가 후미타케',
+      isbn: '9788996991342',
+      contents:
+        '인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기 위해서는 ‘용기’가 필요하다고 말한 철학자가 있다.',
+      imageUrl:
+        'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038',
+      url: 'https://search.daum.net/search?w=bookpage&bookId=1467038&q=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
+      publisher: '인플루엔셜',
+    },
+    {
+      bookId: 3,
+      title: '미움받을 용기',
+      author: '기시미 이치로, 고가 후미타케',
+      isbn: '9788996991342',
+      contents:
+        '인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기 위해서는 ‘용기’가 필요하다고 말한 철학자가 있다.',
+      imageUrl:
+        'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F5140806%3Ftimestamp%3D20230217200817',
+      url: 'https://search.daum.net/search?w=bookpage&bookId=1467038&q=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
+      publisher: '인플루엔셜',
+    },
+  ],
+  empty: false,
+  first: true,
+  last: true,
+};
+
 export const DUMMY_BOOKS = [
   {
     id: '0',
