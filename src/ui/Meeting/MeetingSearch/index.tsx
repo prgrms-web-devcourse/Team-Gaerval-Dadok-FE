69bec5aa6edcffcf1111e9d3df0803a2cc33f2d6
DaeHyun_Woo <po668312@naver.com>
2023-03-06 15:56:09 +0900

    [#74] 모임 리스트 페이지 검색창 구현 (#82)
    * feat: 모임 리스트 검색창 마크업
    
    * feat: 모임 리스트 검색 기능 구현
    
    * fix: 모임 상세 페이지 하단 높이 조절
    
    * fix: 검색창 InputGroup으로 변경
    
    * chore: next.config.js rewrites부분 주석 처리 해제
    
    * chore: interface명 변경

---
+++ src/ui/Meeting/MeetingSearch/index.tsx
@@ -0,0 +1,82 @@
+import { SearchIcon } from '@chakra-ui/icons';
+import {
+  Input,
+  Select,
+  Flex,
+  InputRightElement,
+  IconButton,
+  InputGroup,
+} from '@chakra-ui/react';
+
+interface SearchValue {
+  input: string;
+  select: string;
+}
+interface MeetingSearchProps {
+  searchValue: SearchValue;
+  handleChange: (name: string, value: string) => void;
+  handleSumbit: () => void;
+}
+
+const MeetingSearch = ({
+  searchValue,
+  handleChange,
+  handleSumbit,
+}: MeetingSearchProps) => {
+  const { input, select } = searchValue;
+
+  const handleSubmit = (event: React.FormEvent<HTMLDivElement>) => {
+    event && event.preventDefault();
+    handleSumbit();
+  };
+
+  return (
+    <Flex
+      as="form"
+      m="1rem 0"
+      onSubmit={handleSubmit}
+      borderRadius="1rem"
+      boxShadow="default"
+    >
+      <Select
+        w="14rem"
+        h="3.5rem"
+        borderRightRadius="none"
+        borderLeftRadius="1rem"
+        borderRight="none"
+        variant="filled"
+        onChange={event => handleChange('select', event.target.value)}
+        value={select}
+      >
+        <option value="모임">모임</option>
+        <option value="책 제목">책 제목</option>
+      </Select>
+      <InputGroup>
+        <Input
+          borderRightRadius="1rem"
+          borderLeftRadius="none"
+          h="3.5rem"
+          background="white"
+          border="none"
+          placeholder="검색어를 입력해 주세요."
+          onChange={event => handleChange('input', event.target.value)}
+          value={input}
+        />
+        <InputRightElement>
+          <IconButton
+            m="1rem 0.6rem 0 0"
+            bgColor="white"
+            borderRightRadius="1rem"
+            h="3.5rem"
+            aria-label="Search database"
+            icon={<SearchIcon />}
+            type="submit"
+            size="lg"
+          />
+        </InputRightElement>
+      </InputGroup>
+    </Flex>
+  );
+};
+
+export default MeetingSearch;
--- src/ui/Meeting/index.tsx
+++ src/ui/Meeting/index.tsx
@@ -1,60 +1,112 @@
 'use client';
 import { Flex } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
-import MeetingList from './MeetingList';
 import MeetingListHeader from './MeetingListHeader';
+import MeetingSearch from './MeetingSearch';
+import MeetingList from './MeetingList';
+
+const DUMMY_DATA = [
+  {
+    title: '악인을 만난적인 있나요?',
+    content:
+      '늘 감사하고 착한 사람 콤플렉스에 빠져 우울한 현실을 마주한 저자가 분노와 열등감으로 자신이 주도하는 삶을 살게 된 이야기를 담았다. 말하기, 글쓰기 기술을 비롯, 훈련을 통해 개발한 성공의 5가지 무기로 치열하게 자신을 불태울 각오로 노력하는 삶으로 이끄는 책이다.',
+    id: 234,
+    avatar: 'https://image.yes24.com/Goods/117498667/L',
+    nickName: '아라한장풍',
+    people: 1,
+    comments: 1,
+    bookImageURL: 'https://image.yes24.com/Goods/117498667/L',
+  },
+  {
+    title: '주식 투자에 매번 실패하시는 분!',
+    content:
+      '성장할 것 같은 종목을 고르고, 해당 종목을 적절한 때 매수 매도해야 하며, 이 과정에서 잘못 들어갔다면 확실한 손절점을 정해서 손실을 보더라도 빠져나와야 결과적으로 수익을 볼 수 있다는 것이다. 초고수익은 운으로 만들어지지 않는다. 마크 미너비니가 공유한 투자법을 통해 모두 차세대 애플, 구글, 스타벅스를 찾길 바란다.',
+    id: 2525,
+    avatar: 'https://bit.ly/dan-abramov',
+    nickName: '잠실동쭈꾸미',
+    people: 22,
+    comments: 23,
+    bookImageURL: 'https://image.yes24.com/Goods/117650984/L',
+  },
+  {
+    title: '살 때, 팔 때, 벌 때',
+    content:
+      '“주식의 시간은 따로 있다!”여의도 1타 브로커 강영현이 공개하는 2023 혼돈의 시장을 돌파할 최강 투자 바이블!',
+    id: 7575,
+    avatar: 'http://image.yes24.com/goods/117614110/XL',
+    nickName: '응모아조씨',
+    people: 3233,
+    comments: 3233,
+    bookImageURL: 'http://image.yes24.com/goods/117614110/XL',
+  },
+  {
+    title: '톨스토이 책 읽으신 분들',
+    content:
+      '러시아의 세계적인 대문호이자 사상가인 톨스토이의 단편집. 인생의 진정한 의미를 잃어버린 사람들을 위해서 글을 쓴 톨스토이와 그의 작품들은 백 년이 지난 지금도 우리에게 강한 울림을 주고 있다.',
+    id: 90,
+    avatar: 'https://image.yes24.com/goods/117726598/L',
+    nickName: '도마뱀꼬리는재생',
+    people: 423,
+    comments: 423,
+    bookImageURL: 'https://image.yes24.com/goods/117726598/L',
+  },
+];
+
+interface props {
+  title: string;
+  content: string;
+  id: number;
+  avatar: string;
+  nickName: string;
+  people: number;
+  comments: number;
+  bookImageURL: string;
+}
+
+interface SearchValue {
+  [key: string]: string;
+  input: string;
+  select: string;
+}
 
 const MeetingPageContainer = () => {
-  const DUMMY_DATA = [
-    {
-      title: '악인을 만난적인 있나요?',
-      content:
-        '늘 감사하고 착한 사람 콤플렉스에 빠져 우울한 현실을 마주한 저자가 분노와 열등감으로 자신이 주도하는 삶을 살게 된 이야기를 담았다. 말하기, 글쓰기 기술을 비롯, 훈련을 통해 개발한 성공의 5가지 무기로 치열하게 자신을 불태울 각오로 노력하는 삶으로 이끄는 책이다.',
-      id: 234,
-      avatar: 'https://image.yes24.com/Goods/117498667/L',
-      nickName: '아라한장풍',
-      people: 1,
-      comments: 1,
-      bookImageURL: 'https://image.yes24.com/Goods/117498667/L',
-    },
-    {
-      title: '주식 투자에 매번 실패하시는 분!',
-      content:
-        '성장할 것 같은 종목을 고르고, 해당 종목을 적절한 때 매수 매도해야 하며, 이 과정에서 잘못 들어갔다면 확실한 손절점을 정해서 손실을 보더라도 빠져나와야 결과적으로 수익을 볼 수 있다는 것이다. 초고수익은 운으로 만들어지지 않는다. 마크 미너비니가 공유한 투자법을 통해 모두 차세대 애플, 구글, 스타벅스를 찾길 바란다.',
-      id: 2525,
-      avatar: 'https://bit.ly/dan-abramov',
-      nickName: '잠실동쭈꾸미',
-      people: 22,
-      comments: 23,
-      bookImageURL: 'https://image.yes24.com/Goods/117650984/L',
-    },
-    {
-      title: '살 때, 팔 때, 벌 때',
-      content:
-        '“주식의 시간은 따로 있다!”여의도 1타 브로커 강영현이 공개하는 2023 혼돈의 시장을 돌파할 최강 투자 바이블!',
-      id: 7575,
-      avatar: 'http://image.yes24.com/goods/117614110/XL',
-      nickName: '응모아조씨',
-      people: 3233,
-      comments: 3233,
-      bookImageURL: 'http://image.yes24.com/goods/117614110/XL',
-    },
-    {
-      title: '톨스토이 책 읽으신 분들',
-      content:
-        '러시아의 세계적인 대문호이자 사상가인 톨스토이의 단편집. 인생의 진정한 의미를 잃어버린 사람들을 위해서 글을 쓴 톨스토이와 그의 작품들은 백 년이 지난 지금도 우리에게 강한 울림을 주고 있다.',
-      id: 90,
-      avatar: 'https://image.yes24.com/goods/117726598/L',
-      nickName: '도마뱀꼬리는재생',
-      people: 423,
-      comments: 423,
-      bookImageURL: 'https://image.yes24.com/goods/117726598/L',
-    },
-  ];
+  const [MeetingListData, setMeetingListData] = useState<props[]>([]);
+  const [searchValue, setSearchValue] = useState<SearchValue>({
+    input: '',
+    select: '모임',
+  });
+
+  const handleSumbit = () => {
+    console.log(searchValue);
+    const { input } = searchValue;
+    if (input.trim().length === 0) {
+      /*공백만 입력한 경우 전체 데이터 렌더링 */
+    } else {
+      /*검색 API호출 및 setMeetingListData 업데이트 */
+    }
+  };
+
+  const handleChange = (name: string, value: string) => {
+    if (!(name in searchValue)) return;
+    const tempSearchValue = { ...searchValue };
+    tempSearchValue[name] = value;
+    setSearchValue(tempSearchValue);
+  };
+
+  useEffect(() => {
+    setMeetingListData(DUMMY_DATA);
+  }, []);
+
   return (
     <Flex mt="2rem" direction="column" px="5%" mb="9rem">
       <MeetingListHeader />
-      <MeetingList meetingInfo={DUMMY_DATA} />
+      <MeetingSearch
+        searchValue={searchValue}
+        handleChange={handleChange}
+        handleSumbit={handleSumbit}
+      />
+      <MeetingList meetingInfo={MeetingListData} />
     </Flex>
   );
 };
