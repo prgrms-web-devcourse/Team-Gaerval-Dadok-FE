583608025c201f019ae9f2418d603a0c4dbcf067
minjongbaek <minjongbaek@gmail.com>
2023-03-07 00:20:39 +0900

    feat: 추가정보 입력 페이지에서 닉네임이 있는 경우 기본값처리

--- src/app/profile/me/add/page.tsx
+++ src/app/profile/me/add/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import useAllJobQuery from '@/queries/job/useAllJobQuery';
+import useMyProfileQuery from '@/queries/user/useMyProfileQuery';
 import AdditionalProfileForm from '@/ui/AdditionalProfileForm';
 import { Heading, Text, VStack } from '@chakra-ui/react';
 
 const AdditionalProfile = () => {
   const allJobQuery = useAllJobQuery();
+  const userProfileQuery = useMyProfileQuery();
 
-  if (!allJobQuery.isSuccess) return null;
+  const isSuccess = allJobQuery.isSuccess && userProfileQuery.isSuccess;
+  if (!isSuccess) return null;
 
   return (
     <VStack
@@ -18,7 +21,7 @@ const AdditionalProfile = () => {
       p="6rem 2rem"
       gap="1rem"
     >
-      <Heading fontSize="lg">추가 정보를 입력해주세요!</Heading>
+      <Heading fontSize="lg">추가 정보를 입력해 주세요!</Heading>
       <Text fontSize="md" textAlign="center">
         추가 정보를 입력하면
         <br />
@@ -27,7 +30,10 @@ const AdditionalProfile = () => {
         </Text>
         이 추천하는 책장을 볼 수 있어요!
       </Text>
-      <AdditionalProfileForm jobGroups={allJobQuery.data.jobGroups} />
+      <AdditionalProfileForm
+        nickname={userProfileQuery.data.nickname}
+        jobGroups={allJobQuery.data.jobGroups}
+      />
     </VStack>
   );
 };
