5b3c307861428268c3e5d5e53683607b342e20cf
DaeHyun_Woo <po668312@naver.com>
2023-03-08 02:50:08 +0900

    Update src/app/meeting/[id]/page.tsx
    Co-authored-by: kyuran kim | fry <57716832+gxxrxn@users.noreply.github.com>

--- src/app/meeting/[id]/page.tsx
+++ src/app/meeting/[id]/page.tsx
@@ -6,7 +6,6 @@ import MeetingDetail from '@/ui/MeetingDetail';
 import IconButton from '@/ui/common/IconButton';
 
 const MeetingDetailPage = ({ params: { id } }: { params: { id: string } }) => {
-  console.log(id);
   return (
     <Flex px="5%" direction="column" justify="center" mt="1rem">
       <IconButton name="back" />
