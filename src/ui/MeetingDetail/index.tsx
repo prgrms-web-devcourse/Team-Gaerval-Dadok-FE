c951825d7c5f5e82672cc383062b0e0778c8511c
DaeHyun_Woo <po668312@naver.com>
2023-03-08 04:53:12 +0900

    feat: 모임 상세 페이지 댓글 리스트 API 연결

--- src/ui/MeetingDetail/index.tsx
+++ src/ui/MeetingDetail/index.tsx
@@ -1,77 +1,28 @@
 'use client';
-import { useState, useEffect } from 'react';
+
 import { Flex } from '@chakra-ui/react';
 
 import MeetingInfo from '@/ui/MeetingDetail/MeetingInfo';
 import CommentInputBox from '@/ui/MeetingDetail/CommentInputBox';
 import CommentsList from '@/ui/MeetingDetail/CommentsList';
-import useMeetingDetailInfoQuery from '@/queries/meeting/useMeetingDetailInfoQuery';
-
-const DUMMY_COMMENTS_LIST_DATA = [
-  {
-    id: 1,
-    avatarURL: 'https://bit.ly/dan-abramov',
-    nickName: '김규란',
-    contents:
-      '백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세',
-    isWrittenUser: false,
-  },
-  {
-    id: 2,
-    avatarURL: 'https://bit.ly/dan-abramov',
-    nickName: '김재현',
-    contents:
-      '백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세',
-    isWrittenUser: true,
-  },
-  {
-    id: 3,
-    avatarURL: 'https://bit.ly/dan-abramov',
-    nickName: '백민종',
-    contents:
-      '백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세',
-    isWrittenUser: false,
-  },
-  {
-    id: 4,
-    avatarURL: 'https://bit.ly/dan-abramov',
-    nickName: '동해물과',
-    contents:
-      '백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세 무궁화 삼천리 화려 강산 대한 사람 대한으로 길이 보전하세',
-    isWrittenUser: false,
-  },
-];
+import useMeetingInfoQuery from '@/queries/meeting/useMeetingInfoQuery';
+import useMeetingCommentsQuery from '@/queries/meeting/useMeetingCommentsQuery';
 
 interface MeetingDetailProps {
   bookGroupId: number;
 }
 
-interface commentsListDataProps {
-  id: number;
-  avatarURL: string;
-  nickName: string;
-  contents: string;
-  isWrittenUser: boolean;
-}
-
 const MeetingDetail = ({ bookGroupId }: MeetingDetailProps) => {
-  const [commentsListData, setCommentsListDate] = useState<
-    commentsListDataProps[]
-  >([]);
+  const meetingInfoQuery = useMeetingInfoQuery({ bookGroupId });
+  const meetingCommentsQuery = useMeetingCommentsQuery({ bookGroupId });
 
-  /* 댓글 리스트 조회 API 연동시, useEffect 삭제 예정 */
-  useEffect(() => {
-    setCommentsListDate(DUMMY_COMMENTS_LIST_DATA);
-  }, []);
-
-  const meetingDetailInfoQuery = useMeetingDetailInfoQuery({ bookGroupId });
-  const isSuccess = meetingDetailInfoQuery.isSuccess;
+  const isSuccess =
+    meetingInfoQuery.isSuccess && meetingCommentsQuery.isSuccess;
   if (!isSuccess) return null;
-
 
   const handleParticipateBtnClick = () => {
     console.log('모임에 참여했습니다.');
-    meetingDetailInfoQuery.refetch();
+    meetingInfoQuery.refetch();
     /*모임 참여 버튼 클릭시, 
       1) 모임 참여 관련 API 호출 예정
       2) 유저의 책장에 책 꽂기 API 호출 예정 
@@ -108,15 +59,15 @@ const MeetingDetail = ({ bookGroupId }: MeetingDetailProps) => {
   return (
     <Flex px="5%" direction="column" justify="center" mt="1rem">
       <MeetingInfo
-        meetingInfoData={meetingDetailInfoQuery.data}
+        meetingInfoData={meetingInfoQuery.data}
         handleParticipateBtnClick={handleParticipateBtnClick}
       />
       <CommentInputBox
-        isPartInUser={meetingDetailInfoQuery.data.isGroupMember}
+        isPartInUser={meetingInfoQuery.data.isGroupMember}
         handleCreateCommentBtnClick={handleCreateCommentBtnClick}
       />
       <CommentsList
-        commentsListData={commentsListData}
+        commentsListData={meetingCommentsQuery.data.bookGroupComments}
         handleDeleteCommentBtnClick={handleDeleteCommentBtnClick}
         handleModifyCommentBtnClick={handleModifyCommentBtnClick}
       />
