2f95c5278d5f93ba7c1a4b9013e0b72b69edfd0c
DaeHyun_Woo <po668312@naver.com>
2023-03-08 03:00:26 +0900

    feat: 모임 상세 페이지 댓글 리스트 타입 정의

---
+++ src/types/meetingDetailCommentsList.ts
@@ -0,0 +1,24 @@
+export interface APIDateAt {
+  createdAt: number;
+  modifiedAt: number;
+}
+
+export interface APIBookGroupComments {
+  commentId: number;
+  contents: string;
+  bookGroupId: number;
+  parentCommentId: number;
+  userId: number;
+  userProfileImage: string;
+  createdAt: APIDateAt[];
+  modifiedAt: APIDateAt[];
+}
+
+export interface APIBookGroupCommentsReference {
+  isFirst: boolean;
+  isLast: boolean;
+  hasNext: boolean;
+  count: number;
+  isEmpty: boolean;
+  bookGroupComments: APIBookGroupComments[];
+}
