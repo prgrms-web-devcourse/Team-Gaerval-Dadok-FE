a24a7f86599875692bc522f1b6d62bfca60ac672
minjongbaek <minjongbaek@gmail.com>
2023-03-09 05:51:56 +0900

    fix: 페이지가 바뀔 때 트랜지션이 두 번 적용되는 이슈

--- src/ui/common/Layout/index.tsx
+++ src/ui/common/Layout/index.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import { Box } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
 import BottomNavigation from '@/ui/BottomNavigation';
 
@@ -10,30 +10,39 @@ const Layout = ({ children }: { children: ReactNode }) => {
   const pathname = usePathname();
   const isShowNavigation = pathname === null || paths.includes(pathname);
   return (
-    <Box h="100vh">
-      <Box
-        as={motion.div}
-        key={pathname}
-        px="2rem"
-        pt="2rem"
-        pb={!isShowNavigation ? '9rem' : '2rem'}
-        h="100%"
-        overflow="auto"
-        initial="initial"
-        animate="animate"
-        variants={{
-          initial: {
-            opacity: 0,
-          },
-          animate: {
-            opacity: 1,
-          },
-        }}
-      >
-        {children}
+    <AnimatePresence mode="wait">
+      <Box h="100vh">
+        <Box
+          as={motion.div}
+          key={pathname}
+          px="2rem"
+          pt="2rem"
+          pb={!isShowNavigation ? '9rem' : '2rem'}
+          h="100%"
+          overflow="auto"
+          variants={{
+            in: {
+              opacity: 1,
+              transition: {
+                duration: 0.5,
+                delay: 0.1,
+              },
+            },
+            out: {
+              opacity: 0,
+              transition: {
+                duration: 0.5,
+              },
+            },
+          }}
+          animate="in"
+          initial="out"
+        >
+          {children}
+        </Box>
+        {!isShowNavigation && <BottomNavigation />}
       </Box>
-      {!isShowNavigation && <BottomNavigation />}
-    </Box>
+    </AnimatePresence>
   );
 };
 
