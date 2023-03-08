96dbbee57d0aa7b0dd1e06a21115fafb805e53bd
minjongbaek <minjongbaek@gmail.com>
2023-03-08 13:49:17 +0900

    refector: MeetingForm에 submit 핸들러를 prop으로 전달하게끔 수정

--- src/app/meeting/create/page.tsx
+++ src/app/meeting/create/page.tsx
@@ -1,13 +1,30 @@
-import CreateMeeting from '@/ui/CreateMeeting';
-import IconButton from '@/ui/common/IconButton';
+'use client';
 
-const createMeeting = () => {
+import MeetingForm from '@/ui/MeetingForm';
+import { VStack } from '@chakra-ui/react';
+import TopNavigation from '@/ui/common/TopNavigation';
+import { useRouter } from 'next/navigation';
+import MeetingAPI from '@/apis/Meeting';
+import { APICreateMeetingReqeust } from '@/types/meeting';
+
+const CreateMeetingPage = () => {
+  const router = useRouter();
+
+  const onSubmit = async (meeting: APICreateMeetingReqeust) => {
+    try {
+      await MeetingAPI.createMeeting({ meeting });
+      router.replace('/meeting');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <>
-      <IconButton name="back" />
-      <CreateMeeting />
-    </>
+    <VStack justify="center" align="center" pt="2rem" px="2rem">
+      <TopNavigation pageTitle="" />
+      <MeetingForm onSubmit={onSubmit} />
+    </VStack>
   );
 };
 
-export default createMeeting;
+export default CreateMeetingPage;
