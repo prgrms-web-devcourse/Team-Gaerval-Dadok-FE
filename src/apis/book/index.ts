9dfe2473ee7bf258d165d1793a9cdbece5e2be03
kyuran kim | fry <57716832+gxxrxn@users.noreply.github.com>
2023-03-08 15:51:13 +0900

    [#129] 책 상세 페이지 > 책 상세 조회 api 연결 (#133)

--- src/apis/book/index.ts
+++ src/apis/book/index.ts
@@ -1,12 +1,13 @@
 import { publicApi } from '../core/axios';
-import type { APIBook } from '@/types/book';
+import type { APIBook, APIBookInfo, APIDefaultBook } from '@/types/book';
 
 const bookAPI = {
   getBooks: ({ query }: { query: string }) =>
     publicApi.get<{ searchBookResponseList: APIBook[] }>(
       `/api/books?query=${query}`
     ),
-
+  getBookInfo: (bookId: APIDefaultBook['bookId']) =>
+    publicApi.get<APIBookInfo>(`/api/books/${bookId}`),
   createBook: ({ book }: { book: APIBook }) =>
     publicApi.post<Pick<APIBook, 'bookId'>>('/api/books', book),
 };
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,42 +1,59 @@
 'use client';
 
-import { Box, Heading, VStack } from '@chakra-ui/react';
+import { Box, Heading, Skeleton, SkeletonText, VStack } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
 
 import IconButton from '@/ui/common/IconButton';
 import { BookInfo, BookCommentList } from '@/ui/BookDetail';
-import type { APIBookInfo, APIDefaultBook } from '@/types/book';
+import useBookInfoQuery from '@/queries/book/useBookInfoQuery';
+
+import type { APIDefaultBook } from '@/types/book';
 
 const BookDetailPage = ({
   params: { id: bookId },
 }: {
   params: { id: APIDefaultBook['bookId'] };
 }) => {
-  const { title, author, contents, imageUrl }: APIBookInfo = {
-    title: '미움받을 용기',
-    author: '기시미 이치로, 고가 후미타케',
-    isbn: '9788996991342',
-    contents:
-      '인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기 위해서는 ‘용기’가 필요하다고 말한 철학자가 있다.',
-    url: 'https://search.daum.net/search?w=bookpage&bookId=1467038&q=%EB%AF%B8%EC%9B%80%EB%B0%9B%EC%9D%84+%EC%9A%A9%EA%B8%B0',
-    imageUrl:
-      'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038',
-    apiProvider: 'KAKAO',
-    publisher: '인플루엔셜',
-    imageKey:
-      '687f74fd-a612-4ec9-9ae5-8f7b7fe8e80f/photo-1606787364406-a3cdf06c6d0c.jpeg',
-    bookId: 123,
-  };
+  const router = useRouter();
+  const bookQueryInfo = useBookInfoQuery(bookId, {
+    onError: () => {
+      router.replace('/');
+    },
+  });
 
   return (
-    <Box pt="2rem" px="2rem">
+    <Box pt="2rem" px="2rem" width="100%">
       <IconButton name="back" />
-      <BookInfo
-        bookId={bookId}
-        title={title}
-        author={author}
-        imageUrl={imageUrl}
-        contents={contents}
-      />
+      <VStack
+        w="100%"
+        bgColor="white"
+        p="2rem"
+        shadow="lg"
+        align="stretch"
+        borderLeftRadius={15}
+        gap="2rem"
+      >
+        {bookQueryInfo.isSuccess && (
+          <BookInfo
+            bookId={bookId}
+            title={bookQueryInfo.data.title}
+            author={bookQueryInfo.data.author}
+            imageUrl={bookQueryInfo.data.imageUrl}
+            contents={bookQueryInfo.data.contents}
+          />
+        )}
+        {bookQueryInfo.isLoading && (
+          <VStack spacing="2rem" align="stretch">
+            <Skeleton width="18rem" height="25rem" />
+            <SkeletonText
+              mt="4"
+              noOfLines={4}
+              spacing="4"
+              skeletonHeight="1.4rem"
+            />
+          </VStack>
+        )}
+      </VStack>
       <VStack align="flex-start">
         <Heading pt="3rem" pb="1rem" fontSize="lg">
           이 책에 남긴 글
