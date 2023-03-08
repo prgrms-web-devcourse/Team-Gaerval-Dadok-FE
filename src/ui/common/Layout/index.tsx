12087a6b98c0ae05af35fd2b457885023455e33a
minjongbaek <minjongbaek@gmail.com>
2023-03-08 19:42:27 +0900

    feat: Layout 컴포넌트에 애니메이션 적용

--- src/ui/common/Layout/index.tsx
+++ src/ui/common/Layout/index.tsx
@@ -1,11 +1,31 @@
 import { ReactNode } from 'react';
 import { Box } from '@chakra-ui/react';
-// import BottomNavigation from '@/ui/BottomNavigation';
+import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
   return (
     <Box h="100vh">
-      <Box px="2rem" pt="2rem" pb="9rem" h="100%" overflow="auto">
+      <Box
+        as={motion.div}
+        key={pathname}
+        px="2rem"
+        pt="2rem"
+        pb="9rem"
+        h="100%"
+        overflow="auto"
+        initial="initial"
+        animate="animate"
+        variants={{
+          initial: {
+            opacity: 0,
+          },
+          animate: {
+            opacity: 1,
+          },
+        }}
+      >
         {children}
       </Box>
     </Box>
