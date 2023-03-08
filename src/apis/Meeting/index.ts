e7720c5d2076f66f73a9f44f65da23ecfacc2c89
DaeHyun_Woo <po668312@naver.com>
2023-03-08 15:57:29 +0900

    [#128] 모임 상세 페이지 댓글 리스트 API 연동 (#132)
    * feat: 모임 상세 페이지 댓글 리스트 타입 정의
    
    * feat: 모임 상세 페이지 댓글 리스트  API작성
    
    * feat: 모임 상세 페이지 댓글 리스트 API 연결
    
    * Update src/types/meetingDetailCommentsList.ts
    
    Co-authored-by: minjong <48359052+minjongbaek@users.noreply.github.com>
    
    * fix: 중복 import 제거
    
    ---------
    
    Co-authored-by: minjong <48359052+minjongbaek@users.noreply.github.com>

--- src/apis/Meeting/index.ts
+++ src/apis/Meeting/index.ts
@@ -1,7 +1,11 @@
-import { APICreateMeetingReqeust, APIEntireMeetingList } from '@/types/meeting';
-import { publicApi } from '../core/axios';
-import { APIMeetingGroup } from '@/types/meeting';
-import { APIMeetingDetail } from '@/types/meetingDetail';
+import {
+  APICreateMeetingReqeust,
+  APIEntireMeetingList,
+  APIMeetingGroup,
+} from "@/types/meeting";
+import { APIMeetingDetailCommentsList } from "@/types/meetingDetailCommentsList";
+import { APIMeetingDetail } from "@/types/meetingDetail";
+import { publicApi } from "../core/axios";
 
 const MeetingAPI = {
   getEntireMeetingList: () =>
@@ -9,19 +13,34 @@ const MeetingAPI = {
       params: {
         pageSize: 10,
         groupCursorId: 999,
-        sortDirection: 'DESC',
+        sortDirection: "DESC",
       },
     }),
 
   createMeeting: ({ meeting }: { meeting: APICreateMeetingReqeust }) =>
-    publicApi.post('/api/book-groups', meeting),
+    publicApi.post("/api/book-groups", meeting),
 
   getMeetingDetailInfo: ({
     bookGroupId,
   }: {
-    bookGroupId: APIMeetingGroup['bookGroupId'];
+    bookGroupId: APIMeetingGroup["bookGroupId"];
   }) => publicApi.get<APIMeetingDetail>(`/api/book-groups/${bookGroupId}`),
 
+  getMeetingDetailCommentsList: ({
+    bookGroupId,
+  }: {
+    bookGroupId: APIMeetingGroup["bookGroupId"];
+  }) =>
+    publicApi.get<APIMeetingDetailCommentsList>(
+      `/api/book-groups/${bookGroupId}/comments`,
+      {
+        params: {
+          groupCommentCursorId: 100,
+          pageSize: 10,
+          sortDirection: "DESC",
+        },
+      }
+    ),
 };
 
 export default MeetingAPI;
--- src/queries/meeting/useMeetingInfoQuery/index.tsx
+++ src/queries/meeting/useMeetingInfoQuery/index.tsx
@@ -2,16 +2,14 @@ import MeetingAPI from '@/apis/Meeting';
 import { useQuery } from '@tanstack/react-query';
 import { APIMeetingDetail } from '@/types/meetingDetail';
 
-const useMeetingDetailInfoQuery = ({
+const useMeetingInfoQuery = ({
   bookGroupId,
 }: {
   bookGroupId: APIMeetingDetail['bookGroupId'];
 }) => {
   return useQuery(['meetingDetailInfo', bookGroupId], () =>
-    MeetingAPI.getMeetingDetailInfo({ bookGroupId }).then(
-      ({data}) => data
-    )
+    MeetingAPI.getMeetingDetailInfo({ bookGroupId }).then(({ data }) => data)
   );
 };
 
-export default useMeetingDetailInfoQuery;
+export default useMeetingInfoQuery;
