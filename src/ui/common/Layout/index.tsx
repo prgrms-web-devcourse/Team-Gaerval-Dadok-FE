26b07fd21635faf98963adb3ce3629d5af618a1e
minjongbaek <minjongbaek@gmail.com>
2023-03-08 17:07:32 +0900

    style: height가 viewport를 넘지 않도록 수정

--- src/ui/common/Layout/index.tsx
+++ src/ui/common/Layout/index.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react';
 import { Box } from '@chakra-ui/react';
+// import BottomNavigation from '@/ui/BottomNavigation';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <Box px="2rem" pt="2rem" mb="9rem">
-      {children}
+    <Box h="100vh">
+      <Box px="2rem" pt="2rem" pb="9rem" h="100%" overflow="auto">
+        {children}
+      </Box>
     </Box>
   );
 };
