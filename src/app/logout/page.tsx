58989f3f5ad069291e5b40ec0fd8d03f380d1620
gxxrxn <rbfks1221@naver.com>
2023-03-10 15:05:21 +0900

    feat: logout page 구현

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useEffect } from 'react';
+import { redirect } from 'next/navigation';
+
+import { useAuth } from '@/hooks/auth';
+
+const Logout = () => {
+  const { removeAuth } = useAuth();
+
+  useEffect(() => {
+    removeAuth();
+    redirect('/login');
+  });
+};
+
+export default Logout;
