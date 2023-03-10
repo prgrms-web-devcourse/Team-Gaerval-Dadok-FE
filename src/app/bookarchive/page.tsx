a960a96a69da87705f4e1fe0ee355c9be1cc642a
hanyugeon <hanyugeon@naver.com>
2023-03-11 03:31:02 +0900

    feat: 비로그인 회원 북카이브 책장추천 api 연동 및 컴포넌트 분리

--- src/app/bookarchive/page.tsx
+++ src/app/bookarchive/page.tsx
@@ -1,47 +1,52 @@
 'use client';
 
+import { useAuth } from '@/hooks/auth';
+import { BookArchiveForUnAuth } from '@/ui/BookArchive';
 import { RecommendedBooks, RecommendedBookshelf } from '@/ui/Recommended';
 import { Flex, VStack } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 export default function BookArchive() {
-  /**
-   * TODO
-   * 로그인 했는지 안했는지 구분하기
-   * API 연결하기
-   */
+  const { isAuthed } = useAuth();
+
+  const [isLogined, setIsLogined] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLogined(isAuthed);
+  }, [isAuthed]);
 
   return (
     <VStack as="main" width="100%" spacing="2rem">
-      <Flex direction="column" width="100%" gap="3rem">
-        <RecommendedBookshelf
-          bookshelfId={RECOMMEND_BOOKSHELF[0].bookshelfId}
-          bookshelfName={RECOMMEND_BOOKSHELF[0].bookshelfName}
-          books={RECOMMEND_BOOKSHELF[0].books}
-        />
-        <RecommendedBookshelf
-          bookshelfId={RECOMMEND_BOOKSHELF[1].bookshelfId}
-          bookshelfName={RECOMMEND_BOOKSHELF[1].bookshelfName}
-          books={RECOMMEND_BOOKSHELF[1].books}
-        />
-      </Flex>
-
-      <RecommendedBooks
-        jobGroup={RECOMMEND_BOOKS.jobGroupKoreanName}
-        books={RECOMMEND_BOOKS.books}
-      />
-
-      <Flex direction="column" width="100%" gap="3rem">
-        <RecommendedBookshelf
-          bookshelfId={RECOMMEND_BOOKSHELF[1].bookshelfId}
-          bookshelfName={RECOMMEND_BOOKSHELF[0].bookshelfName}
-          books={RECOMMEND_BOOKSHELF[0].books}
-        />
-        <RecommendedBookshelf
-          bookshelfId={RECOMMEND_BOOKSHELF[1].bookshelfId}
-          bookshelfName={RECOMMEND_BOOKSHELF[1].bookshelfName}
-          books={RECOMMEND_BOOKSHELF[1].books}
-        />
-      </Flex>
+      {isLogined ? (
+        <Flex direction="column" width="100%" gap="3rem">
+          <RecommendedBookshelf
+            bookshelfId={RECOMMEND_BOOKSHELF[0].bookshelfId}
+            bookshelfName={RECOMMEND_BOOKSHELF[0].bookshelfName}
+            books={RECOMMEND_BOOKSHELF[0].books}
+          />
+          <RecommendedBookshelf
+            bookshelfId={RECOMMEND_BOOKSHELF[1].bookshelfId}
+            bookshelfName={RECOMMEND_BOOKSHELF[1].bookshelfName}
+            books={RECOMMEND_BOOKSHELF[1].books}
+          />
+          <RecommendedBooks
+            jobGroup={RECOMMEND_BOOKS.jobGroupKoreanName}
+            books={RECOMMEND_BOOKS.books}
+          />
+          <RecommendedBookshelf
+            bookshelfId={RECOMMEND_BOOKSHELF[1].bookshelfId}
+            bookshelfName={RECOMMEND_BOOKSHELF[0].bookshelfName}
+            books={RECOMMEND_BOOKSHELF[0].books}
+          />
+          <RecommendedBookshelf
+            bookshelfId={RECOMMEND_BOOKSHELF[1].bookshelfId}
+            bookshelfName={RECOMMEND_BOOKSHELF[1].bookshelfName}
+            books={RECOMMEND_BOOKSHELF[1].books}
+          />
+        </Flex>
+      ) : (
+        <BookArchiveForUnAuth />
+      )}
     </VStack>
   );
 }
@@ -57,8 +62,7 @@ const RECOMMEND_BOOKS = {
   books: [
     {
       bookId: 1,
-      imageUrl:
-        'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+      imageUrl: 'https://image.yes24.com/goods/92742567/m',
       title: 'jpa',
       author: '김영한',
       isbn: '123456789',
@@ -71,7 +75,7 @@ const RECOMMEND_BOOKS = {
     {
       bookId: 2,
       imageUrl:
-        'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+        'https://image.aladin.co.kr/product/20481/95/cover200/k662635453_1.jpg',
       title: '리액트',
       author: '김민준',
       isbn: '123456789',
@@ -84,7 +88,7 @@ const RECOMMEND_BOOKS = {
     {
       bookId: 3,
       imageUrl:
-        'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+        'https://image.aladin.co.kr/product/29234/92/coveroff/k682837293_1.jpg',
       title: 'jpa',
       author: '김영한',
       isbn: '123456789',
@@ -97,7 +101,7 @@ const RECOMMEND_BOOKS = {
     {
       bookId: 4,
       imageUrl:
-        'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+        'https://image.aladin.co.kr/product/23618/61/cover200/k932638523_1.jpg',
       title: '리액트',
       author: '김민준',
       isbn: '123456789',
@@ -110,7 +114,7 @@ const RECOMMEND_BOOKS = {
     {
       bookId: 5,
       imageUrl:
-        'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+        'https://image.aladin.co.kr/product/21856/89/cover200/8965402603_2.jpg',
       title: 'jpa',
       author: '김영한',
       isbn: '123456789',
@@ -123,7 +127,7 @@ const RECOMMEND_BOOKS = {
     {
       bookId: 6,
       imageUrl:
-        'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+        'https://image.aladin.co.kr/product/20481/95/cover200/k662635453_1.jpg',
       title: '리액트',
       author: '김민준',
       isbn: '123456789',
@@ -136,7 +140,7 @@ const RECOMMEND_BOOKS = {
     {
       bookId: 7,
       imageUrl:
-        'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+        'https://image.aladin.co.kr/product/23618/61/cover200/k932638523_1.jpg',
       title: '리팩터링',
       author: '마틴 파울러',
       isbn: '123456789',
@@ -149,7 +153,7 @@ const RECOMMEND_BOOKS = {
     {
       bookId: 99,
       imageUrl:
-        'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+        'https://image.aladin.co.kr/product/29234/92/coveroff/k682837293_1.jpg',
       title: '스프링부트',
       author: '이동욱',
       isbn: '123456789',
@@ -171,25 +175,25 @@ const RECOMMEND_BOOKSHELF = [
         bookId: 1,
         title: '해리포터1',
         imageUrl:
-          'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+          'https://image.aladin.co.kr/product/21856/89/cover200/8965402603_2.jpg',
       },
       {
         bookId: 2,
         title: '해리포터2',
         imageUrl:
-          'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+          'https://image.aladin.co.kr/product/29234/92/coveroff/k682837293_1.jpg',
       },
       {
         bookId: 3,
         title: '해리포터1',
         imageUrl:
-          'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+          'https://image.aladin.co.kr/product/23618/61/cover200/k932638523_1.jpg',
       },
       {
         bookId: 4,
         title: '해리포터2',
         imageUrl:
-          'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+          'https://image.aladin.co.kr/product/20481/95/cover200/k662635453_1.jpg',
       },
     ],
   },
@@ -201,25 +205,24 @@ const RECOMMEND_BOOKSHELF = [
         bookId: 1,
         title: '해리포터1',
         imageUrl:
-          'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+          'https://image.aladin.co.kr/product/20481/95/cover200/k662635453_1.jpg',
       },
       {
         bookId: 2,
         title: '해리포터2',
-        imageUrl:
-          'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+        imageUrl: 'https://image.yes24.com/goods/92742567/m',
       },
       {
         bookId: 3,
         title: '해리포터1',
         imageUrl:
-          'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+          'https://image.aladin.co.kr/product/29234/92/coveroff/k682837293_1.jpg',
       },
       {
         bookId: 4,
         title: '해리포터2',
         imageUrl:
-          'https://image.aladin.co.kr/product/31180/30/cover200/8954691676_1.jpg',
+          'https://image.aladin.co.kr/product/23618/61/cover200/k932638523_1.jpg',
       },
     ],
   },
