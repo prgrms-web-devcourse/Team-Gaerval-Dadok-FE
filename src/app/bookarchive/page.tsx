7ab81fd87bec4c7ed4b8643a91439e967e0781ce
hanyugeon <hanyugeon@naver.com>
2023-03-11 18:09:42 +0900

    chore: 코드리뷰 반영(불필요한 isLogined useState 제거)

--- src/app/bookarchive/page.tsx
+++ src/app/bookarchive/page.tsx
@@ -3,21 +3,14 @@
 import { useAuth } from '@/hooks/auth';
 import { BookArchiveForUnAuth } from '@/ui/BookArchive';
 import { VStack } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
 
 export default function BookArchive() {
   const { isAuthed } = useAuth();
 
-  const [isLogined, setIsLogined] = useState<boolean>(false);
-
-  useEffect(() => {
-    setIsLogined(isAuthed);
-  }, [isAuthed]);
-
   return (
     <VStack as="main" width="100%" spacing="2rem">
-      {isLogined ? (
-        // TODO: 로그인한 유저 책장 추천 api 연결하기.
+      {isAuthed ? (
+        // 'TODO: 로그인한 유저 책장 추천 api 연결하기.'
         'TODO: 로그인한 유저 책장 추천 api 연결하기.'
       ) : (
         <BookArchiveForUnAuth />
