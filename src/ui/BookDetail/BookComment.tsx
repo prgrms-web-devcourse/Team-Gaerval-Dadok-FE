7b872ec4fa9c102281b8efb870ede7861185be56
kyuran kim | fry <57716832+gxxrxn@users.noreply.github.com>
2023-03-13 02:00:46 +0900

    [#199] 책 상세 페이지 > 코멘트 삭제 기능 구현 (#203)
    * feat: 코멘트 삭제 기능 구현
    
    * chore: 불필요한 로그 삭제

--- src/ui/BookDetail/BookComment.tsx
+++ src/ui/BookDetail/BookComment.tsx
@@ -32,7 +32,7 @@ interface Props
   style?: CSSProperties;
   editable?: boolean;
   onEdit?: (commentId: number, comment: string) => void;
-  onDelete?: () => void;
+  onDelete?: (commentId: number) => void;
 }
 
 const BookComment = ({
@@ -73,8 +73,7 @@ const BookComment = ({
   };
 
   const handleCommentDelete = () => {
-    /** @todo 삭제 api 연결 */
-    onDelete && onDelete();
+    onDelete && onDelete(commentId);
     onDeleteModalClose();
   };
 
