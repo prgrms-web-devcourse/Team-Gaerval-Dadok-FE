114127b9d8094c6e633715ed003d695ab65fae45
kyuran kim | fry <57716832+gxxrxn@users.noreply.github.com>
2023-03-13 19:00:03 +0900

    [#205] 책 상세 페이지 > 책장에 책 꽂은 사용자 보여주는 api 연결 (#216)

--- src/queries/book/useBookUserInfoQuery.ts
+++ src/queries/book/useBookUserInfoQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import bookAPI from '@/apis/book';
 
 const useBookUserInfoQuery = (bookId: number) =>
-  useQuery(['bookDetail', bookId], () =>
+  useQuery(['bookUserInfo', bookId], () =>
     bookAPI.getBookUserInfo(bookId).then(({ data }) => data)
   );
 
