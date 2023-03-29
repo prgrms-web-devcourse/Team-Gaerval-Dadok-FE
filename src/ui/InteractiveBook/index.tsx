151a8ca437a04c9507d03a53da37768f750c1167
minjongbaek <minjongbaek@gmail.com>
2023-03-30 02:56:56 +0900

    refector: 책 사이즈와 관련된 상수 제거

--- src/ui/InteractiveBook/index.tsx
+++ src/ui/InteractiveBook/index.tsx
@@ -3,15 +3,11 @@ import { Box, Flex, Image } from '@chakra-ui/react';
 import { usePalette } from 'color-thief-react';
 import Link from 'next/link';
 
-const BOOK_WIDTH = '8.5rem';
-const BOOK_HEIGHT = '11rem';
-const BOOK_COVER_WIDTH = '3rem';
-
 const InteractiveBook = ({
   imageUrl,
   bookId,
 }: Pick<APIBook, 'imageUrl' | 'bookId'>) => {
-  const { data } = usePalette(
+  const { data: colors } = usePalette(
     '/api/book/image?' + new URLSearchParams({ url: imageUrl }),
     2,
     'hex'
@@ -19,12 +15,12 @@ const InteractiveBook = ({
 
   return (
     <Flex style={{ perspective: '200px' }} justify="center" flexGrow={1}>
-      {data && (
+      {colors && (
         <Box
           as={Link}
           href={`/book/${bookId}`}
-          w={BOOK_WIDTH}
-          h={BOOK_HEIGHT}
+          w="8.5rem"
+          h="11rem"
           transform="rotateY(30deg) translateX(1rem)"
           boxSizing="border-box"
           style={{
@@ -33,15 +29,14 @@ const InteractiveBook = ({
           }}
         >
           <Box
-            w={BOOK_WIDTH}
+            w="100%"
             h="1rem"
             pos="absolute"
             bottom={0}
-            transform="translateZ(-3rem)"
+            transform={`translateZ(-3rem)`}
             boxShadow="1px -4px 20px 3px"
           ></Box>
           <Image
-            width="4rem"
             src={imageUrl}
             pos="absolute"
             alt="book cover"
@@ -49,9 +44,9 @@ const InteractiveBook = ({
             h="100%"
           />
           <Box
-            w={BOOK_COVER_WIDTH}
+            w="3rem"
             h="100%"
-            bgColor={data[0]}
+            bgColor={colors[0]}
             transform="rotateY(-90deg) translateX(-1.49rem) translateZ(1.5rem)"
           ></Box>
         </Box>
