85755961b284ac513420f4afa38b0ce9fd446a68
minjongbaek <minjongbaek@gmail.com>
2023-04-11 00:29:08 +0900

    feat: APISearchedBookPagination 타입 추가

--- src/types/book.ts
+++ src/types/book.ts
@@ -21,6 +21,15 @@ export interface APISearchedBook
   apiProvider: string;
 }
 
+export interface APISearchedBookPagination {
+  searchBookResponseList: APISearchedBook[];
+  requestedPageNumber: number;
+  requestedPageSize: 10;
+  isLast: true;
+  pageableCount: number;
+  totalCount: number;
+}
+
 export interface APIBookDetail extends APIBook {
   apiProvider: string;
   imageKey: string;
