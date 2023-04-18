ecb137a73064b6db4946b37b76f5911b68bf2e62
minjongbaek <minjongbaek@gmail.com>
2023-04-18 19:14:14 +0900

    feat: QueryErrorBounaryFallback 컴포넌트 작성

--- src/ui/Profile/ProfileInfo/index.tsx
+++ src/ui/Profile/ProfileInfo/index.tsx
@@ -1,7 +1,11 @@
 import { ReactNode, Suspense } from 'react';
 import MyProfileContainer from './MyProfileInfoContainer';
 import UserProfileInfoContainer from './UserProfileInfoContainer';
+import { Skeleton, SkeletonCircle, VStack } from '@chakra-ui/react';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
 import type { APIUser } from '@/types/user';
+import QueryErrorBounaryFallback from '@/ui/common/QueryErrorBoundaryFallback';
 
 type ProfileInfoProps = { children?: ReactNode } & (
   | { isMe: true; userId?: undefined }
@@ -10,15 +14,38 @@ type ProfileInfoProps = { children?: ReactNode } & (
 
 const ProfileInfo = ({ isMe, userId, children }: ProfileInfoProps) => {
   return (
-    <Suspense fallback={<>Loading...</>}>
-      {isMe ? (
-        <MyProfileContainer />
-      ) : (
-        <UserProfileInfoContainer userId={userId} />
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          onReset={reset}
+          fallbackRender={({ resetErrorBoundary }) => (
+            <QueryErrorBounaryFallback
+              minH="12rem"
+              resetErrorBoundary={resetErrorBoundary}
+            />
+          )}
+        >
+          <Suspense fallback={<ProfileInfoSkelenton />}>
+            {isMe ? (
+              <MyProfileContainer />
+            ) : (
+              <UserProfileInfoContainer userId={userId} />
+            )}
+            {children && children}
+          </Suspense>
+        </ErrorBoundary>
       )}
-      {children && children}
-    </Suspense>
+    </QueryErrorResetBoundary>
   );
 };
 
 export default ProfileInfo;
+
+const ProfileInfoSkelenton = () => {
+  return (
+    <VStack gap="1rem" align="stretch" w="100%">
+      <SkeletonCircle size="8rem" />
+      <Skeleton w="80%" height="3rem" />
+    </VStack>
+  );
+};
