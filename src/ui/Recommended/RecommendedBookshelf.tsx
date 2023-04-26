8f5410b47c6ddd0b2cbd7a70fac62205f6de065a
hanyugeon <hanyugeon@naver.com>
2023-04-26 17:22:44 +0900

    feat: 좋아요 버튼 페이지에 추가

--- src/ui/Recommended/RecommendedBookshelf.tsx
+++ src/ui/Recommended/RecommendedBookshelf.tsx
@@ -1,8 +1,9 @@
 import { APIBookshelf } from '@/types/bookshelf';
 import { Flex, Text, VStack } from '@chakra-ui/react';
 import Link from 'next/link';
-import IconButton from '../common/IconButton';
-import InteractiveBookShelf from '../InteractiveBookShelf';
+import IconButton from '@/ui/common/IconButton';
+import LikeButton from '@/ui/common/LikeButton';
+import InteractiveBookShelf from '@/ui/InteractiveBookShelf';
 
 const RecommendedBookshelf = ({
   bookshelfId,
@@ -11,17 +12,25 @@ const RecommendedBookshelf = ({
 }: APIBookshelf) => {
   return (
     <VStack align="flex-start" spacing="2rem">
-      <Flex
-        as={Link}
-        href={`/bookshelf/${bookshelfId}`}
-        align="center"
-        w="100%"
-        justify="space-between"
-      >
-        <Text fontSize="md" fontWeight="bold">
+      <Flex align="center" w="100%" justify="space-between">
+        <Text
+          as={Link}
+          href={`/bookshelf/${bookshelfId}`}
+          fontSize="md"
+          fontWeight="bold"
+        >
           {`${bookshelfName}`}
         </Text>
-        <IconButton name="more-circle" size="1.6rem" fill />
+        <Flex h="2.4rem" gap="1.6rem" align="center">
+          <LikeButton />
+          <IconButton
+            as={Link}
+            href={`/bookshelf/${bookshelfId}`}
+            name="more-circle"
+            size="1.6rem"
+            fill
+          />
+        </Flex>
       </Flex>
       <InteractiveBookShelf books={books} />
     </VStack>
