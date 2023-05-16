5290b1accd142cf548de5cfc7d9afcd7e8cb8b3d
hanyugeon <hanyugeon@naver.com>
2023-05-17 00:35:59 +0900

    refactor: 좋아요 버튼, 카운트 분리

--- src/ui/common/LikeButton/index.tsx
+++ src/ui/common/LikeButton/index.tsx
@@ -1,16 +1,14 @@
 import { APIBookshelfInfo } from '@/types/bookshelf';
 import IconButton from '@/ui/common/IconButton';
-import { Box, Flex, Text, useTheme } from '@chakra-ui/react';
+import { Button, Flex, Text, useTheme } from '@chakra-ui/react';
 
-interface LikeButtonTypes {
-  isButton?: boolean;
-  handleLikeButton?: () => void;
-  isLiked?: APIBookshelfInfo['isLiked'];
+type LikeButtonTypes = {
+  handleLikeButton: () => void;
+  isLiked: APIBookshelfInfo['isLiked'];
   likeCount: APIBookshelfInfo['likeCount'];
-}
+};
 
 const LikeButton = ({
-  isButton = true,
   handleLikeButton,
   isLiked,
   likeCount,
@@ -18,26 +16,23 @@ const LikeButton = ({
   const theme = useTheme();
 
   return (
-    <Box
-      as={isButton ? 'button' : 'div'}
-      onClick={isButton ? handleLikeButton : undefined}
-      p={isButton ? '0.6rem 0.8rem' : '0.4rem 0.6rem'}
+    <Button
+      onClick={handleLikeButton}
+      p="0.6rem 0.8rem"
       w="5.4rem"
       h="2.4rem"
       fontSize="xs"
       color={isLiked ? theme.colors.white['800'] : theme.colors.red['800']}
       bg={isLiked ? theme.colors.red['800'] : theme.colors.white['800']}
       border="solid 0.1rem"
-      borderColor={
-        isButton ? theme.colors.red['800'] : theme.colors.white['800']
-      }
+      borderColor={theme.colors.red['800']}
       borderRadius="1.6rem"
     >
       <Flex w="100%" h="100%" gap="0.2rem" justify="flex-start" align="center">
         <IconButton
           as="div"
           name="like"
-          size={isButton ? '1.3rem' : '1.5rem'}
+          size="1.3rem"
           strokeWidth={'0.05rem'}
           color={isLiked ? theme.colors.white['800'] : theme.colors.red['800']}
           fill={true}
@@ -46,7 +41,7 @@ const LikeButton = ({
           {likeCount ? likeCount : 0}
         </Text>
       </Flex>
-    </Box>
+    </Button>
   );
 };
 
