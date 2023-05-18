3bc9e2eb0aaf85fe2c047a81ad0f1ec29d0c7321
gxxrxn <rbfks1221@naver.com>
2023-05-18 22:48:52 +0900

    fix: 기간 만료조건 수정

--- src/utils/helpers/date.ts
+++ src/utils/helpers/date.ts
@@ -2,7 +2,7 @@ const isDateExpired = (date: string) => {
   const today = new Date();
   const targetDate = new Date(date);
 
-  return targetDate.valueOf() <= today.valueOf();
+  return targetDate.valueOf() < today.valueOf();
 };
 
 export { isDateExpired };
