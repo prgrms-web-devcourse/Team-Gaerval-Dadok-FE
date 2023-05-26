a9c9301efe7a7d4bb384985d95d2167d09d06e2b
minjongbaek <minjongbaek@gmail.com>
2023-05-26 11:47:57 +0900

    refactor: 로그인 리다이렉트 페이지에서 프로필 조회 쿼리 제거

--- src/pages/login/redirect/index.tsx
+++ src/pages/login/redirect/index.tsx
@@ -1,47 +1,41 @@
 import { Flex, Spinner } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-
-import useMyProfileQuery from '@/queries/user/useMyProfileQuery';
-import { isAuthed, setAuth } from '@/utils/helpers';
+import { useCallback, useEffect } from 'react';
+import { setAuth } from '@/utils/helpers';
+import userAPI from '@/apis/user';
 
 const RedirectPage = () => {
   const router = useRouter();
 
   const { access_token: accessToken } = router.query as {
-    access_token: string;
+    access_token?: string;
   };
-  const { isSuccess, data, refetch } = useMyProfileQuery({
-    enabled: isAuthed(),
-  });
 
-  useEffect(() => {
-    const isAuthed = !!accessToken;
-
-    if (isAuthed) {
-      accessToken && setAuth(accessToken);
-      refetch();
-    }
-  }, [accessToken, refetch]);
-
-  useEffect(() => {
-    if (!isSuccess) {
-      return;
-    }
-
-    const {
-      job: { jobName, jobGroupName },
-      nickname,
-    } = data;
-    const isSavedAdditioanlInfo = !!(nickname && jobGroupName && jobName);
+  const checkSavedAdditioanlInfo = useCallback(async () => {
+    const isSavedAdditioanlInfo = await userAPI.getMyProfile().then(
+      ({
+        data: {
+          job: { jobName, jobGroupName },
+          nickname,
+        },
+      }) => !!(nickname && jobGroupName && jobName)
+    );
 
     if (!isSavedAdditioanlInfo) {
       router.replace('/profile/me/add');
-      return;
     }
 
     router.replace('/bookarchive');
-  }, [data, isSuccess, router]);
+  }, [router]);
+
+  useEffect(() => {
+    const hasAccessToken = !!accessToken;
+
+    if (hasAccessToken) {
+      accessToken && setAuth(accessToken);
+      checkSavedAdditioanlInfo();
+    }
+  }, [accessToken, checkSavedAdditioanlInfo]);
 
   return (
     <Flex align="center" justify="center" height="95vh">
