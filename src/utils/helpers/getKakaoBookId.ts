c7a14f2939861960e4dbfb2da4616ca6504458b5
minjongbaek <minjongbaek@gmail.com>
2023-06-02 23:48:07 +0900

    refactor: 카카오 이미지 CORS를 위한 함수, next 설정 제거

--- src/utils/helpers/getKakaoBookId.ts
+++
@@ -1,8 +0,0 @@
-const getKakaoBookId = (kakaoImageUrl: string) => {
-  const kakaoUrl = new URL(
-    new URL(kakaoImageUrl).searchParams.get('fname') || ''
-  ).pathname;
-  return kakaoUrl.replace('/lbook/image/', '');
-};
-
-export default getKakaoBookId;
