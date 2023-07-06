cf17b84f7df5e9df7a93668721c5793fa714089f
minjongbaek <minjongbaek@gmail.com>
2023-07-06 21:49:24 +0900

    style: next/Head -> head 태그 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,11 @@ import { PropsWithChildren } from 'react';
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html>
-      {/* <Head>
+      <head>
         <title>다독다독</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" href="/favicon.ico" />
-      </Head> */}
+      </head>
       <body>
         <Global styles={LineSeed.className} />
         <RecoilRoot>
