dca52172dda712bcec5c5030a7325b498f5eca7e
minjongbaek <minjongbaek@gmail.com>
2023-07-10 15:45:30 +0900

    chore: storybook, chromatic 패키지 추가

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
+
+export default preview;
