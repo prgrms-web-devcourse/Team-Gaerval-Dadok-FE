7ad8aefd608670a7c7571aff98e81ab2188ed260
minjong <48359052+minjongbaek@users.noreply.github.com>
2023-07-10 22:57:39 +0900

    [#370] Storybook & Headless UI 추가 (#371)
    * chore: storybook, chromatic 패키지 추가
    
    * feat: 스토리북 프리뷰 확인용 workflow 작성
    
    * chore: @babel/core 등 몇몇 종속성 패키지 추가
    
    * chore: Headless UI 추가
    
    * chore: Tailwind CSS 추가
    
    * setting: tailwind 설정
    
    * Update tailwind.config.js
    
    Co-authored-by: harry kim <73218463+hanyugeon@users.noreply.github.com>
    
    * feat: PR에 새로운 커밋 발생하면 새로 코멘트를 생성하지 않고 수정
    
    ---------
    
    Co-authored-by: harry kim <73218463+hanyugeon@users.noreply.github.com>

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  refs: {
+    '@chakra-ui/react': {
+      disable: true,
+    },
+  },
+};
+export default config;
