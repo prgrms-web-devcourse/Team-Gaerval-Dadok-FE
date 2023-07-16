94a608e7459d52e1f6d455738e1ff0cc6169eb02
DaeHyun_Woo <po668312@naver.com>
2023-07-17 05:49:02 +0900

    fix: TopHeader 컴포넌트 패딩 값 재설정

--- src/ui/Base/TopHeader.tsx
+++ src/ui/Base/TopHeader.tsx
@@ -7,7 +7,7 @@ interface TopHeaderProps {
 
 const TopHeader = ({ label, children }: TopHeaderProps) => {
   return (
-    <div className="flex items-center justify-between px-5 pb-2 pt-5">
+    <div className="flex items-center justify-between px-[2rem] pb-[0.8rem] pt-[2rem]">
       <p className="text-xl font-bold text-main-900">{label}</p>
       {children}
     </div>
