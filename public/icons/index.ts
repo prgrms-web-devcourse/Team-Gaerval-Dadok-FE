2a62a7120b17a8ec060df75394a96388a57c5826
hanyugeon <hanyugeon@naver.com>
2023-07-19 19:54:20 +0900

    feat: 새로운 아이콘 추가 및 default export 적용

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,4 +1,33 @@
-export { default as IconLeftArrow } from './left-arrow.svg';
-export { default as IconShare } from './share.svg';
+// 상단 Nav & Header
+export { default as IconArrowLeft } from './arrow-left.svg';
+export { default as IconClose } from './close.svg';
 export { default as IconPost } from './post.svg';
-export { default as IconOptions } from './options.svg';
+export { default as IconHamburger } from './hamburger.svg';
+export { default as IconShare } from './share.svg';
+
+// 하단 Nav
+export { default as IconBookarchive } from './bookarchive.svg';
+export { default as IconDiscover } from './discover.svg';
+export { default as IconGroup } from './group.svg';
+export { default as IconProfile } from './profile.svg';
+
+// 책 & 책장
+export { default as IconArrowRight } from './arrow-right.svg';
+export { default as IconBookmark } from './bookmark.svg';
+export { default as IconHeart } from './heart.svg';
+
+// 검색
+export { default as IconSearch } from './search.svg';
+export { default as IconWarningCircle } from './warning-circle.svg';
+export { default as IconCheckCircle } from './check-circle.svg';
+
+// 독서 모임
+export { default as IconAvatar } from './avatar.svg';
+export { default as IconCalendar } from './calendar.svg';
+export { default as IconCheck } from './check.svg';
+export { default as IconComments } from './comments.svg';
+export { default as IconDelete } from './delete.svg';
+export { default as IconMembers } from './members.svg';
+
+// 카카오
+export { default as IconKakao } from './kakao.svg';
