6692f4fa3ab6553608e5a4b1aee36a64a7a935fe
minjongbaek <minjongbaek@gmail.com>
2023-07-20 17:47:59 +0900

    feat: Select 컴포넌트 에러 스타일링

--- src/stories/Base/Select.stories.tsx
+++ src/stories/Base/Select.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import Select from '@/ui/Base/Select';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import Button from '@/ui/Base/Button';
 
 const meta: Meta<typeof Select> = {
   title: 'Base/Select',
@@ -7,9 +9,70 @@ const meta: Meta<typeof Select> = {
   tags: ['autodocs'],
 };
 
-const numbers = [1, 2, 3, 4, 5] as const;
+const numbers = [1, 2, 3] as const;
 
 export default meta;
+
+type DefaultValues = {
+  requiredNumber: number;
+  number: number;
+};
+
+const SelectWithUseForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DefaultValues>({
+    mode: 'all',
+  });
+
+  const handleSubmitForm: SubmitHandler<DefaultValues> = ({
+    requiredNumber,
+    number,
+  }) => {
+    alert(`requiredNumber: ${requiredNumber}, number: ${number}`);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(handleSubmitForm)}
+      className="flex w-[43rem] flex-col gap-[1.6rem]"
+    >
+      <Select
+        placeholder="숫자를 선택해주세요. (필수)"
+        {...register('requiredNumber', {
+          required: '필수 항목입니다.',
+        })}
+        error={errors.requiredNumber}
+      >
+        {numbers.map(number => (
+          <Select.Option key={number} value={number}>
+            {number}
+          </Select.Option>
+        ))}
+      </Select>
+      <Select
+        placeholder="숫자를 선택해주세요."
+        {...register('number')}
+        error={errors.number}
+      >
+        {numbers.map(number => (
+          <Select.Option key={number} value={number}>
+            {number}
+          </Select.Option>
+        ))}
+      </Select>
+      <Button
+        size="large"
+        type="submit"
+        onClick={handleSubmit(handleSubmitForm)}
+      >
+        Submit
+      </Button>
+    </form>
+  );
+};
 
 type Story = StoryObj<typeof Select>;
 
@@ -18,12 +81,22 @@ export const Default: Story = {
     placeholder: '입력해 주세요.',
   },
   render: args => (
-    <Select {...args}>
-      {numbers.map(number => (
-        <Select.Option key={number} value={number}>
-          {number}
-        </Select.Option>
-      ))}
-    </Select>
+    <div className="min-h-[20rem]">
+      <Select {...args}>
+        {numbers.map(number => (
+          <Select.Option key={number} value={number}>
+            {number}
+          </Select.Option>
+        ))}
+      </Select>
+    </div>
+  ),
+};
+
+export const WithUseForm: Story = {
+  render: () => (
+    <div className="min-h-[20rem]">
+      <SelectWithUseForm />
+    </div>
   ),
 };
--- src/ui/Base/Select.tsx
+++ src/ui/Base/Select.tsx
@@ -1,42 +1,68 @@
-import { forwardRef, PropsWithChildren, ReactNode, Ref } from 'react';
+import {
+  ChangeEvent,
+  FocusEvent,
+  forwardRef,
+  Fragment,
+  PropsWithChildren,
+  ReactNode,
+  Ref,
+} from 'react';
 import { Listbox } from '@headlessui/react';
 import SelectIcon from '@public/icons/select-icon.svg';
-
-type Error = {
-  message: string;
-};
+import { FieldError } from 'react-hook-form';
 
 interface SelectProps {
+  name?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
   placeholder?: string;
-  error?: Error;
+  error?: FieldError;
 }
 
 const Select = (
-  { placeholder = '', children }: PropsWithChildren<SelectProps>,
+  {
+    placeholder = '',
+    error,
+    children,
+    ...props
+  }: PropsWithChildren<SelectProps>,
   ref: Ref<HTMLElement>
 ) => {
+  const borderColor = error
+    ? 'border-warning-800'
+    : 'border-black-400 data-[headlessui-state=open]:border-main-900';
+
   return (
-    <Listbox ref={ref}>
-      <Listbox.Button className="relative flex w-full items-center rounded-[0.5rem] border-[0.05rem] border-black-400 px-[1rem] py-[1.3rem] text-sm data-[headlessui-state=open]:border-main-900">
-        {({ value }) => (
-          <>
-            {value ? (
-              <div className="min-h-[2.1rem] w-full pr-[2.35rem] text-left">
-                {value}
-              </div>
-            ) : (
-              <div className="min-h-[2.1rem] w-full pr-[2.35rem] text-left text-placeholder">
-                {placeholder}
-              </div>
-            )}
-            <SelectIcon className="absolute right-[1.35rem]" />
-          </>
+    <div className="flex flex-col gap-[0.5rem] text-sm">
+      <Listbox as="div" {...props} ref={ref} className="relative">
+        <Listbox.Button as={Fragment}>
+          {({ value }) => (
+            <button
+              className={`relative flex w-full items-center rounded-[0.5rem] border-[0.05rem] px-[1rem] py-[1.3rem] ${borderColor}`}
+            >
+              {value ? (
+                <div className="min-h-[2.1rem] w-full pr-[2.35rem] text-left">
+                  {value}
+                </div>
+              ) : (
+                <div className="min-h-[2.1rem] w-full pr-[2.35rem] text-left text-placeholder">
+                  {placeholder}
+                </div>
+              )}
+              <SelectIcon className="absolute right-[1.35rem]" />
+            </button>
+          )}
+        </Listbox.Button>
+        {children && (
+          <div className="absolute z-10 w-full">
+            <Listbox.Options className=" my-[0.5rem] rounded-[0.5rem] bg-white shadow-md">
+              {children}
+            </Listbox.Options>
+          </div>
         )}
-      </Listbox.Button>
-      {children && (
-        <Listbox.Options className="mt-[1rem]">{children}</Listbox.Options>
-      )}
-    </Listbox>
+      </Listbox>
+      {error && <div className="text-warning-800">{error.message}</div>}
+    </div>
   );
 };
 
@@ -49,7 +75,7 @@ const Option = ({ value, children }: OptionProps) => {
   return (
     <Listbox.Option
       value={value}
-      className="cursor-pointer rounded-[0.5rem] px-[1rem] py-[1.3rem] text-sm hover:bg-main-900 hover:text-white"
+      className="cursor-pointer rounded-[0.5rem] px-[1rem] py-[1.3rem] hover:bg-main-900 hover:font-bold hover:text-white"
     >
       {children}
     </Listbox.Option>
