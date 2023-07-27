7c189bc81c5f6cd5e620903f3690c0520baf14d8
minjong <48359052+minjongbaek@users.noreply.github.com>
2023-07-27 22:27:35 +0900

    [#383] Select 컴포넌트 (#385)
    * feat: Select 컴포넌트 마크업
    
    * feat: Select 컴포넌트 에러 스타일링
    
    * refactor: 기본 select 태그 사용하도록 수정
    
    * setting: 스토리북 staticDir 설정
    
    * refactor: SelectProps 타입 수정
    
    * Update src/ui/Base/Select.tsx
    
    Co-authored-by: kyuran kim <57716832+gxxrxn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: kyuran kim <57716832+gxxrxn@users.noreply.github.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,12 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        postCss: true,
-      },
-    },
+    '@storybook/addon-styling',
   ],
   framework: {
     name: '@storybook/nextjs',
