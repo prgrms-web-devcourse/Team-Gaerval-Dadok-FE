583fb372335c0c2de6ee6cab052eb75fa87c7c93
hanyugeon <hanyugeon@naver.com>
2023-08-09 00:33:38 +0900

    refactor: NavigationItem 컴포넌트 코드이동 및 삭제

--- src/ui/Base/BottomNavigation.tsx
+++ src/ui/Base/BottomNavigation.tsx
@@ -6,8 +6,8 @@ import {
   IconGroup,
   IconProfile,
 } from '@public/icons';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import BottomNavigationItem from './BottomNavigationItem';
 
 const BASE_ICON_CLASSES = 'h-[2.6rem] w-[2.6rem] text-placeholder';
 
@@ -37,16 +37,25 @@ const icons = [
 const BottomNavigation = () => {
   const pathname = usePathname();
 
+  const getIconColorClasses = (href: string) => {
+    return href === pathname
+      ? 'fill-main-900 text-main-900'
+      : 'fill-placeholder text-placeholder';
+  };
+
   return (
     <div className="border-top-[0.05rem] fixed bottom-0 flex h-[6.4rem] w-full max-w-[39.3rem] justify-between border-black-200 bg-white px-[2.6rem] pb-[1.2rem] pt-[0.8rem]">
       {icons.map(({ icon, label, href }) => (
-        <BottomNavigationItem
-          key={label}
-          icon={icon}
-          label={label}
-          href={href}
-          isActive={pathname === href}
-        />
+        <Link key={label} type="button" href={href}>
+          <div
+            className={`flex h-[4.4rem] w-[4.6rem] flex-col items-center justify-center text-xs font-bold ${getIconColorClasses(
+              href
+            )}`}
+          >
+            {icon}
+            {label}
+          </div>
+        </Link>
       ))}
     </div>
   );
