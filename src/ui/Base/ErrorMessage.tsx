84682da696e82ec0a5a11eabc7d621a0c906a4cd
minjongbaek <minjongbaek@gmail.com>
2023-08-10 11:12:56 +0900

    style: 아이콘에 직접 크기를 주지 않고 컨테이너를 통해 크기가 정해지도록 수정

--- src/ui/Base/ErrorMessage.tsx
+++ src/ui/Base/ErrorMessage.tsx
@@ -4,7 +4,9 @@ import { ReactNode } from 'react';
 const ErrorMessage = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex items-center gap-[0.4rem] text-xs text-warning-800">
-      <IconWarningCircle />
+      <div className="h-[1.2rem] w-[1.2rem]">
+        <IconWarningCircle />
+      </div>
       {children}
     </div>
   );
