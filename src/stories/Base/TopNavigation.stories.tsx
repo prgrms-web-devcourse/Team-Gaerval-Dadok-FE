e3f4e8452579622c69b88e0acedba446ece50d40
minjongbaek <minjongbaek@gmail.com>
2023-08-13 17:52:09 +0900

    refactor: TopNavigation 컴포넌트는 레이아웃만 제공하는 형태로 수정

--- src/stories/Base/TopNavigation.stories.tsx
+++ src/stories/Base/TopNavigation.stories.tsx
@@ -1,6 +1,12 @@
 import { Meta, StoryObj } from '@storybook/react';
 import TopNavigation from '@/ui/Base/TopNavigation';
-import { IconPost, IconShare } from '@public/icons';
+import {
+  IconPost,
+  IconShare,
+  IconArrowLeft,
+  IconHamburger,
+  IconClose,
+} from '@public/icons';
 
 const meta: Meta<typeof TopNavigation> = {
   title: 'Base/TopNavigation',
@@ -13,26 +19,97 @@ export default meta;
 type Story = StoryObj<typeof TopNavigation>;
 
 export const Default: Story = {
-  args: {
-    title: 'Refactoring 2nd(리팩터링 2판)',
-  },
-  render: args => <TopNavigation {...args} />,
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.LeftItem>
+        <IconArrowLeft />
+      </TopNavigation.LeftItem>
+      <TopNavigation.CenterItem>
+        Refactoring 2nd(리팩터링 2판)
+      </TopNavigation.CenterItem>
+      <TopNavigation.RightItem>
+        <IconShare />
+      </TopNavigation.RightItem>
+    </TopNavigation>
+  ),
 };
 
-export const BookshelfPage: Story = {
-  args: {
-    titleAlign: 'center',
-    children: <IconShare />,
-  },
-  render: args => <TopNavigation {...args} />,
+export const OnlySide: Story = {
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.LeftItem>
+        <IconArrowLeft />
+      </TopNavigation.LeftItem>
+      <TopNavigation.RightItem>
+        <IconShare />
+      </TopNavigation.RightItem>
+    </TopNavigation>
+  ),
 };
 
-export const GroupPage: Story = {
-  args: {
-    titleAlign: 'left',
-    title: '프롱이 리팩터링 스터디',
-    isOwner: true,
-    children: <IconPost />,
-  },
-  render: args => <TopNavigation {...args} />,
+export const OnlyCenter: Story = {
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.CenterItem textAlign="left">
+        Refactoring 2nd(리팩터링 2판)
+      </TopNavigation.CenterItem>
+    </TopNavigation>
+  ),
+};
+
+export const CenterWithLeft: Story = {
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.LeftItem>
+        <IconArrowLeft />
+      </TopNavigation.LeftItem>
+      <TopNavigation.CenterItem textAlign="left">
+        Refactoring 2nd(리팩터링 2판)
+      </TopNavigation.CenterItem>
+    </TopNavigation>
+  ),
+};
+
+export const CenterWithRight: Story = {
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.CenterItem>
+        Refactoring 2nd(리팩터링 2판)
+      </TopNavigation.CenterItem>
+      <TopNavigation.RightItem>
+        <IconShare />
+      </TopNavigation.RightItem>
+    </TopNavigation>
+  ),
+};
+
+export const TextLeft: Story = {
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.LeftItem>
+        <IconArrowLeft />
+      </TopNavigation.LeftItem>
+      <TopNavigation.CenterItem textAlign="left">
+        프롱이 리팩터링 스터디
+      </TopNavigation.CenterItem>
+      <TopNavigation.RightItem>
+        <IconPost />
+        <IconHamburger />
+      </TopNavigation.RightItem>
+    </TopNavigation>
+  ),
+};
+
+export const PostWrite: Story = {
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.LeftItem>
+        <IconClose />
+      </TopNavigation.LeftItem>
+      <TopNavigation.CenterItem>글 작성하기</TopNavigation.CenterItem>
+      <TopNavigation.RightItem>
+        <button className="font-bold text-main-900">완료</button>
+      </TopNavigation.RightItem>
+    </TopNavigation>
+  ),
 };
