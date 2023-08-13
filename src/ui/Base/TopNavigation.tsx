eb005da06f2f955b1574df5ea08be4eb3e5f4d7a
minjongbaek <minjongbaek@gmail.com>
2023-08-13 23:42:56 +0900

    style: TopNavigation 컴포넌트 높이 지정

--- src/ui/Base/TopNavigation.tsx
+++ src/ui/Base/TopNavigation.tsx
@@ -8,7 +8,7 @@ type ItemProps = TopNavigationProps;
 
 const TopNavigation = ({ children }: TopNavigationProps) => {
   return (
-    <div className="relative flex w-full items-center justify-center bg-opacity-0 px-[2rem] py-[1.7rem] text-md">
+    <div className="relative flex h-[2.4rem] w-full items-center justify-center bg-opacity-0 px-[2rem] py-[1.7rem] text-md">
       {children}
     </div>
   );
