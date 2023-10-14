1910972f87b6dd27b7b7fd8528e44faf91321e3b
DaeHyun_Woo <po668312@naver.com>
2023-10-15 01:45:59 +0900

    feat: FloatingButton base 컴포넌트 생성

---
+++ src/stories/Base/FloatingButton.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react';
+import FloatingButton from '@/ui/Base/FloatingButton';
+
+const meta: Meta<typeof FloatingButton> = {
+  title: 'Base/FloatingButton',
+  component: FloatingButton,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FloatingButton>;
+
+export const Default: Story = {
+  render: FloatingButton,
+};
