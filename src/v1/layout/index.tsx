1de0f7143bddc0a3a267a530a01e8285b4994883
hanyugeon <hanyugeon@naver.com>
2023-10-19 19:45:59 +0900

    chore: 변수명 수정

--- src/v1/layout/index.tsx
+++ src/v1/layout/index.tsx
@@ -17,20 +17,17 @@ type LayoutProps = {
 
 const rootPaths = ['/bookarchive', '/book/search', '/group', '/profile/me'];
 
-const BASE_LAYOUT_CLASS =
-  'h-screen w-full max-w-[43rem] px-[2rem] animate-page-transition overflow-auto';
-
 const Layout = ({ children }: LayoutProps) => {
   const pathname = usePathname();
   const isRootPath = pathname && rootPaths.includes(pathname);
 
-  const DYNAMIC_LAYOUT_CLASS = isRootPath
-    ? 'pb-[6.4rem] pt-[2rem]'
-    : 'py-[2rem]';
+  const dynamicClass = isRootPath ? 'pb-[6.4rem] pt-[2rem]' : 'py-[2rem]';
 
   return (
     <>
-      <div className={`${BASE_LAYOUT_CLASS} ${DYNAMIC_LAYOUT_CLASS}`}>
+      <div
+        className={`h-screen w-full max-w-[43rem] animate-page-transition overflow-auto px-[2rem] ${dynamicClass}`}
+      >
         {isRootPath && <TopHeader pathname={pathname} />}
         {children}
       </div>
