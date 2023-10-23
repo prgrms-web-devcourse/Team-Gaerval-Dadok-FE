4ad39a03e4eee12f3d525915ebfec300280a1e93
gxxrxn <rbfks1221@naver.com>
2023-10-23 18:39:31 +0900

    refactor: avatar size 수정 및 svgr wrapper 태그 제거
    - 3.5rem -> 3.2rem 으로 통일

--- src/ui/bookgroup/detail/BookGroupInfo.tsx
+++ src/ui/bookgroup/detail/BookGroupInfo.tsx
@@ -141,11 +141,18 @@ const Owner = ({
     <div className="flex items-center gap-[1rem]">
       {/** FIXME: Avatar 컴포넌트로 변경 */}
       <span
-        className={`relative h-[3.5rem] w-[3.5rem] rounded-full ${
+        className={`relative h-[3.2rem] w-[3.2rem] rounded-full ${
           profileImageSrc ? 'bg-white' : 'bg-black-400'
         }`}
       >
-        {profileImageSrc && <Image alt={name} src={profileImageSrc} fill />}
+        {profileImageSrc && (
+          <Image
+            alt={name}
+            src={profileImageSrc}
+            fill
+            className="rounded-full"
+          />
+        )}
       </span>
       <span className="text-center text-sm font-bold">
         {name} {isMe && ' 👑'}
@@ -168,7 +175,7 @@ const BookInfoCard = ({
   author: string;
 }) => {
   return (
-    <div className="flex min-h-[10rem] w-full gap-[2.4rem] rounded-[0.5rem] border-[0.05rem] border-cancel px-[2.2rem] py-[1.8rem]">
+    <div className="flex min-h-[10rem] w-full cursor-pointer gap-[2.4rem] rounded-[0.5rem] border-[0.05rem] border-cancel px-[2.2rem] py-[1.8rem]">
       <span className="relative h-[8.7rem] w-[6.4rem]">
         {bookImageSrc && (
           <Image
@@ -181,12 +188,10 @@ const BookInfoCard = ({
       </span>
       <div className="flex flex-grow flex-col">
         <span className="text-sm font-bold">{title}</span>
-        <span className="text-2xs text-placeholder">{author}</span>
+        <span className="text-xs text-placeholder">{author}</span>
       </div>
       {/** 왼쪽 방향의 화살표를 180도 회전하여 사용 */}
-      <span className="h-[1.5rem] w-[1.5rem] rotate-180">
-        <IconArrowLeft />
-      </span>
+      <IconArrowLeft className="h-[1.5rem] w-[1.5rem] rotate-180" />
     </div>
   );
 };
@@ -194,9 +199,7 @@ const BookInfoCard = ({
 const Duration = ({ start, end }: { start: string; end: string }) => {
   return (
     <div className="flex items-center gap-[1rem]">
-      <span className="h-auto w-[1.6rem] fill-placeholder">
-        <IconCalendar />
-      </span>
+      <IconCalendar className="h-auto w-[1.6rem] fill-placeholder" />
       <span className="text-sm text-placeholder">
         {start} - {end}
       </span>
@@ -207,9 +210,7 @@ const Duration = ({ start, end }: { start: string; end: string }) => {
 const MemberCapacity = ({ current, max }: { current: number; max: number }) => {
   return (
     <div className="flex items-center gap-[1rem]">
-      <span className="h-auto w-[1.6rem] fill-placeholder">
-        <IconMembers />
-      </span>
+      <IconMembers className="h-auto w-[1.6rem] fill-placeholder" />
       <span className="text-sm text-placeholder">
         <span className="text-main-900">{current}</span>
         {` / ${max}명`}
