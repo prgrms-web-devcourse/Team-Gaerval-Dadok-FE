a9362aa6a5e962fa6e456632a8290fbf94f04ff5
Daehyun <po668312@naver.com>
2023-11-05 21:08:31 +0900

    [#411][독서 모임] Detail 독서 모임 컴포넌트 (#428)
    * feat: 모임 목록 페이지 모임 상세 컴포넌트 구현
    
    * fix: 컴포넌트명 변경
    
    * fix: bookGroup 컴포넌트명 변경

---
+++ src/stories/bookGroup/DetailBookGroupCard.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import DetailBookGroupCard from '@/v1/bookgroup/DetailBookGroupCard';
+
+const meta: Meta<typeof DetailBookGroupCard> = {
+  title: 'bookgroup/DetailBookGroupCard',
+  component: DetailBookGroupCard,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DetailBookGroupCard>;
+
+export const Default: Story = {
+  args: {
+    title: '프롱이 리팩터링 스터디',
+    description:
+      '제1차 프롱이 기수연합 독서 스터디 입니다. 마틴 파울러의 저서 ‘리팩터링 2판’과 함께 진행합니다.',
+    book: {
+      title: '리팩터링 2판',
+      bookImageSrc: 'https://image.yes24.com/goods/89649360/XL',
+    },
+    date: {
+      start: '2023-10-31',
+      end: '2023-11-27',
+    },
+    memberCount: 3,
+    owner: {
+      name: '소피아',
+      profileImageSrc: '',
+    },
+    isPublic: false,
+    commentCount: 12,
+    handleClick: () => {
+      alert('모임 상세 페이지로 이동');
+    },
+  },
+};
