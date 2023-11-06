08c919d173e9997f5e5e2c94d6ea47e2798f2555
Shaw <minjongbaek@gmail.com>
2023-11-06 22:30:35 +0900

    [#410] [북카이브] 북카이브 페이지 (비회원) (#425)
    * feat: 책장 컴포넌트 작성
    
    * feat: 책장에 있는 책 컴포넌트 작성
    
    * feat: 책장 컴포넌트 스토리 작성
    
    * fix: 책장 컴포넌트에서 CLS 현상이 발생하지 않도록 수정
    
    * feat: 책장에 있는 책 그림자 추가
    
    * fix: remove icon wrapper element
    
    * feat: Add a bookshelf shortcut icon
    
    * feat: '인기 책장' 문구 추가
    
    * fix: 책장 local image 사용하도록 수정
    
    * Update src/ui/BookArchive/BookArchiveForUnAuth.tsx
    
    Co-authored-by: kyuran kim <57716832+gxxrxn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: gxxrxn <rbfks1221@naver.com>
    Co-authored-by: kyuran kim <57716832+gxxrxn@users.noreply.github.com>

---
+++ src/stories/Base/Bookshelf/Bookshelf.stories.tsx
@@ -0,0 +1,43 @@
+import Bookshelf from '@/ui/Bookshelf';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Bookshelf> = {
+  title: 'Bookshelf/Bookshelf',
+  component: Bookshelf,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Bookshelf>;
+
+// TODO: 스토리북에서 이미지를 불러올 때 발생하는 CORS 이슈 해결
+export const Default: Story = {
+  args: {
+    bookshelfId: 9,
+    bookshelfName: '백민종님의 책장',
+    books: [
+      {
+        bookId: 3,
+        title: '리액트를 다루는 기술',
+        imageUrl: '/images/book-cover/book1.jpeg',
+      },
+      {
+        bookId: 11,
+        title: '모던 자바스크립트 Deep Dive',
+        imageUrl: '/images/book-cover/book2.jpeg',
+      },
+      {
+        bookId: 22,
+        title: '이펙티브 타입스크립트',
+        imageUrl: '/images/book-cover/book3.jpeg',
+      },
+      {
+        bookId: 23,
+        title: '리팩터링 2판',
+        imageUrl: '/images/book-cover/book4.jpeg',
+      },
+    ],
+    likeCount: 3,
+  },
+};
