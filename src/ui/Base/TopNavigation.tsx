b0eb6553ad08f2bb35168d82e240a8cd083ded5b
gxxrxn <rbfks1221@naver.com>
2023-11-12 02:16:47 +0900

    style(TopNaviation): CenterItem px 수정

--- src/ui/Base/TopNavigation.tsx
+++ src/ui/Base/TopNavigation.tsx
@@ -32,7 +32,7 @@ const textAligns = {
 const CenterItem = ({ children, textAlign = 'center' }: CenterItemProps) => {
   const alignClassName = textAligns[textAlign];
   return (
-    <div className={`w-full px-[3.5rem] ${alignClassName}`}>{children}</div>
+    <div className={`w-full px-[1.5rem] ${alignClassName}`}>{children}</div>
   );
 };
 
