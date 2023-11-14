bd2b45e9a4c3612293dacd22b5b480945bb6d15f
hanyugeon <hanyugeon@naver.com>
2023-11-14 16:54:38 +0900

    refactor: InputLength 컴포넌트 props 수정

--- src/ui/Base/InputLength.tsx
+++ src/ui/Base/InputLength.tsx
@@ -1,31 +1,15 @@
-import {
-  FieldPath,
-  FieldValues,
-  UseControllerProps,
-  useWatch,
-} from 'react-hook-form';
-
-type InputLengthProps<
-  TFieldValues extends FieldValues = FieldValues,
-  TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
-> = Pick<UseControllerProps<TFieldValues, TFieldName>, 'control' | 'name'> & {
-  minLength: number;
+type InputLengthProps = {
+  currentValue: string;
+  isError: boolean;
   maxLength: number;
 };
 
-const InputLength = <T extends FieldValues>({
-  control,
-  name,
-  minLength,
+const InputLength = ({
+  currentValue = '',
+  isError,
   maxLength,
-}: InputLengthProps<T>) => {
-  const targetInput = useWatch({
-    control,
-    name,
-  });
-
-  const currentLength = targetInput ? targetInput.length : 0;
-  const isError = currentLength < minLength || currentLength > maxLength;
+}: InputLengthProps) => {
+  const currentLength = currentValue ? currentValue.length : 0;
   const textColor = isError ? 'text-warning-800 ' : 'text-main-900';
 
   return (
