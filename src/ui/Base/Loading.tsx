8527f8e86cbfd54080a40db322f47b929acb8956
gxxrxn <rbfks1221@naver.com>
2023-11-24 04:43:47 +0900

    feat: Loading 컴포넌트 작성

---
+++ src/ui/Base/Loading.tsx
@@ -0,0 +1,55 @@
+import Portal from './Portal';
+
+const schemes = {
+  main: 'bg-main-900',
+  grey: 'bg-black-400',
+} as const;
+
+type DotScheme = keyof typeof schemes;
+
+interface LoadingProps {
+  color?: DotScheme;
+  fullpage?: boolean;
+}
+
+const Loading = ({ color = 'main', fullpage = false }: LoadingProps) => {
+  if (fullpage) {
+    return (
+      <Portal id="loading">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <LoadingDots color={color} />
+        </div>
+      </Portal>
+    );
+  }
+
+  return <LoadingDots color={color} />;
+};
+
+export default Loading;
+
+const LoadingDots = ({ color }: { color: DotScheme }) => (
+  <div className="flex gap-[1rem]">
+    <LoadingDot color={color} animationStep={0} />
+    <LoadingDot color={color} animationStep={1} />
+    <LoadingDot color={color} animationStep={2} />
+  </div>
+);
+
+const animations = {
+  0: 'animate-dot-flash',
+  1: 'animate-dot-flash-delay-0.5',
+  2: 'animate-dot-flash-delay-1',
+} as const;
+
+export const LoadingDot = ({
+  color,
+  animationStep = 0,
+}: {
+  color: DotScheme;
+  animationStep?: keyof typeof animations;
+}) => (
+  <span
+    className={`h-[1rem] w-[1rem] rounded-full ${schemes[color]} ${animations[animationStep]}`}
+  ></span>
+);
