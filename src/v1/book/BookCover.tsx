45342c8f8df25d8dfa38d3149d85ea3118281288
gxxrxn <rbfks1221@naver.com>
2023-11-24 15:54:48 +0900

    fix: next/image sizes 관련 경고 해결

--- src/v1/book/BookCover.tsx
+++ src/v1/book/BookCover.tsx
@@ -18,31 +18,31 @@ type BookCoverProps = Required<
   size?: BookCoverSize;
 };
 
-const getCoverSizeClasses = (size: BookCoverSize) => {
+const getCoverSize = (size: BookCoverSize) => {
   switch (size) {
     case 'xsmall': {
-      return 'w-[6.5rem] h-[9.1rem]';
+      return ['w-[6.5rem] h-[9.1rem]', { width: 65, height: 91 }] as const;
     }
     case 'small': {
-      return 'w-[7.0rem] h-[9.8rem]';
+      return ['w-[7.0rem] h-[9.8rem]', { width: 70, height: 98 }] as const;
     }
     case 'medium': {
-      return 'w-[7.5rem] h-[10.5rem]';
+      return ['w-[7.5rem] h-[10.5rem]', { width: 75, height: 105 }] as const;
     }
     case 'large': {
-      return 'w-[9.0rem] h-[12.6rem]';
+      return ['w-[9.0rem] h-[12.6rem]', { width: 90, height: 126 }] as const;
     }
     case 'xlarge': {
-      return 'w-[11.0rem] h-[15.4rem]';
+      return ['w-[11.0rem] h-[15.4rem]', { width: 110, height: 154 }] as const;
     }
     case '2xlarge': {
-      return 'w-[18.0rem] h-[25.2rem]';
+      return ['w-[18.0rem] h-[25.2rem]', { width: 180, height: 252 }] as const;
     }
   }
 };
 
 const BookCover = ({ src, title, size = 'medium' }: BookCoverProps) => {
-  const sizeClasses = getCoverSizeClasses(size);
+  const [sizeClasses, sizeProps] = getCoverSize(size);
 
   return (
     <div className={`relative flex-shrink-0 ${sizeClasses}`}>
@@ -51,8 +51,8 @@ const BookCover = ({ src, title, size = 'medium' }: BookCoverProps) => {
         alt={title || 'book-cover'}
         placeholder="blur"
         blurDataURL={DATA_URL['placeholder']}
-        className="object-fit rounded-[0.5rem] shadow-bookcover"
-        fill
+        className={`object-fit h-full w-full rounded-[0.5rem] shadow-bookcover`}
+        {...sizeProps}
       />
     </div>
   );
