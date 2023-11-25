9214c5a65614dbde40b9af481c1bad519d4ac649
gxxrxn <rbfks1221@naver.com>
2023-11-26 03:49:54 +0900

    fix: 비공개 모임인 경우 댓글 보이지 않게 수정

--- src/v1/bookGroup/detail/CommentList.tsx
+++ src/v1/bookGroup/detail/CommentList.tsx
@@ -3,38 +3,51 @@ import Avatar from '@/ui/Base/Avatar';
 import { useBookGroupComments } from '@/queries/group/useBookGroupCommentsQuery';
 import { useMyProfileId } from '@/queries/user/useMyProfileQuery';
 import { isAuthed } from '@/utils/helpers';
+import { useBookGroup } from '@/queries/group/useBookGroupQuery';
 
 const CommentList = ({ groupId }: { groupId: number }) => {
+  const { data: bookGroupInfo } = useBookGroup(groupId);
   const { data: comments } = useBookGroupComments(groupId);
   const { data: myId } = useMyProfileId({ enabled: isAuthed() });
+  const { isPublic } = bookGroupInfo;
+
+  if (!isPublic) {
+    return (
+      <p className="py-[2rem] text-center text-sm">
+        {`멤버만 볼 수 있어요 🥲`}
+      </p>
+    );
+  }
+
+  if (comments.length === 0) {
+    return (
+      <p className="self-center whitespace-pre-line py-[2rem] text-center text-sm">
+        {`아직 게시글이 없어요.
+          가장 먼저 게시글을 남겨보세요!`}
+      </p>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-[1rem]">
-      {comments.length ? (
-        comments.map(({ id, writer, createdAt, content }) => (
-          <div className="flex flex-col gap-[1.7rem] pt-[1rem]" key={id}>
-            <div className="flex gap-[1rem]">
-              <Avatar
-                src={writer.profileImageSrc}
-                name={writer.name}
-                size="medium"
-              />
-              <div className="flex flex-grow flex-col">
-                <Name name={writer.name} />
-                <Date date={createdAt} />
-              </div>
-              {writer.id === myId && <MenuButton />}
+      {comments.map(({ id, writer, createdAt, content }) => (
+        <div className="flex flex-col gap-[1.7rem] pt-[1rem]" key={id}>
+          <div className="flex gap-[1rem]">
+            <Avatar
+              src={writer.profileImageSrc}
+              name={writer.name}
+              size="medium"
+            />
+            <div className="flex flex-grow flex-col">
+              <Name name={writer.name} />
+              <Date date={createdAt} />
             </div>
-            <Comment content={content} />
-            <Divider />
+            {writer.id === myId && <MenuButton />}
           </div>
-        ))
-      ) : (
-        <p className="self-center whitespace-pre-line py-[2rem] text-center text-sm">
-          {`아직 게시글이 없어요.
-          가장 먼저 게시글을 남겨보세요!`}
-        </p>
-      )}
+          <Comment content={content} />
+          <Divider />
+        </div>
+      ))}
     </div>
   );
 };
