b5394340cd423252f4b7327226b1d503a7b15e74
hanyugeon <hanyugeon@naver.com>
2023-12-08 20:03:34 +0900

    feat: BottomNavigation에 BottomSheet 이벤트 등록

--- src/v1/base/BottomNavigation.tsx
+++ src/v1/base/BottomNavigation.tsx
@@ -7,6 +7,11 @@ import {
   IconProfile,
 } from '@public/icons';
 
+import useDisclosure from '@/hooks/useDisclosure';
+import { isAuthed } from '@/utils/helpers';
+
+import LoginBottomSheet from '@/v1/profile/LoginBottomSheet';
+
 type BottomNavigationProps = {
   pathname: string;
 };
@@ -40,21 +45,40 @@ const iconColor = {
 } as const;
 
 const BottomNavigation = ({ pathname }: BottomNavigationProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleClickProfileIcon = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    if (isAuthed()) return;
+
+    onOpen();
+    event.preventDefault();
+  };
+
   return (
-    <div className="absolute bottom-0 left-0 flex h-[6.4rem] w-full max-w-[43rem] justify-between border-t-[0.05rem] border-black-200 bg-white px-[2.6rem] pb-[1.2rem] pt-[0.8rem]">
-      {icons.map(({ icon, label, href }) => (
-        <Link key={label} type="button" href={href}>
-          <div
-            className={`flex h-[4.4rem] w-[4.6rem] flex-col items-center justify-center text-xs font-bold ${
-              href === pathname ? iconColor.active : iconColor.inactive
-            }`}
-          >
-            <div className="h-[2.6rem] w-[2.6rem] text-placeholder">{icon}</div>
-            {label}
-          </div>
-        </Link>
-      ))}
-    </div>
+    <>
+      <nav className="absolute bottom-0 left-0 flex h-[6.4rem] w-full max-w-[43rem] justify-between border-t-[0.05rem] border-black-200 bg-white px-[2.6rem] pb-[1.2rem] pt-[0.8rem]">
+        {icons.map(({ icon, label, href }) => (
+          <Link key={label} type="button" href={href}>
+            <button
+              className={`flex h-[4.4rem] w-[4.6rem] flex-col items-center justify-center text-xs font-bold ${
+                href === pathname ? iconColor.active : iconColor.inactive
+              }`}
+              onClick={
+                href === '/profile/me' ? handleClickProfileIcon : undefined
+              }
+            >
+              <div className="h-[2.6rem] w-[2.6rem] text-placeholder">
+                {icon}
+              </div>
+              {label}
+            </button>
+          </Link>
+        ))}
+      </nav>
+      <LoginBottomSheet isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
 
