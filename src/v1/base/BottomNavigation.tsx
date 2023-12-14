0b8d4643135034bd3c656d151270a03d84d70df1
hanyugeon <hanyugeon@naver.com>
2023-12-14 19:55:12 +0900

    fix: BottomNavigation 롤백 및 시맨틱 태그 적용

--- src/v1/base/BottomNavigation.tsx
+++ src/v1/base/BottomNavigation.tsx
@@ -7,11 +7,6 @@ import {
   IconProfile,
 } from '@public/icons';
 
-import useDisclosure from '@/hooks/useDisclosure';
-import { isAuthed } from '@/utils/helpers';
-
-import LoginBottomSheet from '@/v1/profile/LoginBottomSheet';
-
 type BottomNavigationProps = {
   pathname: string;
 };
@@ -45,39 +40,24 @@ const iconColor = {
 } as const;
 
 const BottomNavigation = ({ pathname }: BottomNavigationProps) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleClickProfileIcon = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    if (isAuthed()) return;
-
-    onOpen();
-    event.preventDefault();
-  };
-
   return (
     <>
       <nav className="absolute bottom-0 left-0 flex h-[6.4rem] w-full max-w-[43rem] justify-between border-t-[0.05rem] border-black-200 bg-white px-[2.6rem] pb-[1.2rem] pt-[0.8rem]">
         {icons.map(({ icon, label, href }) => (
           <Link key={label} type="button" href={href}>
             <button
-              className={`flex h-[4.4rem] w-[4.6rem] flex-col items-center justify-center text-xs font-bold ${
+              className={`flex h-[4.4rem] w-[4.6rem] flex-col items-center justify-center ${
                 href === pathname ? iconColor.active : iconColor.inactive
               }`}
-              onClick={
-                href === '/profile/me' ? handleClickProfileIcon : undefined
-              }
             >
               <div className="h-[2.6rem] w-[2.6rem] text-placeholder">
                 {icon}
               </div>
-              {label}
+              <p className="text-xs font-bold">{label}</p>
             </button>
           </Link>
         ))}
       </nav>
-      <LoginBottomSheet isOpen={isOpen} onClose={onClose} />
     </>
   );
 };
