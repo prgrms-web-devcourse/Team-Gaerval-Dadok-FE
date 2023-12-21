3ff30f51b9f22a21a813b85542654ada87b12dbc
hanyugeon <hanyugeon@naver.com>
2023-12-21 16:54:32 +0900

    feat: BookSearchInput 컴포넌트 마크업

---
+++ src/v1/bookSearch/BookSearchInput.tsx
@@ -0,0 +1,17 @@
+import { forwardRef } from 'react';
+
+import { IconSearch } from '@public/icons';
+
+const BookSearchInput = () => {
+  return (
+    <div className="align-center flex w-full gap-[2rem] border-b-[0.05rem] border-black-900 p-[1rem] focus-within:border-main-900">
+      <IconSearch className="fill-black h-[2.1rem] w-[2.1rem]" />
+      <input
+        className={`h-[1.6rem] w-full appearance-none text-sm font-normal focus:outline-none`}
+        placeholder="책 제목, 작가를 검색해보세요"
+      />
+    </div>
+  );
+};
+
+export default forwardRef(BookSearchInput);
