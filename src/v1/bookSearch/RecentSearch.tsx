34dcbcb458abd2081935a410eda6aa6031120ddd
hanyugeon <hanyugeon@naver.com>
2023-12-21 18:36:01 +0900

    feat: 최근 검색어 마크업

---
+++ src/v1/bookSearch/RecentSearch.tsx
@@ -0,0 +1,61 @@
+import Button from '@/v1/base/Button';
+
+const RecentSearch = () => {
+  return (
+    <section>
+      <h2 className="mb-[1.7rem] h-[2.4rem] text-lg">최근 검색어</h2>
+      <div className="relative flex h-[3.5rem] w-[calc(100%+2rem)] gap-[1rem] overflow-x-scroll whitespace-nowrap">
+        <Button
+          size="medium"
+          fill={true}
+          fullRadius={true}
+          colorScheme="main-light"
+        >
+          스즈메의 문단속
+        </Button>
+        <Button
+          size="medium"
+          fill={true}
+          fullRadius={true}
+          colorScheme="main-light"
+        >
+          히가시노 게이고
+        </Button>
+        <Button
+          size="medium"
+          fill={true}
+          fullRadius={true}
+          colorScheme="main-light"
+        >
+          세이노의 가르침
+        </Button>
+        <Button
+          size="medium"
+          fill={true}
+          fullRadius={true}
+          colorScheme="main-light"
+        >
+          김길규 개새끼
+        </Button>
+        <Button
+          size="medium"
+          fill={true}
+          fullRadius={true}
+          colorScheme="main-light"
+        >
+          김길규 개새끼
+        </Button>
+        <Button
+          size="medium"
+          fill={true}
+          fullRadius={true}
+          colorScheme="main-light"
+        >
+          김길규 개새끼
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default RecentSearch;
