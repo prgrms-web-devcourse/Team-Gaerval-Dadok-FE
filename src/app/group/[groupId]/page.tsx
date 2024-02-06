0b12a651aa954690ddcc6cb459b34b648205ea00
gxxrxn <rbfks1221@naver.com>
2024-02-07 02:28:54 +0900

    feat: BookGroupCommentList 구현 (contaier)

--- src/app/group/[groupId]/page.tsx
+++ src/app/group/[groupId]/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import Link from 'next/link';
+
+import { isAuthed } from '@/utils/helpers';
+import { KAKAO_LOGIN_URL } from '@/constants/url';
+
 import SSRSafeSuspense from '@/components/SSRSafeSuspense';
 import BookGroupInfo from '@/v1/bookGroup/detail/BookGroupInfo';
-import CommentList from '@/v1/bookGroup/detail/CommentList';
+import BookGroupCommentList from '@/v1/comment/BookGroupCommentList';
 import BookGroupNavigation from '@/v1/bookGroup/BookGroupNavigation';
 import JoinBookGroupButton from '@/v1/bookGroup/detail/JoinBookGroupButton';
 import BottomActionButton from '@/v1/base/BottomActionButton';
-import { isAuthed } from '@/utils/helpers';
-import { KAKAO_LOGIN_URL } from '@/constants/url';
 
 const DetailBookGroupPage = ({
   params: { groupId },
@@ -30,7 +32,7 @@ const DetailBookGroupPage = ({
           <Divider />
           <div className="flex flex-col gap-[1rem]">
             <Heading text="게시글" />
-            <CommentList groupId={groupId} />
+            <BookGroupCommentList groupId={groupId} />
           </div>
         </div>
         {isAuthed() ? (
---
+++ src/v1/comment/BookGroupCommentList.tsx
@@ -0,0 +1,28 @@
+import { useBookGroupComments } from '@/queries/group/useBookGroupCommentsQuery';
+import { useMyProfileId } from '@/queries/user/useMyProfileQuery';
+import { useBookGroup } from '@/queries/group/useBookGroupQuery';
+import { isAuthed } from '@/utils/helpers';
+
+import CommentList from './CommentList';
+
+const BookGroupCommentList = ({ groupId }: { groupId: number }) => {
+  const { data: bookGroupInfo } = useBookGroup(groupId);
+  const { data: comments } = useBookGroupComments(groupId);
+  const { data: myId } = useMyProfileId({ enabled: isAuthed() });
+  const { isPublic, isMember } = bookGroupInfo;
+
+  const hidden = !isPublic && !isMember;
+
+  return (
+    <CommentList
+      comments={comments}
+      editableComment={writerId => writerId === myId}
+      hidden={hidden}
+      hiddenText={`멤버만 볼 수 있어요 🥲`}
+      emptyText={`아직 게시글이 없어요.
+                  가장 먼저 게시글을 남겨보세요!`}
+    />
+  );
+};
+
+export default BookGroupCommentList;
