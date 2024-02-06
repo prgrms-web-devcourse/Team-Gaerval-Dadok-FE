fc63d7d025f9672ca799d57d509cb4ffe7a8fb14
gxxrxn <rbfks1221@naver.com>
2024-02-07 02:27:22 +0900

    feat: 책 코멘트 query에 suspense 적용
    - useBookComments 쿼리 추가

--- src/queries/book/useBookCommentsQuery.ts
+++ src/queries/book/useBookCommentsQuery.ts
@@ -1,19 +1,48 @@
-import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+import { UseQueryOptions } from '@tanstack/react-query';
 
+import type {
+  APIBook,
+  APIBookCommentPagination,
+  BookComment,
+} from '@/types/book';
 import bookAPI from '@/apis/book';
-import type { APIBook } from '@/types/book';
+import useQueryWithSuspense from '@/hooks/useQueryWithSuspense';
 
-const useBookCommentsQuery = (
+const useBookCommentsQuery = <TData = APIBookCommentPagination>(
   bookId: APIBook['bookId'],
-  options?: Pick<
-    UseQueryOptions<Awaited<ReturnType<typeof bookAPI.getComments>>['data']>,
-    'onSuccess' | 'onError'
+  options?: UseQueryOptions<
+    Awaited<ReturnType<typeof bookAPI.getComments>>['data'],
+    unknown,
+    TData
   >
 ) =>
-  useQuery(
+  useQueryWithSuspense(
     ['bookComments', bookId],
     () => bookAPI.getComments(bookId).then(({ data }) => data),
     options
   );
 
 export default useBookCommentsQuery;
+
+const transformBookCommentsData = ({
+  bookComments,
+}: APIBookCommentPagination) => {
+  return bookComments.map(
+    ({ contents, createdAt, commentId, userId, userProfileImage, nickname }) =>
+      ({
+        id: commentId,
+        writer: {
+          id: userId,
+          profileImageSrc: userProfileImage,
+          name: nickname,
+        },
+        createdAt,
+        content: contents,
+      } as BookComment)
+  );
+};
+
+export const useBookComments = (bookId: APIBook['bookId']) =>
+  useBookCommentsQuery(bookId, {
+    select: transformBookCommentsData,
+  });
