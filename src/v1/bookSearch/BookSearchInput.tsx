0ba2d84c05c4738e76f3d8430e0c792a0e6d33bb
hanyugeon <hanyugeon@naver.com>
2024-02-11 03:18:19 +0900

    fix: 검색 input, 최근 검색어 스타일 수정

--- src/v1/bookSearch/BookSearchInput.tsx
+++ src/v1/bookSearch/BookSearchInput.tsx
@@ -11,7 +11,7 @@ const BookSearchInput = ({ onChange, ...props }: BookSearchInputProps) => {
     <div className="flex w-full items-center gap-[2rem] border-b-[0.05rem] border-black-900 p-[1rem] focus-within:border-main-900">
       <IconSearch className="fill-black h-[2.1rem] w-[2.1rem]" />
       <input
-        className={`h-[1.6rem] w-full appearance-none text-sm font-normal focus:outline-none`}
+        className={`w-full appearance-none text-sm font-normal focus:outline-none`}
         placeholder="책 제목, 작가를 검색해보세요"
         onChange={onChange}
         {...props}
