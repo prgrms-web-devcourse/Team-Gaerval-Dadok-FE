3bfac3ba688104b6b7916c809435d06677f68390
hanyugeon <hanyugeon@naver.com>
2024-02-11 16:42:43 +0900

    fix: 최근 검색어 스크롤바로인해 납작해 보이던 이슈 해결

--- src/v1/bookSearch/RecentSearch.tsx
+++ src/v1/bookSearch/RecentSearch.tsx
@@ -15,7 +15,7 @@ const RecentSearch = ({
     <section className="flex flex-col gap-[1.7rem]">
       <h2 className="h-[2.4rem] text-lg">최근 검색어</h2>
       {recentSearches ? (
-        <div className="relative flex h-[4.5rem] w-[calc(100%+2rem)] gap-[1rem] overflow-x-scroll whitespace-nowrap pb-[1rem]">
+        <div className="relative flex w-[calc(100%+2rem)] gap-[1rem] overflow-x-scroll whitespace-nowrap pb-[1rem]">
           {recentSearches.map(value => (
             <Button
               key={`${value.keyword}-${value.modifiedAt}`}
