ed09aa4cacd086f15dd499ed6cbc2e96f44aa63d
hanyugeon <hanyugeon@naver.com>
2024-02-22 01:05:57 +0900

    fix: storybook build 에러 수정

--- src/stories/bookSearch/RecentSearch.stories.tsx
+++ src/stories/bookSearch/RecentSearch.stories.tsx
@@ -14,7 +14,7 @@ type Story = StoryObj<typeof RecentSearch>;
 export const Default: Story = {
   args: {
     recentSearches: undefined,
-    setInputSearchValue: () => alert('선택한 검색어 검색!'),
+    setSearchValue: () => alert('선택한 검색어 검색!'),
   },
 };
 
@@ -29,6 +29,6 @@ export const RecentSearches: Story = {
       { keyword: '풀어', modifiedAt: 'now' },
       { keyword: '어때', modifiedAt: 'now' },
     ],
-    setInputSearchValue: () => alert('선택한 검색어 검색!'),
+    setSearchValue: () => alert('선택한 검색어 검색!'),
   },
 };
