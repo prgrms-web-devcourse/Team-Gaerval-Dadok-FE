ba38c94d4235b05eae8388a6cd38e4fc95e561f2
hanyugeon <hanyugeon@naver.com>
2024-03-06 21:16:24 +0900

    refactor: 도서 검색 관련 useQuery에 쿼리키 팩토리 패턴 적용

--- src/queries/book/key.ts
+++ src/queries/book/key.ts
@@ -5,11 +5,14 @@ const bookKeys = {
   details: () => [...bookKeys.all, 'detail'] as const,
   detail: (bookId: APIBook['bookId']) =>
     [...bookKeys.details(), bookId] as const,
-  bestSeller: () => [...bookKeys.all, 'bestSeller'],
   bookmark: (bookId: APIBook['bookId']) =>
     [...bookKeys.detail(bookId), 'bookmark'] as const,
   comments: (bookId: APIBook['bookId']) =>
     [...bookKeys.detail(bookId), 'comments'] as const,
+  bookSearch: (query: string) =>
+    [...bookKeys.all, 'bookSearch', query] as const,
+  recentSearch: () => [...bookKeys.all, 'recentSearch'],
+  bestSeller: () => [...bookKeys.all, 'bestSeller'],
 };
 
 export default bookKeys;
--- src/queries/book/useRecentSearchesQuery.ts
+++ src/queries/book/useRecentSearchesQuery.ts
@@ -3,10 +3,11 @@ import type { QueryOptions } from '@/types/query';
 
 import bookAPI from '@/apis/book';
 import type { APIRecentSearches } from '@/types/book';
+import bookKeys from './key';
 
 const useRecentSearchesQuery = (options?: QueryOptions<APIRecentSearches>) =>
   useQueryWithSuspense(
-    ['recentSearches'],
+    bookKeys.recentSearch(),
     () => bookAPI.getRecentSearches().then(({ data }) => data),
     options
   );
