66e0b84e6dafaf8fe942ead6f440e728fc1320ae
hanyugeon <hanyugeon@naver.com>
2024-03-08 22:07:40 +0900

    refactor: BestSeller, RecentSearch 쿼리 함수의 options 타입 수정

--- src/queries/book/useBestSellersQuery.ts
+++ src/queries/book/useBestSellersQuery.ts
@@ -1,5 +1,6 @@
-import useQueryWithSuspense from '@/hooks/useQueryWithSuspense';
-import type { UseQueryOptions } from '@tanstack/react-query';
+import useQueryWithSuspense, {
+  type UseQueryOptionWithoutSuspense,
+} from '@/hooks/useQueryWithSuspense';
 
 import bookAPI from '@/apis/book';
 import type { APIBestSellerRes } from '@/types/book';
@@ -7,7 +8,7 @@ import type { APIBestSellerRes } from '@/types/book';
 import bookKeys from './key';
 
 const useBestSellersQuery = <TData = APIBestSellerRes>(
-  options?: UseQueryOptions<APIBestSellerRes, unknown, TData>
+  options?: UseQueryOptionWithoutSuspense<APIBestSellerRes, unknown, TData>
 ) =>
   useQueryWithSuspense(
     bookKeys.bestSeller(),
--- src/queries/book/useRecentSearchesQuery.ts
+++ src/queries/book/useRecentSearchesQuery.ts
@@ -1,5 +1,6 @@
-import useQueryWithSuspense from '@/hooks/useQueryWithSuspense';
-import type { UseQueryOptions } from '@tanstack/react-query';
+import useQueryWithSuspense, {
+  type UseQueryOptionWithoutSuspense,
+} from '@/hooks/useQueryWithSuspense';
 
 import bookAPI from '@/apis/book';
 import type { APIRecentSearches } from '@/types/book';
@@ -7,7 +8,7 @@ import type { APIRecentSearches } from '@/types/book';
 import bookKeys from './key';
 
 const useRecentSearchesQuery = <TData = APIRecentSearches>(
-  options?: UseQueryOptions<APIRecentSearches, unknown, TData>
+  options?: UseQueryOptionWithoutSuspense<APIRecentSearches, unknown, TData>
 ) =>
   useQueryWithSuspense(
     bookKeys.recentSearch(),
