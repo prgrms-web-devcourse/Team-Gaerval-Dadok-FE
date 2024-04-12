dcff9e4cf6ca7bfb46a4a6376beb8eef6b4bf839
hanyugeon <hanyugeon@naver.com>
2024-04-12 21:32:06 +0900

    feat: tailwind.config에 battomNav boxShadow 값 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -78,6 +78,7 @@ module.exports = {
           '0px 0px 2px rgba(0, 0, 0, 0.2), 2px 2px 6px rgba(0, 0, 0, 0.1)',
         bookcard: '0px 0px 7px 0px rgba(207, 207, 207, 0.5)',
         searchResultItem: '0px 0px 6px 1px rgba(114, 114, 114, 0.10);',
+        bottomNav: 'rgba(0, 0, 0, 0.05) 0px 0px 10px 1px',
       },
       keyframes: {
         'page-transition': {
