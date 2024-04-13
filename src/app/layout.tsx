ce7c363d97ceffb42042aff3b71c8ae6ddcaa8c9
harry kim <73218463+hanyugeon@users.noreply.github.com>
2024-04-13 18:50:48 +0900

    [#509] [레이아웃] BottomNavigation 버그 및 UI 수정 (#510)
    * feat: TailwindCSS에 h-app 프로퍼티 추가
    
    * refactor: Layout 컴포넌트 시멘틱 태그 적용 및 fallback height 추가
    
    * refactor: Layout 컴포넌트 padding값 수정 및 children props 옵셔널로 변경
    
    * refactor: Layout 컴포넌트를 layout.tsx로 이동
    
    * fix: BottomNavigation 컴포넌트 position을 fixed로 수정
    
    * feat: tailwind.config에 battomNav boxShadow 값 추가
    
    * fix: BottomNavigation UI 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import ContextProvider from '@/components/ContextProvider';
-import { ReactNode } from 'react';
+import Layout from '@/v1/layout/Layout';
 
 import { LineSeedKR } from '@/styles/font';
 import '@/styles/global.css';
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
       <head>
@@ -12,12 +12,11 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body className="app-layout">
-        <ContextProvider>
-          <main className={`${LineSeedKR.variable} font-lineseed`}>
-            {children}
-          </main>
-        </ContextProvider>
+      {/* @todo Chakra 제거시 app-layout 프로퍼티 제거. */}
+      <body className={`${LineSeedKR.variable} app-layout font-lineseed`}>
+        <Layout>
+          <ContextProvider>{children}</ContextProvider>
+        </Layout>
       </body>
     </html>
   );
