cd5c56977fd9fa01f9867b5a9b6cef398fc542d9
gxxrxn <rbfks1221@naver.com>
2024-04-14 03:12:22 +0900

    feat: Input 컴포넌트 style prop 추가

--- src/v1/base/Input.tsx
+++ src/v1/base/Input.tsx
@@ -1,20 +1,48 @@
 import { ComponentPropsWithoutRef, forwardRef, Ref } from 'react';
 
+type InputStyle = 'default' | 'line';
+type FontSize = 'small' | 'large';
 interface InputProps extends ComponentPropsWithoutRef<'input'> {
+  inputStyle?: InputStyle;
+  fontSize?: FontSize;
   error?: boolean;
 }
 
+const FONT_SIZE_CLASSES = {
+  small: 'text-sm',
+  large: 'text-lg font-bold',
+};
+
+const getInputStyleClasses = (inputStyle: InputStyle) => {
+  switch (inputStyle) {
+    case 'line':
+      return 'border-b-[0.1rem] border-black-400 bg-transparent';
+    case 'default':
+    default:
+      return 'rounded-[0.5rem] border-[0.05rem]';
+  }
+};
+
 const Input = (
-  { error, children, ...props }: InputProps,
+  {
+    inputStyle = 'default',
+    fontSize = 'small',
+    error = false,
+    children,
+    ...props
+  }: InputProps,
   ref: Ref<HTMLInputElement>
 ) => {
-  const borderColor = error
+  const inputStyleClass = getInputStyleClasses(inputStyle);
+  const fontSizeClass = FONT_SIZE_CLASSES[fontSize];
+  const borderColorClass = error
     ? 'border-warning-800 focus:border-warning-800'
     : 'border-black-400 focus:border-main-900';
+
   return (
-    <div className="text-sm">
+    <div className={fontSizeClass}>
       <input
-        className={`w-full rounded-[0.5rem] border-[0.05rem] px-[1rem] py-[1.3rem] outline-none ${borderColor}`}
+        className={`w-full px-[1rem] py-[1.3rem] outline-none ${inputStyleClass} ${borderColorClass}`}
         {...props}
         ref={ref}
       />
