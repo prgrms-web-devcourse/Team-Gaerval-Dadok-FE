36c1e71747e358cb7fe4c970a52a975f228c6e7d
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-04-13 18:50:35 +0900

    feat: 모임 게시글 수정, 삭제 기능 구현 (#513)

--- src/v1/comment/BookGroupCommentList.tsx
+++ src/v1/comment/BookGroupCommentList.tsx
@@ -1,18 +1,50 @@
+import { APIGroupComment } from '@/types/group';
+import useToast from '@/v1/base/Toast/useToast';
 import { useBookGroupComments } from '@/queries/group/useBookGroupCommentsQuery';
 import { useMyProfileId } from '@/queries/user/useMyProfileQuery';
 import { useBookGroup } from '@/queries/group/useBookGroupQuery';
+import usePatchBookGroupCommentMutation from '@/queries/group/usePatchBookGroupCommentMutation';
+import useDeleteBookGroupCommentMutation from '@/queries/group/useDeleteBookGroupCommentMutation';
 import { checkAuthentication } from '@/utils/helpers';
 
 import CommentList from './CommentList';
 
 const BookGroupCommentList = ({ groupId }: { groupId: number }) => {
   const isAuthenticated = checkAuthentication();
+
+  const { show: showToast } = useToast();
+
   const { data: bookGroupInfo } = useBookGroup(groupId);
   const { data: comments } = useBookGroupComments(groupId);
   const { data: myId } = useMyProfileId({ enabled: isAuthenticated });
-  const { isPublic, isMember } = bookGroupInfo;
 
+  const editComment = usePatchBookGroupCommentMutation(groupId);
+  const deleteComment = useDeleteBookGroupCommentMutation(groupId);
+
+  const { isPublic, isMember } = bookGroupInfo;
   const isHidden = !isPublic && !isMember;
+
+  const handleBookGroupCommentEdit = (
+    commentId: APIGroupComment['commentId'],
+    comment: string
+  ) => {
+    editComment.mutate(
+      { commentId, comment },
+      {
+        onSuccess: () =>
+          showToast({ type: 'success', message: '코멘트를 수정했어요' }),
+      }
+    );
+  };
+
+  const handleBookGroupCommentDelete = (
+    commentId: APIGroupComment['commentId']
+  ) => {
+    deleteComment.mutate(commentId, {
+      onSuccess: () =>
+        showToast({ type: 'success', message: '코멘트를 삭제했어요' }),
+    });
+  };
 
   return (
     <CommentList
@@ -23,6 +55,8 @@ const BookGroupCommentList = ({ groupId }: { groupId: number }) => {
       hiddenText={`멤버만 볼 수 있어요 🥲`}
       emptyText={`아직 게시글이 없어요.
                   가장 먼저 게시글을 남겨보세요!`}
+      onEditConfirm={handleBookGroupCommentEdit}
+      onDeleteConfirm={handleBookGroupCommentDelete}
     />
   );
 };
