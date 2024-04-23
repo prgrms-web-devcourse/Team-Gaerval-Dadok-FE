8f27b7cecd00e4ea33544a4c51a91a9217a0996a
hanyugeon <hanyugeon@naver.com>
2024-04-23 23:12:14 +0900

    fix: floatingButton 개선

--- src/v1/base/FloatingButton.tsx
+++ src/v1/base/FloatingButton.tsx
@@ -1,39 +1,48 @@
-import { IconPlus } from '@public/icons';
 import { ComponentPropsWithoutRef } from 'react';
-import { createPortal } from 'react-dom';
+
+import { IconPlus } from '@public/icons';
+
+import Portal from './Portal';
 
 interface FloatingButtonProps extends ComponentPropsWithoutRef<'button'> {
   position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+  onClick?: () => void;
 }
 
 const getPositionClasses = (position: string) => {
   switch (position) {
     case 'top-left': {
-      return 'top-[6.4rem] left-[1.2rem]';
+      return 'top-[1.3rem] left-[1.7rem]';
     }
     case 'top-right': {
-      return 'top-[6.4rem] right-[1.2rem]';
+      return 'top-[1.3rem] right-[1.7rem]';
     }
     case 'bottom-left': {
-      return 'bottom-[7.2rem] left-[1.2rem]';
+      return 'bottom-[8.3rem] left-[1.7rem]';
     }
     case 'bottom-right': {
-      return 'bottom-[7.2rem] right-[1.2rem]';
+      return 'bottom-[8.3rem] right-[1.7rem]';
     }
   }
 };
 
-const FloatingButton = ({ position, ...props }: FloatingButtonProps) => {
+const FloatingButton = ({
+  position,
+  onClick,
+  ...props
+}: FloatingButtonProps) => {
   const positionClasses = getPositionClasses(position);
 
-  return createPortal(
-    <button
-      className={`${positionClasses} fixed left-[50%] top-[50%] flex h-[5.1rem] w-[5.1rem] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-main-900`}
-      {...props}
-    >
-      <IconPlus />
-    </button>,
-    document.body
+  return (
+    <Portal id="createBookGroupButton">
+      <button
+        className={`${positionClasses} absolute flex h-[5.1rem] w-[5.1rem] items-center justify-center rounded-full bg-main-900`}
+        onClick={onClick}
+        {...props}
+      >
+        <IconPlus className="fill-black" />
+      </button>
+    </Portal>
   );
 };
 
