ffeb085878614befadff92872f480fee97c30e52
gxxrxn <rbfks1221@naver.com>
2024-04-27 01:28:28 +0900

    feat: input에 leftIconType prop 추가

--- src/v1/base/Input.tsx
+++ src/v1/base/Input.tsx
@@ -2,24 +2,37 @@ import { ComponentPropsWithoutRef, forwardRef, Ref } from 'react';
 
 type InputStyle = 'default' | 'line';
 type FontSize = 'small' | 'large';
+type LeftIconType = 'search';
+
 interface InputProps extends ComponentPropsWithoutRef<'input'> {
   inputStyle?: InputStyle;
+  leftIconType?: LeftIconType;
   fontSize?: FontSize;
   error?: boolean;
+  showSearchIcon?: boolean;
 }
 
 const FONT_SIZE_CLASSES = {
-  small: 'text-sm',
-  large: 'text-lg font-bold',
+  small: 'text-sm after:text-sm',
+  large: 'text-lg font-bold after:text-lg after:font-bold',
 };
 
 const getInputStyleClasses = (inputStyle: InputStyle) => {
   switch (inputStyle) {
     case 'line':
-      return 'border-b-[0.1rem] border-black-400 bg-transparent';
+      return 'border-b-[0.1rem] border-black-400';
     case 'default':
     default:
       return 'rounded-[0.5rem] border-[0.05rem] px-[1rem]';
+  }
+};
+
+const getLeftIconClass = (iconType?: LeftIconType) => {
+  switch (iconType) {
+    case 'search':
+      return 'px-[1rem] before:relative before:top-[0.3rem] before:pr-[1rem] before:content-search';
+    default:
+      return '';
   }
 };
 
@@ -28,22 +41,29 @@ const Input = (
     inputStyle = 'default',
     fontSize = 'small',
     error = false,
+    leftIconType,
+    className = '',
     ...props
   }: InputProps,
   ref: Ref<HTMLInputElement>
 ) => {
   const inputStyleClass = getInputStyleClasses(inputStyle);
+  const leftIconClass = getLeftIconClass(leftIconType);
   const fontSizeClass = FONT_SIZE_CLASSES[fontSize];
   const borderColorClass = error
-    ? 'border-warning-800 focus:border-warning-800'
-    : 'border-black-400 focus:border-main-900';
+    ? 'border-warning-800 focus-within:border-warning-800'
+    : 'border-black-400 focus-within:border-main-900';
 
   return (
-    <input
-      className={`w-full py-[1.3rem] outline-none ${fontSizeClass} ${inputStyleClass} ${borderColorClass}`}
-      {...props}
-      ref={ref}
-    />
+    <div
+      className={`flex w-full items-center bg-transparent ${inputStyleClass} ${borderColorClass} ${fontSizeClass} ${leftIconClass} ${className}`}
+    >
+      <input
+        className={`h-[4.4rem] w-full bg-transparent outline-none ${fontSizeClass}`}
+        {...props}
+        ref={ref}
+      />
+    </div>
   );
 };
 
