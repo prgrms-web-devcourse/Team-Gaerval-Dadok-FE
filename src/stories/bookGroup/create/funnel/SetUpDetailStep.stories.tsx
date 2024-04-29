42654d2460c8855dbc1cdbd947e1aaaf66340c2d
hanyugeon <hanyugeon@naver.com>
2024-04-30 00:12:04 +0900

    feat: 모임 상세 퍼널 스텝 스토리 작성

--- src/stories/bookGroup/create/funnel/SetUpDetailStep.stories.tsx
+++ src/stories/bookGroup/create/funnel/SetUpDetailStep.stories.tsx
@@ -1,3 +1,7 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import type { APICreateGroup } from '@/types/group';
+import { getTodayDate } from '@/utils/date';
+
 import { SetUpDetailStep } from '@/v1/bookGroup/create/funnel';
 import { Meta, StoryObj } from '@storybook/react';
 
@@ -10,7 +14,36 @@ const meta: Meta<typeof SetUpDetailStep> = {
 export default meta;
 
 type Story = StoryObj<typeof SetUpDetailStep>;
+interface FunnelFormValues extends APICreateGroup {
+  customMemberCount: string | number;
+}
+
+const SetUpDetailForm = () => {
+  const methods = useForm<FunnelFormValues>({
+    mode: 'all',
+    defaultValues: {
+      bookId: 23,
+      title: '',
+      introduce: '',
+      maxMemberCount: 9999,
+      startDate: getTodayDate(),
+      endDate: '',
+      isPublic: false,
+      hasJoinPasswd: false,
+      joinQuestion: '',
+      joinPasswd: '',
+    },
+  });
+
+  return (
+    <FormProvider {...methods}>
+      <form>
+        <SetUpDetailStep />
+      </form>
+    </FormProvider>
+  );
+};
 
 export const Default: Story = {
-  args: {},
+  render: () => <SetUpDetailForm />,
 };
