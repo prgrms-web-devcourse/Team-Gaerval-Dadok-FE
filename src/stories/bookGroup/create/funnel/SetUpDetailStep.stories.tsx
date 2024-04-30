2faa4661cb1a170a534286f20e1759a51e26f6af
hanyugeon <hanyugeon@naver.com>
2024-04-30 22:51:12 +0900

    refactor: 코드리뷰 반영

--- src/stories/bookGroup/create/funnel/SetUpDetailStep.stories.tsx
+++ src/stories/bookGroup/create/funnel/SetUpDetailStep.stories.tsx
@@ -4,11 +4,12 @@ import { getTodayDate } from '@/utils/date';
 
 import { SetUpDetailStep } from '@/v1/bookGroup/create/funnel';
 import { Meta, StoryObj } from '@storybook/react';
+import { appLayoutMeta } from '@/stories/meta';
 
 const meta: Meta<typeof SetUpDetailStep> = {
   title: 'bookGroup/funnel/SetUpDetailStep',
   component: SetUpDetailStep,
-  tags: ['autodocs'],
+  ...appLayoutMeta,
 };
 
 export default meta;
--- src/v1/bookGroup/create/funnel/SetUpDetailStep.tsx
+++ src/v1/bookGroup/create/funnel/SetUpDetailStep.tsx
@@ -1,8 +1,4 @@
-import {
-  type FieldErrors,
-  type UseFormRegister,
-  useFormContext,
-} from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 
 import type { APICreateGroup } from '@/types/group';
 
@@ -30,42 +26,31 @@ interface SetUpDetailStepValues
     APICreateGroup,
     'bookId' | 'title' | 'introduce' | 'startDate' | 'endDate' | 'isPublic'
   > {
-  maxMemberCount: string | number;
-  customMemberCount: string | number;
+  maxMemberCount: string;
+  customMemberCount: string;
 }
 
 const SetUpDetailStep = ({
   // FIXME: goToSelectBookStep,
   onNextStep,
 }: MoveFunnelStepProps) => {
-  const {
-    register,
-    handleSubmit,
-    getValues,
-    watch,
-    formState: { errors },
-  } = useFormContext<SetUpDetailStepValues>();
+  const { handleSubmit, getValues } = useFormContext<SetUpDetailStepValues>();
 
   return (
     <article className="flex flex-col gap-[3.2rem] overflow-y-scroll pb-[7rem]">
-      <TitleField
-        currentLength={watch('title')?.length}
-        errors={errors}
-        register={register}
-      />
+      <TitleField name={'title'} />
       <SelectedBookInfoField bookId={getValues('bookId')} />
-      <IntroduceField errors={errors} register={register} />
-      <MaxMemberCountField
-        isCustomMemberCount={watch('maxMemberCount') === 'custom'}
-        errors={errors}
-        register={register}
-      />
-      <PickDateField
-        selectedStartDate={watch('startDate')}
-        errors={errors}
-        register={register}
-      />
-      <SwitchIsPublicField register={register} />
+
+      <IntroduceField name={'introduce'} />
+      <section className="flex flex-col gap-[1.6rem]">
+        <MaxMemberCountField name={'maxMemberCount'} />
+        <CustomMemberCountField name={'customMemberCount'} />
+      </section>
+
+      <PickStartDateField name={'startDate'} />
+      <PickEndDateField name={'endDate'} />
+
+      <SwitchIsPublicField name={'isPublic'} />
 
       <BottomActionButton
         type="submit"
@@ -79,25 +64,30 @@ const SetUpDetailStep = ({
 
 export default SetUpDetailStep;
 
-interface _DefaultFieldProps {
-  errors?: FieldErrors<SetUpDetailStepValues>;
-  register?: UseFormRegister<SetUpDetailStepValues>;
-}
+type SetUpDetailFieldProps = {
+  name: keyof SetUpDetailStepValues;
+};
 
-const TitleField = ({
-  currentLength,
-  errors,
-  register,
-}: {
-  currentLength: number;
-} & _DefaultFieldProps) => {
+const TitleField = ({ name }: SetUpDetailFieldProps) => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext<SetUpDetailStepValues>();
+
+  const watchedName = watch(name);
+  const currentLength =
+    typeof watchedName === 'string' ? watchedName.length : 0;
+
+  const titleErrors = errors[name];
+
   return (
     <section className="flex flex-col gap-[0.5rem]">
       <Input
         fontSize="large"
         inputStyle="line"
-        error={!!errors?.title}
-        {...register?.('title', {
+        error={!!titleErrors}
+        {...register(name, {
           required: '독서모임 이름을 적어주세요',
           minLength: { value: 2, message: '2글자 이상 입력해주세요' },
           maxLength: { value: 20, message: '20글자 이하로 입력해주세요' },
@@ -107,9 +97,9 @@ const TitleField = ({
         <InputLength
           currentLength={currentLength}
           maxLength={20}
-          isError={!!errors?.title}
+          isError={!!titleErrors}
         />
-        <ErrorMessage>{errors?.title?.message}</ErrorMessage>
+        <ErrorMessage>{titleErrors?.message}</ErrorMessage>
       </div>
     </section>
   );
@@ -123,116 +113,152 @@ const SelectedBookInfoField = ({ bookId }: { bookId?: number }) => {
   );
 };
 
-const IntroduceField = ({ errors, register }: _DefaultFieldProps) => {
+const IntroduceField = ({ name }: SetUpDetailFieldProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<SetUpDetailStepValues>();
+
+  const introduceErrors = errors[name];
+
   return (
     <section className="flex flex-col gap-[1.6rem]">
       <h2>활동 내용</h2>
       <TextArea
         count={true}
-        error={!!errors?.introduce}
+        error={!!introduceErrors}
         placeholder="독서모임에서 어떤 활동을 할 계획인지 자세히 설명해주세요"
-        {...register?.('introduce', {
+        {...register(name, {
           required: '독서모임에 대한 설명을 적어주세요',
           minLength: { value: 10, message: '10글자 이상 입력해주세요' },
           maxLength: { value: 500, message: '500자 이하로 입력해주세요' },
         })}
       >
-        <ErrorMessage>{errors?.introduce?.message}</ErrorMessage>
+        <ErrorMessage>{introduceErrors?.message}</ErrorMessage>
       </TextArea>
     </section>
   );
 };
 
-const MaxMemberCountField = ({
-  isCustomMemberCount,
-  errors,
-  register,
-}: {
-  isCustomMemberCount: boolean;
-} & _DefaultFieldProps) => {
+const MaxMemberCountField = ({ name }: SetUpDetailFieldProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<SetUpDetailStepValues>();
+
+  const maxMemberCountErrors = errors[name];
+
   return (
-    <section className="flex flex-col gap-[1.6rem]">
+    <>
       <h2>최대 인원</h2>
-      <div className="flex flex-col gap-[0.5rem]">
-        <div className="inline-flex w-[23rem] flex-wrap gap-[1rem]">
-          {MAX_MEMBER_COUNT_OPTIONS.map(option => (
-            <RadioButton
-              key={option.value}
-              value={option.value}
-              label={option.label}
-              {...register?.('maxMemberCount', {
-                required: '모임 최대 인원을 선택해 주세요',
-              })}
-            />
-          ))}
-        </div>
-        <ErrorMessage>{errors?.maxMemberCount?.message}</ErrorMessage>
+      <div className="inline-flex w-[23rem] flex-wrap gap-[1rem]">
+        {MAX_MEMBER_COUNT_OPTIONS.map(option => (
+          <RadioButton
+            key={option.value}
+            value={option.value}
+            label={option.label}
+            {...register(name, {
+              required: '모임 최대 인원을 선택해 주세요',
+            })}
+          />
+        ))}
       </div>
-      {isCustomMemberCount && (
+      <ErrorMessage>{maxMemberCountErrors?.message}</ErrorMessage>
+    </>
+  );
+};
+
+const CustomMemberCountField = ({ name }: SetUpDetailFieldProps) => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext<SetUpDetailStepValues>();
+
+  const customMemberCountErrors = errors[name];
+  const isCustomInputCount = watch('maxMemberCount') === 'custom';
+
+  return (
+    <>
+      {isCustomInputCount && (
         <div className="flex flex-col gap-[0.5rem]">
           <Input
             type="number"
             placeholder="1000명 이상의 인원은 제한 없음을 선택해주세요"
-            {...register?.('customMemberCount', {
+            {...register(name, {
               required: {
-                value: isCustomMemberCount,
+                value: isCustomInputCount,
                 message: '모임 최대 인원을 입력해주세요',
               },
               min: { value: 2, message: '모임 인원은 최소 2명부터 가능해요' },
               max: { value: 1000, message: '1000 이하의 값을 입력해주세요' },
             })}
           />
-          <ErrorMessage>{errors?.customMemberCount?.message}</ErrorMessage>
+          <ErrorMessage>{customMemberCountErrors?.message}</ErrorMessage>
         </div>
       )}
-    </section>
-  );
-};
-
-const PickDateField = ({
-  selectedStartDate,
-  errors,
-  register,
-}: {
-  selectedStartDate: string;
-} & _DefaultFieldProps) => {
-  return (
-    <>
-      <section className="flex flex-col gap-[0.5rem]">
-        <div className="flex items-center justify-between">
-          <h2>모임 시작일</h2>
-          <DatePicker
-            {...register?.('startDate', {
-              required: '모임 시작일을 선택해주세요',
-              min: {
-                value: getTodayDate(),
-                message: '모임 시작일은 오늘 혹은 그 이후로 선택해주세요',
-              },
-            })}
-          />
-        </div>
-        <ErrorMessage>{errors?.startDate?.message}</ErrorMessage>
-      </section>
-      <section className="flex flex-col gap-[0.5rem]">
-        <div className="flex items-center justify-between">
-          <h2>모임 종료일</h2>
-          <DatePicker
-            {...register?.('endDate', {
-              required: '모임 종료일을 선택해주세요',
-              min: {
-                value: selectedStartDate,
-                message: '모임 종료일은 시작일보다 늦어야해요',
-              },
-            })}
-          />
-        </div>
-        <ErrorMessage>{errors?.endDate?.message}</ErrorMessage>
-      </section>
     </>
   );
 };
 
-const SwitchIsPublicField = ({ register }: _DefaultFieldProps) => {
+const PickStartDateField = ({ name }: SetUpDetailFieldProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<SetUpDetailStepValues>();
+
+  const startDateErrors = errors[name];
+
+  return (
+    <section className="flex flex-col gap-[0.5rem]">
+      <div className="flex items-center justify-between">
+        <h2>모임 시작일</h2>
+        <DatePicker
+          {...register(name, {
+            required: '모임 시작일을 선택해주세요',
+            min: {
+              value: getTodayDate(),
+              message: '모임 시작일은 오늘 혹은 그 이후로 선택해주세요',
+            },
+          })}
+        />
+      </div>
+      <ErrorMessage>{startDateErrors?.message}</ErrorMessage>
+    </section>
+  );
+};
+
+const PickEndDateField = ({ name }: SetUpDetailFieldProps) => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext<SetUpDetailStepValues>();
+
+  const endDateErrors = errors[name];
+
+  return (
+    <section className="flex flex-col gap-[0.5rem]">
+      <div className="flex items-center justify-between">
+        <h2>모임 종료일</h2>
+        <DatePicker
+          {...register(name, {
+            required: '모임 종료일을 선택해주세요',
+            min: {
+              value: watch('startDate'),
+              message: '모임 종료일은 시작일보다 늦어야해요',
+            },
+          })}
+        />
+      </div>
+      <ErrorMessage>{endDateErrors?.message}</ErrorMessage>
+    </section>
+  );
+};
+
+const SwitchIsPublicField = ({ name }: SetUpDetailFieldProps) => {
+  const { register } = useFormContext<SetUpDetailStepValues>();
+
   return (
     <section className="flex items-start justify-between">
       <div className="flex min-w-0 flex-col gap-[0.3rem]">
@@ -241,7 +267,7 @@ const SwitchIsPublicField = ({ register }: _DefaultFieldProps) => {
           모임에 가입하지 않은 사람도 댓글을 볼 수 있어요.
         </p>
       </div>
-      <Switch {...register?.('isPublic')} />
+      <Switch {...register(name)} />
     </section>
   );
 };
