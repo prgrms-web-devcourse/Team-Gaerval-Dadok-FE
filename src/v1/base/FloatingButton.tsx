890c135791f178df5b84cab449396cc445f2b7df
hanyugeon <hanyugeon@naver.com>
2024-05-10 16:51:28 +0900

    refactor: FloatingButton 수정
    - fixed 적용
    - 위치값을 사용부에서 string형식으로 props를 받을 수 있도록 수정

--- src/v1/base/FloatingButton.tsx
+++ src/v1/base/FloatingButton.tsx
@@ -5,38 +5,19 @@ import { IconPlus } from '@public/icons';
 import Portal from './Portal';
 
 interface FloatingButtonProps extends ComponentPropsWithoutRef<'button'> {
-  position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+  position?: string;
   onClick?: () => void;
 }
-
-const getPositionClasses = (position: string) => {
-  switch (position) {
-    case 'top-left': {
-      return 'top-[1.3rem] left-[1.7rem]';
-    }
-    case 'top-right': {
-      return 'top-[1.3rem] right-[1.7rem]';
-    }
-    case 'bottom-left': {
-      return 'bottom-[8.3rem] left-[1.7rem]';
-    }
-    case 'bottom-right': {
-      return 'bottom-[8.3rem] right-[1.7rem]';
-    }
-  }
-};
 
 const FloatingButton = ({
   position,
   onClick,
   ...props
 }: FloatingButtonProps) => {
-  const positionClasses = getPositionClasses(position);
-
   return (
-    <Portal id="floating-button-portal">
+    <Portal id="floating-button-root">
       <button
-        className={`${positionClasses} absolute flex h-[5.1rem] w-[5.1rem] items-center justify-center rounded-full bg-main-900 shadow-floating-button`}
+        className={`fixed ${position} flex h-[5.1rem] w-[5.1rem] items-center justify-center rounded-full bg-main-900 shadow-floating-button`}
         onClick={onClick}
         {...props}
       >
