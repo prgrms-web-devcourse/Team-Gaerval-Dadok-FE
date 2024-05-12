19fdc9c056a70e39988b66275052da9fd8075aea
hanyugeon <hanyugeon@naver.com>
2024-05-12 19:51:46 +0900

    feat: 모임 생성 퍼널 작성

--- src/app/group/create/page.tsx
+++ src/app/group/create/page.tsx
@@ -1,19 +1,82 @@
 'use client';
 
-import AddGroupForm from '@/ui/Group/AddGroupForm';
-import { VStack } from '@chakra-ui/react';
-import TopNavigation from '@/ui/common/TopNavigation';
-import AuthRequired from '@/ui/AuthRequired';
+import { FormProvider, useForm } from 'react-hook-form';
 
-const GroupCreatePage = () => {
+import type { SearchedBookWithId } from '@/types/book';
+import type { APICreateGroup } from '@/types/group';
+
+import { useFunnel } from '@/hooks/useFunnel';
+import { getTodayDate } from '@/utils/date';
+
+import { IconArrowLeft } from '@public/icons';
+import TopNavigation from '@/v1/base/TopNavigation';
+import { SelectBookStep } from '@/v1/bookGroup/create/steps/SelectBookStep';
+import { EnterTitleStep } from '@/v1/bookGroup/create/steps/EnterTitleStep';
+import { SetUpDetailStep } from '@/v1/bookGroup/create/steps/SetUpDetailStep';
+import { SelectJoinTypeStep } from '@/v1/bookGroup/create/steps/SelectJoinTypeStep';
+
+const FUNNEL_STEPS = [
+  'SelectBook',
+  'EnterTitle',
+  'SetUpDetail',
+  'SelectJoinType',
+] as const;
+
+interface FunnelValues extends APICreateGroup {
+  book: SearchedBookWithId;
+  queryKeyword: string;
+  customMemberCount: string;
+}
+
+const GroupCreateFunnel = () => {
+  const [Funnel, setStep] = useFunnel(FUNNEL_STEPS, {
+    initialStep: 'SelectBook',
+  });
+
+  const methods = useForm<FunnelValues>({
+    mode: 'all',
+    defaultValues: {
+      title: '',
+      introduce: '',
+      maxMemberCount: 9999,
+      startDate: getTodayDate(),
+      endDate: '',
+      isPublic: false,
+      hasJoinPasswd: false,
+      joinQuestion: '',
+      joinPasswd: '',
+    },
+  });
+
   return (
-    <AuthRequired>
-      <VStack justify="center" align="center">
-        <TopNavigation pageTitle="모임 생성" />
-        <AddGroupForm />
-      </VStack>
-    </AuthRequired>
+    <FormProvider {...methods}>
+      <TopNavigation>
+        <TopNavigation.LeftItem>
+          <IconArrowLeft onClick={() => console.log('<')} />
+        </TopNavigation.LeftItem>
+      </TopNavigation>
+
+      <form>
+        <Funnel>
+          <Funnel.Step name="SelectBook">
+            <SelectBookStep onNextStep={() => setStep('EnterTitle')} />
+          </Funnel.Step>
+          <Funnel.Step name="EnterTitle">
+            <EnterTitleStep onNextStep={() => setStep('SetUpDetail')} />
+          </Funnel.Step>
+          <Funnel.Step name="SetUpDetail">
+            <SetUpDetailStep
+              goToSelectBookStep={() => setStep('SelectBook')}
+              onNextStep={() => setStep('SelectJoinType')}
+            />
+          </Funnel.Step>
+          <Funnel.Step name="SelectJoinType">
+            <SelectJoinTypeStep onSubmit={() => console.log} />
+          </Funnel.Step>
+        </Funnel>
+      </form>
+    </FormProvider>
   );
 };
 
-export default GroupCreatePage;
+export default GroupCreateFunnel;
