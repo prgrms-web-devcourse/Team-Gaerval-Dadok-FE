cff01fd5473cb5ee71a65ea045b034d9795bfc7b
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-05-16 15:06:40 +0900

    [#575] Drawer 컴포넌트 개선 (#576)
    * refactor: Drawer 컴포넌트 headless-ui로 리팩터링
    
    * test: body scroll 막기 위한 textarea blink 애니메이션 적용
    
    * feat: touchevent preventDefault 호출
    
    * fix: textarea 내부에 스크롤 없는 경우에도 toucmove 브라우저 동작 막음
    
    * fix: touchmove 이벤트 조건문 수정
    
    * fix: textarea scrollbar 유무 판단 로직 수정
    
    * fix: useBodyScrollLock -> useRemoveScroll 수정
    
    - eventListner option 타입 정의 (FixedEventListenerOptions)
    - nonPassive option 정의
    
    * fix: textarea 내부에서 바깥 영역 스크롤 되는 현상 해결
    
    * refactor: 불필요한 log, textarea 배경색 제거, event handler memoization
    
    * chore: touchstart handler 선언부 상위로 이동
    
    * feat: comment drawer 열릴 때 text 끝에 focus되도록 수정
    
    - Drawer.CloseButton이 foucs를 받을 수 있도록 button 컨테이너 추가

--- src/v1/comment/CommentDrawer.tsx
+++ src/v1/comment/CommentDrawer.tsx
@@ -1,4 +1,6 @@
-import { forwardRef } from 'react';
+'use client';
+
+import { forwardRef, useEffect } from 'react';
 
 import Button from '@/v1/base/Button';
 import Drawer from '@/v1/base/Drawer';
@@ -19,6 +21,21 @@ const CommentDrawer = forwardRef<HTMLTextAreaElement, CommentDrawerProps>(
       onClose();
     };
 
+    useEffect(() => {
+      if (!isOpen) return;
+
+      // Drawer가 열릴 때 textarea의 끝에 focus
+      setTimeout(() => {
+        const textarea = document.querySelector('textarea');
+
+        if (textarea) {
+          textarea.focus();
+          textarea.select();
+          window.getSelection()?.collapseToEnd();
+        }
+      }, 100);
+    }, [isOpen]);
+
     return (
       <Drawer isOpen={isOpen} onClose={onClose}>
         <Drawer.Header>
@@ -36,7 +53,7 @@ const CommentDrawer = forwardRef<HTMLTextAreaElement, CommentDrawerProps>(
         </Drawer.Header>
         <Drawer.Content>
           <textarea
-            className="h-full w-full resize-none border-none text-md focus:outline-none"
+            className="w-full resize-none border-none text-md focus:outline-none"
             rows={15}
             defaultValue={defaultComment}
             placeholder={placeholder}
