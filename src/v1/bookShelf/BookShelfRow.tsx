831b6ca2128e052157ff19823ffde1f67832e2bc
gxxrxn <rbfks1221@naver.com>
2024-05-21 21:52:37 +0900

    style: 책장 padding 수정

--- src/v1/bookShelf/BookShelfRow.tsx
+++ src/v1/bookShelf/BookShelfRow.tsx
@@ -4,7 +4,7 @@ import BookShelf from './BookShelf';
 const BookShelfRow = ({ books }: Pick<APIBookshelf, 'books'>) => {
   return (
     <BookShelf>
-      <div className="relative left-[-2rem] w-[calc(100%+4rem)] px-[-2rem] pb-[2.5rem] pt-[2rem] shadow-[0px_28px_20px_-16px_#D1D1D1]">
+      <div className="relative left-[-2rem] w-[calc(100%+4rem)] pb-[2.5rem] pt-[2rem] shadow-[0px_28px_20px_-16px_#D1D1D1]">
         <BookShelf.Background />
         <BookShelf.Books books={books} />
       </div>
