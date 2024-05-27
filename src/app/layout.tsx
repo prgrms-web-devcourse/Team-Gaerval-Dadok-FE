f86006033e48ed1e2d9c717a52d6252df2c5dfad
gxxrxn <rbfks1221@naver.com>
2024-05-28 01:27:31 +0900

    feat: AuthFailedErrorBoundary 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import ContextProvider from '@/components/ContextProvider';
+import AuthFailedErrorBoundary from '@/components/AuthFailedErrorBoundary';
 import Layout from '@/v1/layout/Layout';
 
 import { LineSeedKR } from '@/styles/font';
@@ -18,7 +19,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       {/* @todo Chakra 제거시 app-layout 프로퍼티 제거. */}
       <body className={`${LineSeedKR.variable} app-layout font-lineseed`}>
         <Layout>
-          <ContextProvider>{children}</ContextProvider>
+          <ContextProvider>
+            <AuthFailedErrorBoundary>{children}</AuthFailedErrorBoundary>
+          </ContextProvider>
         </Layout>
       </body>
     </html>
---
+++ src/components/AuthFailedErrorBoundary.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useEffect } from 'react';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+
+import useToast from '@/v1/base/Toast/useToast';
+import { isAuthFailedError, isAxiosErrorWithCustomCode } from '@/utils/helpers';
+import Loading from '@/v1/base/Loading';
+
+const AuthFailedErrorBoundary = ({
+  children,
+}: {
+  children?: React.ReactNode;
+}) => {
+  return (
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary onReset={reset} FallbackComponent={AuthFailedFallback}>
+          {children}
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
+  );
+};
+
+export default AuthFailedErrorBoundary;
+
+const AuthFailedFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  const { show: showToast } = useToast();
+
+  useEffect(() => {
+    if (
+      isAxiosErrorWithCustomCode(error) &&
+      isAuthFailedError(error.response.data.code)
+    ) {
+      showToast({ message: '다시 로그인 해주세요' });
+      resetErrorBoundary();
+    }
+  }, [error, resetErrorBoundary, showToast]);
+
+  return <Loading fullpage />;
+};
