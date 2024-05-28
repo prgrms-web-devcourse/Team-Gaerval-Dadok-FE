7509597936ee6e830840a168c46cad2f156779d7
hanyugeon <hanyugeon@naver.com>
2024-05-29 00:50:23 +0900

    fix: ErrorBoundary 제거

--- src/v1/profile/bookShelf/ProfileBookShelf.tsx
+++ src/v1/profile/bookShelf/ProfileBookShelf.tsx
@@ -1,52 +1,14 @@
-import QueryErrorBoundaryFallback from '@/v1/base/QueryErrorBoundaryFallback';
-import { QueryErrorResetBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 import type { APIUser } from '@/types/user';
+
 import MyProfileBookshelfContainer from './MyProfileBookshelfContainer';
 import UserProfileBookshelfContainer from './UserProfileBookshelfContainer';
-import useMounted from '@/hooks/useMounted';
-import Loading from '@/v1/base/Loading';
 
 const ProfileBookShelf = ({ userId }: { userId: 'me' | APIUser['userId'] }) => {
-  const mounted = useMounted();
-
-  if (!mounted) return null;
-
-  return (
-    <QueryErrorResetBoundary>
-      {({ reset }) => (
-        <ErrorBoundary
-          onReset={reset}
-          fallbackRender={({ resetErrorBoundary }) => (
-            <QueryErrorBoundaryFallback
-              resetErrorBoundary={resetErrorBoundary}
-            />
-          )}
-        >
-          <Suspense fallback={<ProfileBookShelfSkeleton />}>
-            {userId === 'me' ? (
-              <MyProfileBookshelfContainer />
-            ) : (
-              <UserProfileBookshelfContainer userId={userId} />
-            )}
-          </Suspense>
-        </ErrorBoundary>
-      )}
-    </QueryErrorResetBoundary>
+  return userId === 'me' ? (
+    <MyProfileBookshelfContainer />
+  ) : (
+    <UserProfileBookshelfContainer userId={userId} />
   );
 };
 
 export default ProfileBookShelf;
-
-const ProfileBookShelfSkeleton = () => {
-  return (
-    <div className="flex animate-pulse flex-col gap-[2rem]">
-      <div className="h-[2.7rem] w-[5rem] bg-placeholder" />
-
-      <div className="flex h-[13.2rem] items-center justify-center">
-        <Loading />
-      </div>
-    </div>
-  );
-};
