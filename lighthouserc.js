b9c62fc9812f2b99d53bae32a72655aef9252d27
hanyugeon <hanyugeon@naver.com>
2024-05-29 16:43:31 +0900

    fix lighthouserc.js collect 옵션 수정

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './dist',
+      startServerCommand: 'yarn start',
+      startServerReadyPattern: 'ready on',
       url: [
         'http://localhost:3000/bookarchive',
         'http://localhost:3000/book/search',
