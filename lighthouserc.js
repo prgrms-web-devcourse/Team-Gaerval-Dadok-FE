48dc5f199a39172666ea6d8ad0f11ae502265dec
hanyugeon <hanyugeon@naver.com>
2024-05-30 18:11:09 +0900

    refactor: script 분리 테스트

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,9 +5,9 @@ module.exports = {
       startServerReadyPattern: 'ready on',
       url: [
         'http://localhost:3000/bookarchive',
-        // 'http://localhost:3000/book/search',
-        // 'http://localhost:3000/group',
-        // 'http://localhost:3000/profile/me',
+        'http://localhost:3000/book/search',
+        'http://localhost:3000/group',
+        'http://localhost:3000/profile/me',
       ],
       numberOfRuns: 1,
     },
