f044cd2309919ba3e9f54948ea94d94d4b6000b7
harry kim <73218463+hanyugeon@users.noreply.github.com>
2024-06-02 22:54:53 +0900

    [#603] [내 프로필] ErrorBoundary 제거 (#604)
    * fix: 비로그인 내 프로필 페이지 컨탠츠 gap 수정
    
    * fix: ErrorBoundary 제거
    
    * refactor: 내 책장 프리뷰 query의 data값을 보장하도록 useQueryWithSuspense를 적용
    
    * refactor: 불필요한 isSuccess 제거 및 LayoutShift 문제 해결
    
    * chore: 불필요한 query 옵션 제거

--- src/v1/profile/info/ProfileInfo.tsx
+++ src/v1/profile/info/ProfileInfo.tsx
@@ -1,44 +1,24 @@
-import { ReactNode, Suspense } from 'react';
+import type { APIUser } from '@/types/user';
+
+import SSRSafeSuspense from '@/components/SSRSafeSuspense';
+
+import Skeleton from '@/v1/base/Skeleton';
 import MyProfileContainer from './MyProfileInfoContainer';
 import UserProfileInfoContainer from './UserProfileInfoContainer';
-import { QueryErrorResetBoundary } from '@tanstack/react-query';
-import { ErrorBoundary } from 'react-error-boundary';
-import type { APIUser } from '@/types/user';
-import QueryErrorBoundaryFallback from '@/v1/base/QueryErrorBoundaryFallback';
-import useMounted from '@/hooks/useMounted';
 
 type ProfileInfoProps = {
-  children?: ReactNode;
   userId: 'me' | APIUser['userId'];
 };
 
-const ProfileInfo = ({ userId, children }: ProfileInfoProps) => {
-  const mounted = useMounted();
-
-  if (!mounted) return null;
-
+const ProfileInfo = ({ userId }: ProfileInfoProps) => {
   return (
-    <QueryErrorResetBoundary>
-      {({ reset }) => (
-        <ErrorBoundary
-          onReset={reset}
-          fallbackRender={({ resetErrorBoundary }) => (
-            <QueryErrorBoundaryFallback
-              resetErrorBoundary={resetErrorBoundary}
-            />
-          )}
-        >
-          <Suspense fallback={<ProfileInfoSkeleton />}>
-            {userId === 'me' ? (
-              <MyProfileContainer />
-            ) : (
-              <UserProfileInfoContainer userId={userId} />
-            )}
-            {children && children}
-          </Suspense>
-        </ErrorBoundary>
+    <SSRSafeSuspense fallback={<ProfileInfoSkeleton />}>
+      {userId === 'me' ? (
+        <MyProfileContainer />
+      ) : (
+        <UserProfileInfoContainer userId={userId} />
       )}
-    </QueryErrorResetBoundary>
+    </SSRSafeSuspense>
   );
 };
 
@@ -46,15 +26,17 @@ export default ProfileInfo;
 
 const ProfileInfoSkeleton = () => {
   return (
-    <div className="mb-[2rem] flex animate-pulse flex-col gap-[2rem]">
-      <div className="flex gap-[0.8rem]">
-        <div className="h-[2.1rem] w-[3.8rem] rounded-lg bg-placeholder" />
-        <div className="h-[2.1rem] w-[7.6rem] rounded-lg bg-placeholder" />
+    <Skeleton>
+      <div className="mb-[2rem] flex flex-col gap-[2rem]">
+        <div className="flex gap-[0.8rem]">
+          <Skeleton.Rect width="3.8rem" height="2.1rem" />
+          <Skeleton.Rect width="10.4rem" height="2.1rem" />
+        </div>
+        <div className="flex items-center gap-[1rem]">
+          <Skeleton.Circle size="large" />
+          <Skeleton.Text fontSize="2xlarge" width="18rem" />
+        </div>
       </div>
-      <div className="flex items-center gap-[1rem]">
-        <div className="h-[7rem] w-[7rem] rounded-full bg-placeholder" />
-        <div className="h-[2.7rem] w-[11rem] bg-placeholder" />
-      </div>
-    </div>
+    </Skeleton>
   );
 };
