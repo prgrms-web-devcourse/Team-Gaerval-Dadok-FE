d5d243adb5f736ce2a0256d95f87f8f4f94ba84d
hanyugeon <hanyugeon@naver.com>
2024-06-07 17:13:16 +0900

    feat: 오픈그래프 이미지 추가

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/server';
+
+// Route segment config
+export const runtime = 'edge';
+export const size = { width: 320, height: 320 };
+export const alt = 'About Acme';
+export const contentType = 'image/png';
+
+// Image generation
+export default async function Image() {
+  // Font
+  const lineSeedKR = fetch(
+    new URL('../styles/font/LINESeedKR-Rg.woff2', import.meta.url)
+  ).then(res => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div className="flex h-full w-full items-center justify-center bg-white text-2xl">
+        다독다독
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: 'LINESeedKR',
+          data: await lineSeedKR,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    }
+  );
+}
