a079313a444d4732c852a9553d0f5f27bddaebb6
hanyugeon <hanyugeon@naver.com>
2024-06-08 20:57:24 +0900

    feat: RootLayout에 메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,23 @@ import { LineSeedKR } from '@/styles/font';
 import '@/styles/global.css';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://dadok.app'),
   title: {
     template: '%s | 다독다독',
     default: '다독다독',
   },
   description: '책에 대한 인사이트를 공유하고 소통하는 독서 소셜 플랫폼',
+  keywords: [
+    '다독다독',
+    'dadok',
+    'dadokdadok',
+    '책장',
+    '책추천',
+    '도서검색',
+    '독서모임',
+    '책',
+    '독서',
+  ],
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -23,6 +35,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           content="width=device-width, initial-scale=1, maximum-scale=1"
           name="viewport"
         />
+        <meta charSet="UTF-8" />
         <link rel="icon" href="/favicon.ico" />
       </head>
       {/* @todo Chakra 제거시 app-layout 프로퍼티 제거. */}
