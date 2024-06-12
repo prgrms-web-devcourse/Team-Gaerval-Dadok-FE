209ba3eabbce518825c147636c265b798d79b596
harry kim <73218463+hanyugeon@users.noreply.github.com>
2024-06-12 15:30:04 +0900

    [#349] 메타 데이터 추가 (#617)
    * feat: 오픈그래프 이미지 추가
    
    * feat: 메타데이터 title, description
    
    * feat: 오픈그래프 이미지와 크기 수정
    
    * fix: 오픈그래프 이미지 용량 축소
    
    * fix: opengraph-image.tsx 파일 수정
    
    * fix: Image Generator 제거
    
    * feat: 오픈그래프 이미지 업데이트
    
    * feat: RootLayout에 메타데이터 추가
    
    * feat: Sitemap 작성
    
    - 북카이브, 도서 검색, 독서 모임, 내 프로필
    - 비회원 북카이브에 노출되는 책장 및 도서
    - 전체 독서 모임
    
    * feat: robots 작성
    
    * fix: 오픈그래프 이미지 업데이트
    
    * feat: site-verification 추가 (구글, 네이버)
    
    * refactor: url주소 매직스트링을 환경변수로 교체
    
    * refactor: sitemap을 route별로 분리
    
    * chore: 불필요한 util 파일 삭제
    
    * chore: 코드 리뷰 반영
    
    * refactor: book sitemap 병합 및 route 변경
    
    * refactor: route 위치 통일성 부여

---
+++ src/app/book/sitemap.ts
@@ -0,0 +1,47 @@
+import type { MetadataRoute } from 'next';
+import type { APIRecommendedBookshelf } from '@/types/bookshelf';
+import type { APIBook } from '@/types/book';
+
+const options = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  next: { revalidate: 60 * 60 * 24 },
+};
+
+export async function booksSitemap() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/suggestions/bookshelves/default`,
+      options
+    );
+
+    if (!res.ok) {
+      return Promise.reject();
+    }
+
+    const data: APIRecommendedBookshelf = await res.json();
+
+    const books = new Set<APIBook['bookId']>();
+
+    data.bookshelfResponses.forEach(bookshelf =>
+      bookshelf.books.forEach(book => books.add(book.bookId))
+    );
+
+    const filteredBooks = Array.from(books);
+
+    return filteredBooks;
+  } catch {
+    return [];
+  }
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const booksId = await booksSitemap();
+  const sitemap = ['search', ...booksId];
+
+  return sitemap.map(value => ({
+    url: `${process.env.NEXT_PUBLIC_HOST}/book/${value}`,
+    lastModified: new Date(),
+  }));
+}
