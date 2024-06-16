25ba068e6dfa8e7c6ca322f56e9acd677908ef09
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-06-16 20:55:00 +0900

    [#632] 사용하지 않는 라이브러리 제거 (#633)
    * refactor: 사용하지 않는 라이브러리 제거
    
    - chakra, framer-motion, emotion, recoil
    
    * chore: 불필요한 주석 제거

--- src/components/ContextProvider.tsx
+++ src/components/ContextProvider.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { RecoilRoot } from 'recoil';
 
 import PWAServiceWorkerProvider from '@/components/PWAServiceWorkerProvider';
-import ChakraThemeProvider from '@/components/ChakraThemeProvider';
 import ReactQueryProvider from '@/components/ReactQueryProvider';
 
 import ToastProvider from '@/v1/base/Toast/ToastProvider';
@@ -12,13 +10,9 @@ import ToastProvider from '@/v1/base/Toast/ToastProvider';
 const ContextProvider = ({ children }: { children: ReactNode }) => {
   return (
     <PWAServiceWorkerProvider>
-      <RecoilRoot>
-        <ReactQueryProvider>
-          <ChakraThemeProvider>
-            <ToastProvider>{children}</ToastProvider>
-          </ChakraThemeProvider>
-        </ReactQueryProvider>
-      </RecoilRoot>
+      <ReactQueryProvider>
+        <ToastProvider>{children}</ToastProvider>
+      </ReactQueryProvider>
     </PWAServiceWorkerProvider>
   );
 };
--- src/ui/BookDetail/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as BookInfo } from './BookInfo';
