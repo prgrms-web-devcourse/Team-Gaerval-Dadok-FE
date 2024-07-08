2b458cf401c1b7ca4f3c4730e11912176e965c2d
gxxrxn <rbfks1221@naver.com>
2024-07-08 18:11:00 +0900

    chore: 불필요한 주석 제거

--- src/components/profile/info/MyProfileInfoContainer.tsx
+++ src/components/profile/info/MyProfileInfoContainer.tsx
@@ -4,18 +4,6 @@ import ProfileInfoPresenter from '@/components/profile/info/ProfileInfoPresenter
 
 const MyProfileContainer = () => {
   const { data } = useMyProfileQuery();
-  // const { replace } = useRouter();
-  // const pathname = usePathname();
-
-  // useEffect(() => {
-  //   const {
-  //     nickname,
-  //     job: { jobGroupName, jobName },
-  //   } = data;
-  //   const isSavedAdditionalInfo = !!(nickname && jobGroupName && jobName);
-  //   if (!isSavedAdditionalInfo) replace(`${pathname}/add`);
-  // }, [data, pathname, replace]);
-
   return <ProfileInfoPresenter {...data} />;
 };
 
