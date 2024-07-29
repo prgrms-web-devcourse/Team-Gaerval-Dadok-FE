a4287733a274555bfad0e57f6b613a220f9a70df
gxxrxn <rbfks1221@naver.com>
2024-07-29 20:18:47 +0900

    style: 모임 생성 유도 배너 heading font size 수정

--- src/components/bookGroup/banner/CreateGroupBanner.tsx
+++ src/components/bookGroup/banner/CreateGroupBanner.tsx
@@ -4,7 +4,7 @@ const CreateGroupBanner = () => {
   return (
     <article className="flex w-full cursor-pointer select-none items-center justify-between rounded-[0.8rem] bg-main-300 p-[2rem]">
       <div className="flex flex-col gap-[0.6rem]">
-        <h3 className="text-main-900 font-body1-bold">원하는 모임이 없다면?</h3>
+        <h3 className="text-main-900 font-body2-bold">원하는 모임이 없다면?</h3>
         <p className="whitespace-pre-line text-black-600 font-body2-bold">
           {`읽고 싶은 책으로
             직접 모임을 만들어 보세요`}
