58200d7daa34946b14fac949923ae35027f65a02
harry kim <73218463+hanyugeon@users.noreply.github.com>
2024-08-05 23:43:29 +0900

    [#669] [유저 책장] 존재하지 않는 유저 책장 페이지 예외처리 (#671)
    * fix: 비로그인 상태에서 꽂은책이 없는 책장에 접근시 발생하는 에러 해결
    
    * fix: 존재하지 않는 유저 책장 페이지 예외처리
    
    * feat: axios interceptor에 isNotFoundError 분기 추가
    
    * feat: 유저 페이지 not-found.tsx 작성
    
    * feat: 유저 프로필 페이지에 ErrorBoundary 추가
    
    * chore: axios에 notFound 분기 제거

---
+++ src/app/profile/[userId]/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Button from '@/components/common/Button';
+
+export default function NotFound() {
+  return (
+    <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center gap-[2rem]">
+      <Image src="/images/loading.gif" width={230} height={160} alt="loading" />
+      <p className="font-heading-bold">
+        <span className="font-bold text-main-900">다독이</span>가 길을 잃었어요.
+      </p>
+      <Link href="/bookarchive">
+        <Button size="large" colorScheme="main" fill={false}>
+          홈으로 가기
+        </Button>
+      </Link>
+    </div>
+  );
+}
