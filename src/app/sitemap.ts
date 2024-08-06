dc7d77d183dc1d2bf9130528467556fc546c2005
hanyugeon <hanyugeon@naver.com>
2024-08-06 18:17:35 +0900

    fix: NEXT_PUBLIC_PRODUCTION_URL을 NEXT_PRODUCTION_URL로 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,15 +7,15 @@ import { default as bookGroupSitemap } from './group/sitemap';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/bookarchive`,
+      url: `${process.env.NEXT_PRODUCTION_URL}/bookarchive`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/group`,
+      url: `${process.env.NEXT_PRODUCTION_URL}/group`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/profile/me`,
+      url: `${process.env.NEXT_PRODUCTION_URL}/profile/me`,
       lastModified: new Date(),
     },
     ...(await bookSitemap()),
