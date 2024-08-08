a472fce25a94ca5a775d42db43f9eed77aefade0
hanyugeon <hanyugeon@naver.com>
2024-08-08 15:23:01 +0900

    fix: LoginBottomActionButton 내부의 link 태그 위치 수정

--- src/components/common/LoginBottomActionButton.tsx
+++ src/components/common/LoginBottomActionButton.tsx
@@ -3,11 +3,11 @@ import LoginLink from '@/components/common/LoginLink';
 import Button from '@/components/common/Button';
 
 const LoginBottomActionButton = () => (
-  <LoginLink>
-    <StickyFooter>
+  <StickyFooter>
+    <LoginLink className="w-full">
       <Button size="full">로그인 및 회원가입</Button>
-    </StickyFooter>
-  </LoginLink>
+    </LoginLink>
+  </StickyFooter>
 );
 
 export default LoginBottomActionButton;
