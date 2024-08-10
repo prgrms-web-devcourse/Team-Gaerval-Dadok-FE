ed5e06932a48313959cb819df751e47e75f11a40
hanyugeon <hanyugeon@naver.com>
2024-08-10 22:10:30 +0900

    fix: TopHeader에 누락된 safeArea 추가

--- src/components/common/TopHeader.tsx
+++ src/components/common/TopHeader.tsx
@@ -8,7 +8,7 @@ type TopHeaderProps = PropsWithChildren<{
 const TopHeader = ({ text, className, children }: TopHeaderProps) => {
   return (
     <header
-      className={`fixed left-0 right-0 top-0 z-50 mx-auto flex w-full max-w-[43rem] items-center justify-between bg-white px-[2rem] pt-[2rem] ${className}`}
+      className={`fixed left-0 right-0 top-0 z-50 mx-auto flex w-full max-w-[43rem] items-center justify-between bg-white px-[2rem] pt-[calc(env(safe-area-inset-top)+2rem)] ${className}`}
     >
       <h1 className="text-main-900 font-heading-bold">{text}</h1>
       {children}
