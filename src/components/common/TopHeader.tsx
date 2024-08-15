2c464cc6dfdbbec039935f8b4ecc4838640d1958
hanyugeon <hanyugeon@naver.com>
2024-08-15 23:11:32 +0900

    refactor: TopHeader 컴포넌트의 text props의존성을 제거

--- src/components/common/TopHeader.tsx
+++ src/components/common/TopHeader.tsx
@@ -1,19 +1,17 @@
 import { PropsWithChildren } from 'react';
 
 type TopHeaderProps = PropsWithChildren<{
-  text: string;
   blur?: boolean;
   className?: string;
 }>;
 
 const DEFAULT_HEADER_CLASSES =
-  'fixed left-0 right-0 top-0 z-30 mx-auto flex w-full max-w-[43rem] items-center justify-between border-0 px-[2rem] pb-[1rem] pt-[calc(env(safe-area-inset-top)+2rem)] transition duration-1000';
+  'fixed left-0 right-0 top-0 z-30 mx-auto w-full max-w-[43rem] border-0 px-[2rem] pb-[1rem] pt-[calc(env(safe-area-inset-top)+2rem)] transition duration-1000';
 
 const BLUR_HEADER_CLASSES =
   'border-b-black-100 border-b-[0.01rem] bg-[#FFFFFFBF] backdrop-blur-[1.6rem]';
 
 const TopHeader = ({
-  text,
   blur = false,
   className = '',
   children,
@@ -22,7 +20,6 @@ const TopHeader = ({
 
   return (
     <header className={`${DEFAULT_HEADER_CLASSES} ${blurClasses} ${className}`}>
-      <h1 className="text-main-900 font-heading-bold">{text}</h1>
       {children}
     </header>
   );
