7c4429b16dcb644ba038915cc8ecd83f8c3914e7
hanyugeon <hanyugeon@naver.com>
2024-08-15 18:14:20 +0900

    feat: TopHeader에 blur 스타일 및 props 추가

--- src/components/common/TopHeader.tsx
+++ src/components/common/TopHeader.tsx
@@ -2,14 +2,26 @@ import { PropsWithChildren } from 'react';
 
 type TopHeaderProps = PropsWithChildren<{
   text: string;
+  blur?: boolean;
   className?: string;
 }>;
 
-const TopHeader = ({ text, className, children }: TopHeaderProps) => {
+const DEFAULT_HEADER_CLASSES =
+  'fixed left-0 right-0 top-0 z-30 mx-auto flex w-full max-w-[43rem] items-center justify-between border-0 px-[2rem] pb-[1rem] pt-[calc(env(safe-area-inset-top)+2rem)] transition duration-1000';
+
+const BLUR_HEADER_CLASSES =
+  'border-b-black-100 border-b-[0.01rem] bg-[#FFFFFFBF] backdrop-blur-[1.6rem]';
+
+const TopHeader = ({
+  text,
+  blur = false,
+  className = '',
+  children,
+}: TopHeaderProps) => {
+  const blurClasses = blur ? BLUR_HEADER_CLASSES : 'bg-white';
+
   return (
-    <header
-      className={`fixed left-0 right-0 top-0 z-50 mx-auto flex w-full max-w-[43rem] items-center justify-between bg-white px-[2rem] pt-[calc(env(safe-area-inset-top)+2rem)] ${className}`}
-    >
+    <header className={`${DEFAULT_HEADER_CLASSES} ${blurClasses} ${className}`}>
       <h1 className="text-main-900 font-heading-bold">{text}</h1>
       {children}
     </header>
