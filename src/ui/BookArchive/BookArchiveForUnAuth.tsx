907964dd02955164a428c552939d4f2dbecb14e5
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#410] [북카이브] 북카이브 페이지 (비회원) (#425)"
    This reverts commit 08c919d173e9997f5e5e2c94d6ea47e2798f2555.

--- src/ui/BookArchive/BookArchiveForUnAuth.tsx
+++ src/ui/BookArchive/BookArchiveForUnAuth.tsx
@@ -1,6 +1,6 @@
 import useUnAuthRecommendedBookshelfQuery from '@/queries/recommend/useUnAuthRecommendedBookshelfQuery';
-import { Skeleton, VStack } from '@chakra-ui/react';
-import Bookshelf from '../Bookshelf';
+import { Flex, Skeleton, VStack } from '@chakra-ui/react';
+import { RecommendedBookshelf } from '../Recommended';
 
 const BookArchiveForUnAuth = () => {
   const { data, isSuccess, isLoading } = useUnAuthRecommendedBookshelfQuery();
@@ -17,14 +17,19 @@ const BookArchiveForUnAuth = () => {
   if (!isSuccess) return null;
 
   return (
-    <div className="flex flex-col gap-[1.5rem] w-full">
-      <div className="text-md font-bold">🔥 인기 책장</div>
-      <div className="flex w-full flex-col gap-[3rem]">
-        {data.bookshelfResponses.map(bookshelf => (
-          <Bookshelf key={bookshelf.bookshelfId} {...bookshelf} />
-        ))}
-      </div>
-    </div>
+    <Flex direction="column" width="100%" gap="3rem">
+      {data.bookshelfResponses.map(
+        ({ bookshelfId, bookshelfName, books, likeCount }) => (
+          <RecommendedBookshelf
+            key={bookshelfId}
+            bookshelfId={bookshelfId}
+            bookshelfName={bookshelfName}
+            books={books}
+            likeCount={likeCount}
+          />
+        )
+      )}
+    </Flex>
   );
 };
 
