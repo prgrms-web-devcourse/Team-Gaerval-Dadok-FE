ed4d2495ea06fc8972bbd54308a61ebc2edb82c6
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#564] BottomActionButton 컴포넌트 position 속성 수정 (#566)"
    This reverts commit 94365eee6b42daa9e02a8ef8f35d54d73b35fd97.

--- src/v1/base/BottomActionButton.tsx
+++ src/v1/base/BottomActionButton.tsx
@@ -11,11 +11,11 @@ const BottomActionButton = ({
   ...props
 }: BottomActionButtonProps) => {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 z-10 mx-auto w-full max-w-[43rem] bg-white px-[2.0rem] py-[1.5rem]">
+    <div className="absolute bottom-0 left-0 z-10 w-full bg-white px-[2.0rem] py-[1.5rem]">
       <Button size="full" {...props}>
         {children}
       </Button>
-    </footer>
+    </div>
   );
 };
 
