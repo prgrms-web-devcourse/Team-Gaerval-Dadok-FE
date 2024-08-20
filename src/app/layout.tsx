6cc240fe1c009f16efc8ef0c0fe687ef3bba5980
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#349] 메타 데이터 추가 (#617)"
    This reverts commit 209ba3eabbce518825c147636c265b798d79b596.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import { Metadata } from 'next';
-
 import ContextProvider from '@/components/ContextProvider';
 import AuthFailedErrorBoundary from '@/components/AuthFailedErrorBoundary';
 import Layout from '@/v1/layout/Layout';
@@ -7,41 +5,15 @@ import Layout from '@/v1/layout/Layout';
 import { LineSeedKR } from '@/styles/font';
 import '@/styles/global.css';
 
-export const metadata: Metadata = {
-  metadataBase: new URL(`${process.env.NEXT_HOST}`),
-  title: {
-    template: '%s | 다독다독',
-    default: '다독다독',
-  },
-  description: '책에 대한 인사이트를 공유하고 소통하는 독서 소셜 플랫폼',
-  keywords: [
-    '다독다독',
-    'dadok',
-    'dadokdadok',
-    '책장',
-    '책추천',
-    '도서검색',
-    '독서모임',
-    '책',
-    '독서',
-  ],
-  verification: {
-    google: '72kN3MWyQHuvSb8V67dVkfPUPMrw102Tm6BsvTvfKmg',
-    other: {
-      'naver-site-verification': '9046af5eda448309a92e2e923a45cb874df986a0',
-    },
-  },
-};
-
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
       <head>
+        <title>다독다독</title>
         <meta
           content="width=device-width, initial-scale=1, maximum-scale=1"
           name="viewport"
         />
-        <meta charSet="UTF-8" />
         <link rel="icon" href="/favicon.ico" />
       </head>
       {/* @todo Chakra 제거시 app-layout 프로퍼티 제거. */}
