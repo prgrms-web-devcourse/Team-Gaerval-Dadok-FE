341846a286c7aad192e49bbddf9d5621d34b1406
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#498] useFunnel 작성 (#501)"
    This reverts commit e6f7878b13cf15993fb5668fc7b0d71715bea86c.

--- src/utils/assert.ts
+++
@@ -1,9 +0,0 @@
-export const assert = (condition: unknown, error: Error | string) => {
-  if (!condition) {
-    if (typeof error === 'string') {
-      throw new Error(error);
-    } else {
-      throw error;
-    }
-  }
-};
