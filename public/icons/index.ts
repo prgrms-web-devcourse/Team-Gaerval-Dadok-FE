3ebec6ff9ae937a4bd357460373bd076d014b195
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#358] TopNavigation 컴포넌트 (#378)"
    This reverts commit 41f8f99f2b248265f0fa137cce7ffb9e6288cb7b.

--- public/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as IconLeftArrow } from './left-arrow.svg';
-export { default as IconShare } from './share.svg';
-export { default as IconPost } from './post.svg';
-export { default as IconOptions } from './options.svg';
