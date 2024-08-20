b1649a43f5376565bcadb6f070f87c12e8520766
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#362] TopNavigation 컴포넌트 리팩터링 (#393)"
    This reverts commit 55a6b98299ced4a96f1e83b8b19ac3ced84077b9.

--- src/ui/Base/TopNavigation.tsx
+++ src/ui/Base/TopNavigation.tsx
@@ -1,53 +1,40 @@
-import { PropsWithChildren, ReactNode } from 'react';
+import { IconArrowLeft, IconHamburger } from '@public/icons';
+import Link from 'next/link';
+import { PropsWithChildren } from 'react';
 
-interface TopNavigationProps {
-  children?: ReactNode;
+interface Props extends PropsWithChildren {
+  titleAlign?: 'center' | 'left';
+  title?: string;
+  isOwner?: boolean;
 }
 
-type ItemProps = TopNavigationProps;
-
-const TopNavigation = ({ children }: TopNavigationProps) => {
+const TopNavigation = ({
+  children,
+  titleAlign = 'center',
+  title = '',
+  isOwner = false,
+}: Props) => {
   return (
-    <div className="relative flex h-[2.4rem] w-full items-center justify-center bg-opacity-0 px-[2rem] py-[1.7rem] text-md">
-      {children}
+    <div className="relative flex h-[5.4rem] w-full gap-[1.5rem] bg-opacity-0 px-[2rem] py-[1.7rem]">
+      <Link href=".">
+        <IconArrowLeft className="hover:cursor-pointer" />
+      </Link>
+      <div
+        className={`flex w-full pr-[3.5rem] text-md font-normal leading-[1.9rem] ${TITLE_ALIGN_CLASSES[titleAlign]}`}
+      >
+        {title}
+      </div>
+      <div className="absolute right-[2rem] flex gap-[1rem]">
+        {children}
+        {isOwner && <IconHamburger className="hover:cursor-pointer" />}
+      </div>
     </div>
   );
 };
-
-const LeftItem = ({ children }: ItemProps) => {
-  return (
-    <div className="absolute left-[2rem] [&_svg]:h-[2rem] [&_svg]:w-[2rem] [&_svg]:cursor-pointer">
-      {children}
-    </div>
-  );
-};
-
-type CenterItemProps = PropsWithChildren<{ textAlign?: 'left' | 'center' }>;
-
-const textAligns = {
-  left: 'text-left',
-  center: 'text-center',
-} as const;
-
-const CenterItem = ({ children, textAlign = 'center' }: CenterItemProps) => {
-  const alignClassName = textAligns[textAlign];
-  return (
-    <div className={`w-full px-[3.5rem] ${alignClassName}`}>{children}</div>
-  );
-};
-
-const RightItem = ({ children }: ItemProps) => {
-  return (
-    <div className="absolute right-[2rem] flex gap-[1rem] [&_svg]:h-[2rem] [&_svg]:w-[2rem] [&_svg]:cursor-pointer">
-      {children}
-    </div>
-  );
-};
-
-TopNavigation.LeftItem = LeftItem;
-
-TopNavigation.CenterItem = CenterItem;
-
-TopNavigation.RightItem = RightItem;
 
 export default TopNavigation;
+
+const TITLE_ALIGN_CLASSES = {
+  center: 'justify-center',
+  left: 'justify-start',
+};
