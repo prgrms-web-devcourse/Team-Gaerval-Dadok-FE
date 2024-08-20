058f9b4b1733caef07be5be4daa1e639f39b9305
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#383] Select 컴포넌트 (#385)"
    This reverts commit 7c189bc81c5f6cd5e620903f3690c0520baf14d8.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,16 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+      },
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
