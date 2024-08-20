fdcde5145544eb9ea763604a1c597ec2908096e1
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#370] Storybook & Headless UI 추가 (#371)"
    This reverts commit 7ad8aefd608670a7c7571aff98e81ab2188ed260.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import ContextProvider from '@/components/ContextProvider';
 import { ReactNode } from 'react';
-import '@/styles/global.css';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
