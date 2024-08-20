3e6caae6a131ec021231a3bcbfeb4cb4a6184cd0
kyuran kim <57716832+gxxrxn@users.noreply.github.com>
2024-08-20 16:15:43 +0900

    Revert "[#437] [독서모임] Avartar, bookGroupStatus 적용 및 typo 수정 (#438)"
    This reverts commit b56d3d5fab326dd0aa9fdcbc61e0d5ce7da33cea.

--- src/v1/bookGroup/DetailBookGroupCard.tsx
+++ src/v1/bookGroup/DetailBookGroupCard.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image';
 
 import Badge from '@/ui/Base/Badge';
-import Avatar from '@/ui/Base/Avatar';
 import { IconCalendar, IconMembers, IconComments } from '@public/icons';
-import BookGroupStatus from './BookGroupStatus';
 
 interface DetailBookGroupCardProps {
   title: string;
@@ -17,6 +15,33 @@ interface DetailBookGroupCardProps {
   handleClick: () => void;
 }
 
+type BookGroupStatus = 'before' | 'dday' | 'ongoing' | 'end';
+
+const getBookGroupStatus = (ddayByStart: number, ddayByEnd: number) => {
+  if (ddayByStart > 0) {
+    return {
+      status: 'before' as const,
+      ddayCount: ddayByStart,
+    };
+  } else if (ddayByStart === 0 && ddayByEnd > 0) {
+    return {
+      status: 'dday' as const,
+    };
+  } else if (ddayByStart < 0 && ddayByEnd >= 0) {
+    return {
+      status: 'ongoing' as const,
+    };
+  } else {
+    return {
+      status: 'end' as const,
+    };
+  }
+};
+
+const toDayFromMillseconds = (value: number) => {
+  return Math.ceil(value / (1000 * 60 * 60 * 24));
+};
+
 const DetailBookGroupCard = ({
   title,
   description,
@@ -28,30 +53,42 @@ const DetailBookGroupCard = ({
   isPublic,
   handleClick,
 }: DetailBookGroupCardProps) => {
+  const ddayByStart = toDayFromMillseconds(
+    new Date(date.start).getTime() - new Date().getTime()
+  );
+  const ddayByEnd = toDayFromMillseconds(
+    new Date(date.end).getTime() - new Date().getTime()
+  );
+
+  const { ...ddayProps } = getBookGroupStatus(ddayByStart, ddayByEnd);
+
   return (
     <div
       onClick={handleClick}
       className="h-[16.142rem] w-[35.5rem] rounded-[0.4rem] px-[1.6rem] py-[0.9rem] shadow-[0_0_0.6rem_rgba(180,180,180,0.25)]"
     >
       <div className="mb-[1rem] flex gap-[0.5rem]">
-        <BookGroupStatus start={date.start} end={date.end} />
-        <Public isPublic={isPublic} />
+        <Dday {...ddayProps}></Dday>
+        <Public isPublic={isPublic}></Public>
       </div>
       <div className="flex gap-[1.4rem]">
         <div className="flex flex-col gap-[0.63rem]">
-          <Title title={title} />
-          <Description description={description} />
-          <Duration start={date.start} end={date.end} />
+          <Title title={title}></Title>
+          <Description description={description}></Description>
+          <Duration start={date.start} end={date.end}></Duration>
           <div className="flex w-[22.5rem] justify-between">
-            <Owner name={owner.name} profileImageSrc={owner.profileImageSrc} />
+            <Owner
+              name={owner.name}
+              profileImageSrc={owner.profileImageSrc}
+            ></Owner>
             <div className="flex gap-[0.5rem]">
-              <MemberCount memberCount={memberCount} />
-              <CommentCount commentCount={commentCount} />
+              <MemberCount memberCount={memberCount}></MemberCount>
+              <CommentCount commentCount={commentCount}></CommentCount>
             </div>
           </div>
         </div>
         <div>
-          <Book title={book.title} bookImageSrc={book.bookImageSrc} />
+          <Book title={book.title} bookImageSrc={book.bookImageSrc}></Book>
         </div>
       </div>
     </div>
@@ -59,6 +96,51 @@ const DetailBookGroupCard = ({
 };
 
 export default DetailBookGroupCard;
+
+const getDdayBadgeInfo = (status: BookGroupStatus, ddayCount?: number) => {
+  switch (status) {
+    case 'before':
+      return {
+        colorScheme: 'main' as const,
+        isFilled: true,
+        text: `D-${ddayCount}`,
+      };
+    case 'dday':
+      return {
+        colorScheme: 'main' as const,
+        isFilled: false,
+        text: 'D-day',
+      };
+    case 'ongoing':
+      return {
+        colorScheme: 'main' as const,
+        isFilled: true,
+        text: '진행중',
+      };
+    case 'end':
+      return {
+        colorScheme: 'grey' as const,
+        isFilled: true,
+        text: '모임종료',
+      };
+  }
+};
+
+const Dday = ({
+  status,
+  ddayCount,
+}: {
+  status: BookGroupStatus;
+  ddayCount?: number;
+}) => {
+  const { text, ...badgeProps } = getDdayBadgeInfo(status, ddayCount);
+
+  return (
+    <Badge size="large" {...badgeProps}>
+      {text}
+    </Badge>
+  );
+};
 
 const Public = ({ isPublic }: { isPublic: boolean }) => (
   <Badge size="large" colorScheme="grey">
@@ -106,7 +188,10 @@ const Owner = ({
 }) => {
   return (
     <div className="flex h-[2rem] gap-[0.5rem]">
-      <Avatar name={name} src={profileImageSrc} size="small" />
+      {/* 아바타 컴포넌트로 변경 예정 */}
+      <div className={'relative h-[2rem] w-[2rem] rounded-full bg-black-400'}>
+        {profileImageSrc && <Image alt={name} src={profileImageSrc} fill />}
+      </div>
       <div className="flex items-center text-xs">
         <span>{name}</span>
       </div>
